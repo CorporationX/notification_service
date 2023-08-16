499ce1248d560f62f45f6034eee369efe1780ea6
RasulMurtuzaliev <ras8705@mail.ru>
2023-08-16 16:19:44 +0500

    created UserDto

---
+++ src/main/java/faang/school/notificationservice/dto/UserDto.java
@@ -0,0 +1,15 @@
+package faang.school.notificationservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    private String username;
+}
