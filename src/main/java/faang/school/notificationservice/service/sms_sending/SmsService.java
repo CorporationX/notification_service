8e8770cf6f571c8ad8c3ccbfaacac24dcea269b8
Viktor Ivanov <avacode78@gmail.com>
2023-08-17 11:55:27 +0700

    small refactoring

--- src/main/java/faang/school/notificationservice/service/sms_sending/SmsService.java
+++ src/main/java/faang/school/notificationservice/service/sms_sending/SmsService.java
@@ -5,46 +5,44 @@ import com.vonage.client.sms.MessageStatus;
 import com.vonage.client.sms.SmsSubmissionResponse;
 import com.vonage.client.sms.messages.TextMessage;
 import faang.school.notificationservice.exception.FailedSendException;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class SmsService implements NotificationService {
-    private final VonageClient vonageClient;
-    private final String from;
-    private final String phone;
+    private VonageClient vonageClient;
+    @Value("${vonage.api.key}")
+    private String apiKey;
+    @Value("${vonage.api.secret}")
+    private String apiSecret;
+    @Value("${vonage.api.from}")
+    private String from;
+    @Value("${vonage.api.phone}")
+    private String phone;
 
-    @Autowired
-    public SmsService(VonageClient vonageClient,
-                      @Value("${vonage.api.from}") String from,
-                      @Value("${vonage.api.phone}") String phone) {
-        this.vonageClient = vonageClient;
-        this.from = from;
-        this.phone = phone;
+    @PostConstruct
+    public void init() {
+        vonageClient = VonageClient.builder()
+                .apiKey(apiKey)
+                .apiSecret(apiSecret)
+                .build();
     }
 
     @Override
     public void send(String msg) {
         TextMessage message = new TextMessage(from, phone, msg);
+        SmsSubmissionResponse response = vonageClient.getSmsClient().submitMessage(message);
 
-        try {
-            SmsSubmissionResponse response = vonageClient.getSmsClient().submitMessage(message);
-
-            if (response.getMessages().get(0).getStatus() == MessageStatus.OK) {
-                log.info("Message sent successfully. {}", message);
-            } else {
-                handleSendFailure(response.getMessages().get(0).getErrorText(), message);
-            }
-        } catch (Exception e) {
-            handleSendFailure(e.getMessage(), message);
+        if (response.getMessages().get(0).getStatus() == MessageStatus.OK) {
+            log.info("Message sent successfully. {}", message);
+        } else {
+            String errorText = response.getMessages().get(0).getErrorText();
+            log.error("Failed to send message. {}", errorText);
+            throw new FailedSendException(errorText);
         }
     }
-
-    private void handleSendFailure(String errorText, TextMessage message) {
-        log.error("Failed to send message. Recipient: {}. Error: {}", phone, errorText);
-        throw new FailedSendException("Failed to send SMS to " + phone + ": " + errorText);
-    }
 }
+
