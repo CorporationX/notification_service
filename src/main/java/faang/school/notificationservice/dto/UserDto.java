08121f089f38ac3f620ddfc649c8a0d1e344599f
RasulMurtuzaliev <ras8705@mail.ru>
2023-08-17 22:27:59 +0500

    updated UserDto

--- src/main/java/faang/school/notificationservice/dto/UserDto.java
+++ src/main/java/faang/school/notificationservice/dto/UserDto.java
@@ -1,15 +1,16 @@
 package faang.school.notificationservice.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserDto {
-    private Long id;
+    private long id;
     private String username;
+    private String email;
+    private String phone;
+    private PreferredContact preference;
+
+    public enum PreferredContact {
+        EMAIL, SMS, TELEGRAM
+    }
 }
