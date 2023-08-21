0750e4cea4429c38e121fcd84bd2b846e3aa6b31
zazozhik <ya.940@mail.ru>
2023-08-21 11:38:13 +0300

    решаю

---
+++ src/main/java/faang/school/notificationservice/massages/MessageBuilder.java
@@ -0,0 +1,10 @@
+package faang.school.notificationservice.massages;
+
+import java.util.Locale;
+
+public interface MessageBuilder <T>{
+
+    String buildMessage(T event, Locale locale);
+
+    boolean supportsEventType(Class<?> eventType);
+}
--- src/main/java/faang/school/notificationservice/service/AbstractEventListener.java
+++ src/main/java/faang/school/notificationservice/service/AbstractEventListener.java
@@ -3,6 +3,8 @@ package faang.school.notificationservice.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.notificationservice.client.UserServiceClient;
 import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.exception.DataValidationException;
+import faang.school.notificationservice.massages.MessageBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -17,84 +19,23 @@ public abstract class AbstractEventListener {
     private final List<NotificationService> notificationServices;
     private final List<MessageBuilder> messageBuilders;
 
-    protected String getMessage(Class<?> eventType, Locale locale, String... args) {
+    public String getMessage(Class<?> eventType, Locale locale) {
         return messageBuilders.stream()
                 .filter(messageBuilder -> messageBuilder.supportsEventType(eventType))
                 .findFirst()
-                .map(messageBuilder -> messageBuilder.buildMessage(eventType, locale,args))
-                .orElseThrow(() -> new IllegalArgumentException("No message builder found for the given event type: " + eventType.getName()));
+                .map(messageBuilder -> messageBuilder.buildMessage(eventType, locale))
+                .orElseThrow(() -> new DataValidationException
+                        ("No message builder found for the given event type: " + eventType.getName()));
     }
 
-    protected void sendNotification(Long id, String message) {
+    public void sendNotification(Long id, String message) {
         UserDto user = userServiceClient.getUser(id);
 
         notificationServices.stream()
                 .filter(notificationService -> notificationService.getPreferredContact().equals(user.getPreference()))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("No notification service found for the user's preferred communication method."))
+                .orElseThrow(() -> new DataValidationException
+                        ("No notification service found for the user's preferred communication method."))
                 .send(user, message);
     }
-
-
-//getMessage — получает в качестве параметров тип эвента (Class<?>),
-// локаль пользователя, а также набор дополнительных аргументов,
-// которые могут использоваться в MessageBuilder для формирования сообщения из шаблона.
-// Выбирает из набора всех MessageBuilder подходящий для переданного типа сообщения
-// и возвращает сообщение, которое строит на основе прочих параметров выбранный MessageBuilder.
-//   public String getMessage(Class<?> eventType, Locale userLocale, Map<String, Object> args) {
-//       messageBuilders.stream()
-//               .filter(messageBuilder -> messageBuilder.isApplicable(eventType))
-//               .toList();
-//       return getMessage(eventType, userLocale, args);
-//   }
-
-
-//sendNotification — получает id пользователя и текст сообщения.
-// Используя UserServiceClient получает всю информацию о пользователе, выбирает из набора всех бино
-// в NotificationService тот, который подходит под предпочитаемый пользователем способ коммуникации
-// (содержится в UserDto), а затем отправляет нотификацию, используя выбранный NotificationService.
-//    public void sendNotification(Long userId, String message) {
-//        UserDto user = userServiceClient.getUser(userId);
-//        notificationServiceList.stream()
-//                .filter(notificationService -> notificationService.supports(user.getPreferredNotificationType()))
-//    }
-
-
-
-    //Критерии приема
-    //
-    //AbstractEventListener полагается только на интерфейсы.
-    //
-    //Корректно выбирает MessageBuilder и NotificationService бины под конкретный эвент и предпочитаемый пользователем способ связи.
-    //
-    //Написаны unit-тесты.
-
-//    public String getMessage(Class<?> eventType, Locale userLocale, Map<String, Object> args) {
-//        Optional<MessageBuilder> selectedMessageBuilder = messageBuilders.stream()
-//                .filter(builder -> builder.supports(eventType))
-//                .findFirst();
-//
-//        if (selectedMessageBuilder.isPresent()) {
-//            MessageBuilder messageBuilder = selectedMessageBuilder.get();
-//            return messageBuilder.buildMessage(eventType, userLocale, args);
-//        } else {
-//            throw new UnsupportedOperationException("No suitable MessageBuilder found for event type: " + eventType);
-//        }
-//    }
-//
-//    public void sendNotification(Long userId, String message) {
-//        UserDto user = userServiceClient.getUserInfo(userId);
-//        NotificationType preferredNotificationType = user.getPreferredNotificationType();
-//
-//        Optional<NotificationService> selectedNotificationService = notificationServices.stream()
-//                .filter(service -> service.supports(preferredNotificationType))
-//                .findFirst();
-//
-//        if (selectedNotificationService.isPresent()) {
-//            NotificationService notificationService = selectedNotificationService.get();
-//            notificationService.sendNotification(user, message);
-//        } else {
-//            throw new UnsupportedOperationException("No suitable NotificationService found for preferred notification type: " + preferredNotificationType);
-//        }
-//    }
 }
