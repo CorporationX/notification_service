31dad26f9186b78b4c9eddba2b671fcc4c8377cc
Zlata <585zlata@mail.ru>
2023-08-24 16:59:58 +0300

    first eventual version for code review (without tests)

--- src/main/java/faang/school/notificationservice/service/SmsService.java
+++ src/main/java/faang/school/notificationservice/service/SmsService.java
@@ -3,53 +3,29 @@ package faang.school.notificationservice.service;
 import com.vonage.client.sms.MessageStatus;
 import com.vonage.client.sms.SmsSubmissionResponse;
 import com.vonage.client.sms.messages.TextMessage;
-import faang.school.notificationservice.client.VonageClient;
 import faang.school.notificationservice.dto.UserDto;
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
+//@Slf4j
 public class SmsService implements NotificationService {
-    // Задание
-//    Создать Spring-компонент SmsService, который будет использоваться для отправки
-//    SMS нотификаций пользователю на его номер телефона. Отправка SMS будет происходить через Vonage.
-//    — это сервис телефонии с удобным и бесплатным пробным API. Нужно будет с ним интегрироваться.
-//
-//    Для этого нужно создать личный аккаунт на https://dashboard.nexmo.com/getting-started/sms
-//    и разобраться с документацией интеграции на Java. Сконфигурировать всё необходимое согласно
-//    документации в notification_service, а затем использовать созданные Vonage компоненты внутри нового SmsService.
-//
-//    SmsService должен реализовать интерфейс NotificationService, который уже предоставлен.
-//    По сути, SmsService просто принимает сообщение и информацию о пользователе и отправляет
-//    ему SMS через Vonage с переданным сообщением.
-//
-//    Критерии приема
-//
-//    1.Создана конфигурация Vonage в application.yaml на личный аккаунт.
-//
-//    2. Созданы все необходимые Spring-бины для работы с Vonage.
-//
-//    3.SMS действительно отправляется на тестовый номер (личный).
-//
-//    4. Написаны unit-тесты.
+    private com.vonage.client.VonageClient vonageClient;
 
-
-    private VonageClient vonageClient;
-    private com.vonage.client.VonageClient client;
+    @Value("${vonage.api.key}")
+    private String apiKey;
+    @Value("${vonage.api-secret}")
+    private String apiSecret;
+    @Value("${vonage.from}")
+    private String from;
 
     @PostConstruct
     public void initializationCreatingVonageClient() {
-        client = VonageClient.builder()
+        vonageClient = com.vonage.client.VonageClient.builder()
                 .apiKey("ec1a2ee7")
                 .apiSecret("HPRDmtd9nEOsdhqh")
                 .build();
-    }
-
-    @Autowired
-    public SmsService(VonageClient vonageClient) {
-
-        this.vonageClient = vonageClient;
     }
 
     @Override
@@ -57,27 +33,13 @@ public class SmsService implements NotificationService {
         return UserDto.PreferredContact.SMS;
     }
 
-    /*    public static void main(String[] args) {
-            VonageClient vonageClient = VonageConfig.createVonageClient();
-            TextMessage message = new TextMessage("Vonage APIs",
-                    "79168822014", "A text message sent using the Vonage SMS API");
-            SmsSubmissionResponse response = vonageClient.getSmsClient().submitMessage(message);
-            if (response.getMessages().get(0).getStatus() == MessageStatus.OK) {
-                System.out.println("Message sent successfully.");
-            } else {
-                System.out.println("Message failed with error: " + response.getMessages().get(0).getErrorText());
-            }
-        }*/
     @Override
-    //SmsService, который будет использоваться для отправки
-    //SMS нотификаций пользователю на его номер телефона
     public void send(UserDto user, String message) {
-        //com.vonage.client.VonageClient vonageClient = VonageClient.initializationCreatingVonageClient();
         /*TextMessage message = new TextMessage("Vonage APIs",
                 "79168822014", "A text message sent using the Vonage SMS API");*/
-        TextMessage message = new TextMessage(VonageClient.from,
-                "79168822014", "A text message sent using the Vonage SMS API");
-        SmsSubmissionResponse response = vonageClient.getSmsClient().submitMessage(message);
+        TextMessage textMessage = new TextMessage(from,
+                user.getPhone(), "This awesome text message sent successfully using the Vonage SMS API");
+        SmsSubmissionResponse response = vonageClient.getSmsClient().submitMessage(textMessage);
         if (response.getMessages().get(0).getStatus() == MessageStatus.OK) {
             System.out.println("Message sent successfully.");
         } else {
