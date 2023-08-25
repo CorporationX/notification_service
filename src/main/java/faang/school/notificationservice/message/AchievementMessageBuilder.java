b40eb684848d32392801f9f6027ef2d8820bbbed
kxnvg <nikita.armeec.88@mail.ru>
2023-08-25 17:46:25 +0300

    bugfix

--- src/main/java/faang/school/notificationservice/message/AchievementMessageBuilder.java
+++ src/main/java/faang/school/notificationservice/message/AchievementMessageBuilder.java
@@ -6,10 +6,7 @@ import faang.school.notificationservice.dto.event.EventType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
-
-import java.util.Locale;
 
 @Component
 @RequiredArgsConstructor
@@ -20,18 +17,9 @@ public class AchievementMessageBuilder implements MessageBuilder {
 
     @Override
     public String buildMessage(UserDto userDto, EventDto eventDto) {
-        String message = getPredefinedMessageForCurrentLocale(userDto.getUsername());
-        log.info("Message for user:{} has built successfully", userDto.getUsername());
+        String message = messageSource.getMessage("achievement.new", new String[]{userDto.getUsername()}, userDto.getLocale());
+        log.info("Message for achievement notification for user:{} has built successfully", userDto.getUsername());
         return message;
-    }
-
-    private String getPredefinedMessageForCurrentLocale(String userName) {
-        Locale locale = LocaleContextHolder.getLocale();
-        return getPredefinedMessage(locale, userName);
-    }
-
-    private String getPredefinedMessage(Locale locale, String userName) {
-        return messageSource.getMessage("achievement.new", new String[]{userName}, locale);
     }
 
     public EventType getEventType() {
--- src/main/java/faang/school/notificationservice/message/FollowerMessageBuilder.java
+++ src/main/java/faang/school/notificationservice/message/FollowerMessageBuilder.java
@@ -6,10 +6,7 @@ import faang.school.notificationservice.dto.event.EventType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
-
-import java.util.Locale;
 
 @Component
 @RequiredArgsConstructor
@@ -20,18 +17,9 @@ public class FollowerMessageBuilder implements MessageBuilder {
 
     @Override
     public String buildMessage(UserDto userDto, EventDto eventDto) {
-        String message = getPredefinedMessageForCurrentLocale(userDto.getUsername());
+        String message = messageSource.getMessage("follower.new", new String[]{userDto.getUsername()}, userDto.getLocale());
         log.info("Message for follower notification for user:{} has built successfully", userDto.getUsername());
         return message;
-    }
-
-    private String getPredefinedMessageForCurrentLocale(String userName) {
-        Locale locale = LocaleContextHolder.getLocale();
-        return getPredefinedMessage(locale, userName);
-    }
-
-    private String getPredefinedMessage(Locale locale, String userName) {
-        return messageSource.getMessage("follower.new", new String[]{userName}, locale);
     }
 
     public EventType getEventType() {
