ef4074013d7b54bc07d929e86fcc8d6e7ccc66f9
Matvey <matveynikishaev@gmail.com>
2023-08-25 19:53:01 +0300

    Added registration via the bot

--- src/main/java/faang/school/notificationservice/client/UserServiceClient.java
+++ src/main/java/faang/school/notificationservice/client/UserServiceClient.java
@@ -4,10 +4,14 @@ import faang.school.notificationservice.dto.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "user-service", url = "${user-service.host}:${user-service.port}")
 public interface UserServiceClient {
 
     @GetMapping("/users/{id}")
     UserDto getUser(@PathVariable long id);
+
+    @PostMapping("/users/{userId}/telegram/{telegramId}")
+    UserDto sendTelegramId(@PathVariable long userId, @PathVariable long telegramId);
 }
--- src/main/java/faang/school/notificationservice/telegram/TelegramNotificationBot.java
+++ src/main/java/faang/school/notificationservice/telegram/TelegramNotificationBot.java
@@ -1,5 +1,7 @@
 package faang.school.notificationservice.telegram;
 
+import faang.school.notificationservice.client.UserServiceClient;
+import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.retry.annotation.Backoff;
@@ -15,22 +17,30 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 public class TelegramNotificationBot extends TelegramLongPollingBot {
     @Value("${telegram.bot.name}")
     private String name;
+    private final UserServiceClient userServiceClient;
 
-    public TelegramNotificationBot(@Value("${telegram.bot.token}") String token) {
+    public TelegramNotificationBot(@Value("${telegram.bot.token}") String token, UserServiceClient userServiceClient) {
         super(token);
+        this.userServiceClient = userServiceClient;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (!(update.hasMessage() && update.getMessage().hasText())) return;
-        SendMessage message = SendMessage.builder()
-                .text("I apologize, but my sole purpose is to deliver notifications.")
-                .chatId(update.getMessage().getChatId())
-                .build();
-        try {
-            execute(message);
-        } catch (TelegramApiException e) {
-            log.error("An error occurred while trying to send a message via Telegram bot: {}", e.getMessage(), e);
+        String text = update.getMessage().getText();
+        long chatId = update.getMessage().getChatId();
+
+        if ("/reg".equals(text)) {
+            SendMessage registrationMessage = SendMessage.builder()
+                    .text("Please provide your ID:")
+                    .chatId(chatId)
+                    .build();
+            try {
+                execute(registrationMessage);
+            } catch (TelegramApiException e) {
+                log.error("An error occurred while trying to send a message: {}", e.getMessage());
+            }
+        } else {
+            processUserResponse(chatId, text);
         }
     }
 
@@ -39,7 +49,6 @@ public class TelegramNotificationBot extends TelegramLongPollingBot {
         return name;
     }
 
-    // @Async Не уверен, нужно ли
     @Retryable(maxAttempts = 5, backoff = @Backoff(delay = 1000, multiplier = 2))
     public void sendMessage(long chat_id, String rowMessage) throws TelegramApiException {
         SendMessage message = SendMessage.builder()
@@ -47,6 +56,16 @@ public class TelegramNotificationBot extends TelegramLongPollingBot {
                 .text(rowMessage)
                 .build();
         execute(message);
+    }
+
+    @Retryable(maxAttempts = 5, backoff = @Backoff(delay = 1000, multiplier = 2))
+    public void processUserResponse(long chatId, String response) {
+        try {
+            long userId = Long.parseLong(response);
+            userServiceClient.sendTelegramId(userId,chatId);
+        } catch (NumberFormatException | FeignException.FeignClientException e) {
+            log.error(e.getMessage());
+        }
     }
 }
 
