159f0ae7d6c8904cf13aace632af4b0516c12b88
Matvey <matveynikishaev@gmail.com>
2023-08-27 16:36:41 +0300

    Added AbstractEventListener and tests

--- src/main/java/faang/school/notificationservice/dto/UserDto.java
+++ src/main/java/faang/school/notificationservice/dto/UserDto.java
@@ -14,6 +14,7 @@ public class UserDto {
     private String username;
     private String email;
     private String phone;
+    private long telegramId;
     private PreferredContact preference;
 
     public enum PreferredContact {
---
+++ src/main/java/faang/school/notificationservice/messaging/AbstractEventListener.java
@@ -0,0 +1,38 @@
+package faang.school.notificationservice.messaging;
+
+import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.service.MessageBuilder;
+import faang.school.notificationservice.service.NotificationService;
+import faang.school.notificationservice.util.JsonMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Locale;
+
+@Component
+@RequiredArgsConstructor
+public class AbstractEventListener<T> {
+    protected final JsonMapper jsonMapper;
+    protected final UserServiceClient userServiceClient;
+    protected final List<MessageBuilder<T>> messageBuilders;
+    protected final List<NotificationService> notificationServices;
+
+    protected String getMessage(T event, Locale locale) {
+        return messageBuilders.stream()
+                .filter(messageBuilder -> messageBuilder.getEventType() == event.getClass())
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("No message builder for " + event.getClass().getName()))
+                .buildMessage(event, locale);
+    }
+
+    protected void sendNotification(long userId, String message) {
+        UserDto userDto = userServiceClient.getUser(userId);
+        notificationServices.stream()
+                .filter(notificationService -> notificationService.getPreferredContact() == userDto.getPreference())
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(userDto.getPreference() + " isn`t supported"))
+                .send(userDto, message);
+    }
+}
