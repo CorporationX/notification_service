2535bb2c7b9654541176958f5710b6b1077da40b
Andrey <khalyavinandrey@gmail.com>
2023-08-30 14:51:04 +0500

    добавил запись в message.properties для нотификации о принятии запроса о менторстве, добавил билдер нотификации о принятии запроса о менторстве, + тест

---
+++ src/main/java/faang/school/notificationservice/service/MentorshipAcceptedEventMessageBuilder.java
@@ -0,0 +1,23 @@
+package faang.school.notificationservice.service;
+
+import faang.school.notificationservice.dto.mentorshiprequest.MentorshipAcceptedDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+@RequiredArgsConstructor
+public class MentorshipAcceptedEventMessageBuilder implements MessageBuilder<MentorshipAcceptedDto> {
+
+    private final MessageSource messageSource;
+
+    @Override
+    public String buildMessage(MentorshipAcceptedDto event, Locale locale) {
+        return messageSource.getMessage(
+                "mentorship_accepted.event",
+                new Object[]{event.getReceiverUsername()},
+                locale);
+    }
+}
