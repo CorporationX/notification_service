3c92b7276c02b6614db141e0cf869c5a210a471f
Devilserk <demonserker@gmail.com>
2023-08-31 16:33:46 +0600

    fix

--- src/main/java/faang/school/notificationservice/dto/redis/LikeEventDto.java
+++ src/main/java/faang/school/notificationservice/dto/redis/LikeEventDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,4 +15,5 @@ public class LikeEventDto {
     private long commentId;
     private long commentAuthor;
     private long likeAuthor;
+    private LocalDateTime dateTime;
 }
