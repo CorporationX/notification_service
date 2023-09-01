a49fdc428c035c694459c56cd373ef86b6d1276e
Basimov Rustam <sombra1nf@yandex.ru>
2023-09-01 18:04:06 +0500

    Write test for Listener

--- src/main/java/faang/school/notificationservice/dto/skill/SkillOfferEvent.java
+++ src/main/java/faang/school/notificationservice/dto/skill/SkillOfferEvent.java
@@ -1,8 +1,10 @@
 package faang.school.notificationservice.dto.skill;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class SkillOfferEvent {
     private Long senderId;
     private Long receiverId;
