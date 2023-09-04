4a643f38ce622c27ded2e822a3ddd03b694eb2ea
AlexanderKolesnikow <kolesnikov.kite@gmail.com>
2023-09-04 22:17:58 +0100

    in progress

--- src/main/java/faang/school/notificationservice/config/telegram/TelegramBotConfig.java
+++ src/main/java/faang/school/notificationservice/config/telegram/TelegramBotConfig.java
@@ -1,4 +1,13 @@
 package faang.school.notificationservice.config.telegram;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class TelegramBotConfig {
+
+    @Value("${telegram-bot.name}")
+    private String name;
+    @Value("${telegram-bot.token}")
+    private String token;
 }
