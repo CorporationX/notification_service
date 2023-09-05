6176bea53fe1691b8d56356c401aead7e074ee75
AlexanderKolesnikow <kolesnikov.kite@gmail.com>
2023-09-05 21:03:08 +0100

    in progress

--- src/main/java/faang/school/notificationservice/config/telegram/TelegramBotConfig.java
+++ src/main/java/faang/school/notificationservice/config/telegram/TelegramBotConfig.java
@@ -1,8 +1,10 @@
 package faang.school.notificationservice.config.telegram;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+@Data
 @Configuration
 public class TelegramBotConfig {
 
---
+++ src/main/java/faang/school/notificationservice/service/TelegramService.java
@@ -0,0 +1,55 @@
+package faang.school.notificationservice.service;
+
+import faang.school.notificationservice.config.telegram.TelegramBotConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class TelegramService extends TelegramLongPollingBot {
+    private final TelegramBotConfig config;
+
+    @Override
+    public String getBotUsername() {
+        return config.getName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            String memberName = update.getMessage().getFrom().getFirstName();
+
+            if (messageText.equals("/start")) {
+                startBot(chatId, memberName);
+            } else {
+                log.info("Unexpected message");
+            }
+        }
+    }
+
+    private void startBot(long chatId, String userName) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText("Hello, " + userName + "! I'm a Telegram bot.");
+
+        try {
+            execute(message);
+            log.info("Reply sent");
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
+        }
+    }
+}
