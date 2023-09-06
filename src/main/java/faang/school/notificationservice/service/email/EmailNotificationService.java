06a81bc2e806c027567e6c565fbb652108791390
gordeens <sstreetunit@yandex.ru>
2023-09-06 22:49:33 +0300

    BC-5831. fix after review.

--- src/main/java/faang/school/notificationservice/service/email/EmailNotificationService.java
+++ src/main/java/faang/school/notificationservice/service/email/EmailNotificationService.java
@@ -2,22 +2,27 @@ package faang.school.notificationservice.service.email;
 
 import faang.school.notificationservice.dto.PreferredContact;
 import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.exception.DataValidationException;
 import faang.school.notificationservice.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
+import static faang.school.notificationservice.commonMessage.ErrorMessageForEmailService.EMAIL_ADDRESS_IS_NULL;
+import static java.util.Objects.isNull;
+
 @Service
 @RequiredArgsConstructor
 public class EmailNotificationService implements NotificationService {
     private final MailSender mailSender;
-    private final EmailValidator emailValidator;
 
     @Override
     public void send(UserDto user, String message) {
         String userEmail = user.email();
-        emailValidator.checkEmail(userEmail);
+        if (isNull(userEmail)) {
+            throw new DataValidationException(EMAIL_ADDRESS_IS_NULL);
+        }
 
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setTo(userEmail);
--- src/test/java/faang/school/notificationservice/service/email/EmailNotificationServiceTest.java
+++ src/test/java/faang/school/notificationservice/service/email/EmailNotificationServiceTest.java
@@ -6,22 +6,14 @@ import faang.school.notificationservice.exception.DataValidationException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 
-import java.text.MessageFormat;
-import java.util.stream.Stream;
-
 import static faang.school.notificationservice.commonMessage.ErrorMessageForEmailService.EMAIL_ADDRESS_IS_NULL;
-import static faang.school.notificationservice.commonMessage.ErrorMessageForEmailService.EMAIL_NOT_VALID_FORMAT;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -29,9 +21,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class EmailNotificationServiceTest {
     @Mock
     private MailSender mailSender;
-
-    @Spy
-    private EmailValidator emailValidator;
 
     @InjectMocks
     private EmailNotificationService emailNotificationService;
@@ -53,42 +42,15 @@ class EmailNotificationServiceTest {
         Mockito.verify(mailSender, Mockito.times(1)).send(mailMessage);
     }
 
-    @ParameterizedTest
-    @MethodSource("provideNotValidData")
-    void testSend_WhenInputDataIsNotValid(UserDto userDto, String expectedMessage) {
+    @Test
+    void testSend_WhenInputDtoWithoutEmail_ShouldThrowException() {
+        UserDto dtoWithoutEmail = UserDto.builder().build();
         String message = ValuesForTest.MESSAGE.getValue();
 
         Exception exception = assertThrows(DataValidationException.class,
-                () -> emailNotificationService.send(userDto, message));
+                () -> emailNotificationService.send(dtoWithoutEmail, message));
 
-        assertEquals(expectedMessage, exception.getMessage());
-    }
-
-
-    private static Stream<Arguments> provideNotValidData() {
-        UserDto dtoWithoutEmail = UserDto.builder().build();
-
-        UserDto dtoWithInvalidEmail1 = UserDto.builder()
-                .email(ValuesForTest.INVALID_EMAIL_1.getValue())
-                .build();
-        String expectedMessage1 = MessageFormat.format(EMAIL_NOT_VALID_FORMAT, ValuesForTest.INVALID_EMAIL_1.getValue());
-
-        UserDto dtoWithInvalidEmail2 = UserDto.builder()
-                .email(ValuesForTest.INVALID_EMAIL_2.getValue())
-                .build();
-        String expectedMessage2 = MessageFormat.format(EMAIL_NOT_VALID_FORMAT, ValuesForTest.INVALID_EMAIL_2.getValue());
-
-        UserDto dtoWithInvalidEmail3 = UserDto.builder()
-                .email(ValuesForTest.INVALID_EMAIL_3.getValue())
-                .build();
-        String expectedMessage3 = MessageFormat.format(EMAIL_NOT_VALID_FORMAT, ValuesForTest.INVALID_EMAIL_3.getValue());
-
-        return Stream.of(
-                Arguments.of(dtoWithoutEmail, EMAIL_ADDRESS_IS_NULL),
-                Arguments.of(dtoWithInvalidEmail1, expectedMessage1),
-                Arguments.of(dtoWithInvalidEmail2, expectedMessage2),
-                Arguments.of(dtoWithInvalidEmail3, expectedMessage3)
-        );
+        assertEquals(EMAIL_ADDRESS_IS_NULL, exception.getMessage());
     }
 
     @Test
