7ef88e46c42951d11e6769d381c7852376403a32
AlexanderKolesnikow <kolesnikov.kite@gmail.com>
2023-09-06 20:36:18 +0100

    Implementing database to the project

--- src/main/java/faang/school/notificationservice/repository/TelegramProfileRepository.java
+++ src/main/java/faang/school/notificationservice/repository/TelegramProfileRepository.java
@@ -2,7 +2,9 @@ package faang.school.notificationservice.repository;
 
 import faang.school.notificationservice.entity.TelegramProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TelegramProfileRepository extends JpaRepository<TelegramProfile, Long> {
     boolean existsByUserName(String userName);
 
