b4cd1172b5147bec420b7c112930d7125d9c5b9d
AlexanderKolesnikow <kolesnikov.kite@gmail.com>
2023-09-08 19:52:46 +0100

    For PR

--- src/main/java/faang/school/notificationservice/NotificationServiceApp.java
+++ src/main/java/faang/school/notificationservice/NotificationServiceApp.java
@@ -3,7 +3,6 @@ package faang.school.notificationservice;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
