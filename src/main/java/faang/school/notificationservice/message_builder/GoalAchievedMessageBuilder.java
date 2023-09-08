8377036fc0875f642d16c87c35f300f74d211604
aliceklim <yuliya.klimovich@icloud.com>
2023-09-08 15:00:59 +0200

    - log added

--- src/main/java/faang/school/notificationservice/message_builder/GoalAchievedMessageBuilder.java
+++ src/main/java/faang/school/notificationservice/message_builder/GoalAchievedMessageBuilder.java
@@ -3,10 +3,12 @@ package faang.school.notificationservice.message_builder;
 import faang.school.notificationservice.dto.GoalCompletedEvent;
 import lombok.AllArgsConstructor;
 import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
 
 import java.util.Locale;
 
 @AllArgsConstructor
+@Component
 public class GoalAchievedMessageBuilder implements MessageBuilder<GoalCompletedEvent>{
 
 private final MessageSource messageSource;
