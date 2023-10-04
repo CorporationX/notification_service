9f7ed399ee8e073d9381b7d60fa51bee9c028726
Stepan Palkov <step.palkov@gmail.com>
2023-10-04 04:53:04 +0300

    ne shlet gmail

--- src/main/java/faang/school/notificationservice/listener/MentorshipAcceptedEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/MentorshipAcceptedEventListener.java
@@ -6,9 +6,11 @@ import faang.school.notificationservice.dto.MentorshipAcceptedEventDto;
 import faang.school.notificationservice.message.MessageBuilder;
 import faang.school.notificationservice.service.notification.NotificationService;
 import org.springframework.data.redis.connection.Message;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class MentorshipAcceptedEventListener extends AbstractEventListener<MentorshipAcceptedEventDto>{
     public MentorshipAcceptedEventListener(ObjectMapper objectMapper, UserServiceClient userServiceClient,
                                            List<NotificationService> notificationServices, List<MessageBuilder<MentorshipAcceptedEventDto>> messageBuilders) {
