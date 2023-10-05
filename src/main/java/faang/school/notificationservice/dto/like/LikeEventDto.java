d2549fb1febd204a3ecffe90c99deb00df8a3fb2
Azat <azat.takhautdinov@gmail.com>
2023-10-05 20:06:32 +0500

    Создание Listener для пост сервиса

---
+++ src/main/java/faang/school/notificationservice/dto/like/LikeEventDto.java
@@ -0,0 +1,31 @@
+package faang.school.notificationservice.dto.like;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+
+import java.time.LocalDateTime;
+
+@Data
+@Validated
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LikeEventDto {
+
+    @NotNull
+    private Long userId;
+
+    @NotNull
+    private Long authorPostId;
+
+    @NotNull
+    private Long postId;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private LocalDateTime createdAt;
+}
