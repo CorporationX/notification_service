8b955b65f94afa16b0b805cbb1f16b34ebfcded1
Azat <azat.takhautdinov@gmail.com>
2023-10-05 20:35:42 +0500

    Добавлены аннотации и final на поле

--- src/main/java/faang/school/notificationservice/notification/LikeMessageBuilder.java
+++ src/main/java/faang/school/notificationservice/notification/LikeMessageBuilder.java
@@ -3,13 +3,17 @@ package faang.school.notificationservice.notification;
 import faang.school.notificationservice.dto.MentorshipOfferRequestSentDto;
 import faang.school.notificationservice.dto.like.LikeEventDto;
 import faang.school.notificationservice.service.MessageBuilder;
+import lombok.AllArgsConstructor;
 import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
 
 import java.util.Locale;
 
+@Component
+@AllArgsConstructor
 public class LikeMessageBuilder implements MessageBuilder<LikeEventDto> {
 
-    private MessageSource messageSource;
+    private final MessageSource messageSource;
 
     @Override
     public String buildMessage(LikeEventDto likeEventDto, Locale locale) {
