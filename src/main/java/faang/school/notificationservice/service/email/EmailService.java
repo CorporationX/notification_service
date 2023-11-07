1ac6f2e70c24bbe3fb6729fc8fe4cc9be6b4a7a2
RasulMurtuzaliev <ras8705@mail.ru>
2023-11-07 17:59:23 +0500

    updated EmailService

--- src/main/java/faang/school/notificationservice/service/email/EmailService.java
+++ src/main/java/faang/school/notificationservice/service/email/EmailService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class EmailService implements NotificationService {
+
     private final JavaMailSender mailSender;
 
     @Override
