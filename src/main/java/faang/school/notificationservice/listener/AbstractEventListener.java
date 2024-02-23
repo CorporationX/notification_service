b9364bb5b84136a900a877f39329c0510e48ff0a
Kamil Gasanov <gasanovkamil516@gmail.com>
2024-02-23 04:43:46 +0300

    update AbstractEventListener

--- src/main/java/faang/school/notificationservice/listener/AbstractEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/AbstractEventListener.java
@@ -1,17 +1,35 @@
 package faang.school.notificationservice.listener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.exception.DataValidationException;
+import faang.school.notificationservice.message.MessageBuilder;
+import faang.school.notificationservice.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public abstract class AbstractEventListener<T> implements MessageListener {
     private ObjectMapper objectMapper;
     private Class<T> eventType;
+    private List<MessageBuilder<T>> messageBuilders;
+    private List<NotificationService> notificationServices;
+
+    @Autowired
+    public void setMessageBuilders(List<MessageBuilder<T>> messageBuilders) {
+        this.messageBuilders = messageBuilders;
+    }
+
+    @Autowired
+    public void setNotificationServices(List<NotificationService> notificationServices) {
+        this.notificationServices = notificationServices;
+    }
 
     @Autowired
     public void setObjectMapper(ObjectMapper objectMapper) {
@@ -31,5 +49,20 @@ public abstract class AbstractEventListener<T> implements MessageListener {
         }
     }
 
-    protected abstract void processEvent(T event);
+    private void processEvent(T event) {
+        UserDto userDto = getUserDto(event);
+        UserDto.PreferredContact preference = userDto.getPreference();
+
+        String message = messageBuilders.stream()
+                .findFirst()
+                .map(messageBuilder -> messageBuilder.buildMessage(event, userDto.getUsername()))
+                .orElseThrow(() -> new DataValidationException("MessageBuilder не найден"));
+
+        notificationServices.stream()
+                .filter(service -> service.getPreferredContact().equals(preference))
+                .findFirst()
+                .ifPresent(service -> service.send(userDto, message));
+    }
+
+    protected abstract UserDto getUserDto(T event);
 }
--- src/main/java/faang/school/notificationservice/listener/GoalCompletedEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/GoalCompletedEventListener.java
@@ -3,35 +3,22 @@ package faang.school.notificationservice.listener;
 import faang.school.notificationservice.client.UserServiceClient;
 import faang.school.notificationservice.dto.GoalCompletedEvent;
 import faang.school.notificationservice.dto.UserDto;
-import faang.school.notificationservice.message.GoalCompletedMessageBuilder;
-import faang.school.notificationservice.service.NotificationService;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 public class GoalCompletedEventListener extends AbstractEventListener<GoalCompletedEvent> {
-    private final GoalCompletedMessageBuilder messageBuilders;
-    private final List<NotificationService> notificationServices;
     private final UserServiceClient userServiceClient;
-    public GoalCompletedEventListener(GoalCompletedMessageBuilder messageBuilders,
-                                      List<NotificationService> notificationServices,
-                                      UserServiceClient userServiceClient) {
+    public GoalCompletedEventListener(UserServiceClient userServiceClient) {
         super(GoalCompletedEvent.class);
-        this.messageBuilders = messageBuilders;
-        this.notificationServices = notificationServices;
         this.userServiceClient = userServiceClient;
     }
 
+    // если ввести абстрактный класс для эвентов, то можно избавиться от этого метода и
+    // сделать абстрактный класс лаконичнее, да и в целом понятнее ведь <AbstractEvent> говрит сам за себя в отличии от <T>
+    // не ввел потому что у остальных возьникнут проблемы
     @Override
-    protected void processEvent(GoalCompletedEvent event) {
-        UserDto userDto = userServiceClient.getUser(event.getUserId());
-        UserDto.PreferredContact preference = userDto.getPreference();
-        String message = messageBuilders.buildMessage(event, userDto.getUsername());
-
-        notificationServices.stream()
-                .filter(service -> service.getPreferredContact().equals(preference))
-                .findFirst()
-                .ifPresent(service -> service.send(userDto, message));
+    protected UserDto getUserDto(GoalCompletedEvent event) {
+        return userServiceClient.getUser(event.getUserId());
     }
+
 }
