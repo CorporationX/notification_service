173724ad7ecb5cc9e97b4febc7248ed522bd2b2a
fd_shell <dariuskpp@gmail.com>
2024-02-24 13:55:41 +0300

    Complete methods without tests

--- src/main/java/faang/school/notificationservice/listener/AbstractEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/AbstractEventListener.java
@@ -3,10 +3,10 @@ package faang.school.notificationservice.listener;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import faang.school.notificationservice.builder.MessageBuilder;
-import faang.school.notificationservice.client.UserServiceClient;
 import faang.school.notificationservice.dto.UserDto;
 import faang.school.notificationservice.exception.EntityNotFoundException;
 import faang.school.notificationservice.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 
@@ -14,25 +14,40 @@ import java.io.IOException;
 import java.util.List;
 
 public abstract class AbstractEventListener<T> implements MessageListener {
-    private final ObjectMapper objectMapper;
-    private final UserServiceClient userServiceClient;
+    private ObjectMapper objectMapper;
     private Class<T> eventDtoClass;
     private List<MessageBuilder<T>> messageBuilders;
     private List<NotificationService> notificationServices;
 
-
-    public AbstractEventListener(ObjectMapper objectMapper, UserServiceClient userServiceClient) {
-        this.objectMapper = objectMapper;
-        this.userServiceClient = userServiceClient;
-        objectMapper.registerModule(new JavaTimeModule());
+    protected AbstractEventListener(Class<T> eventDtoClass) {
+        this.eventDtoClass = eventDtoClass;
     }
+
+    @Autowired
+    private void setObjectMapper(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+        this.objectMapper.registerModule(new JavaTimeModule());
+    }
+
+
+    @Autowired
+    private void setMessageBuilders(List<MessageBuilder<T>> messageBuilders) {
+        this.messageBuilders = messageBuilders;
+    }
+
+    @Autowired
+    private void setNotificationServices(List<NotificationService> notificationServices) {
+        this.notificationServices = notificationServices;
+    }
+
 
     public void onMessage(Message message, byte[] pattern) {
         try {
             T event = objectMapper.readValue(message.getBody(), eventDtoClass);
-
+            UserDto reciever = getUser(event);
+            sendMessage(getBuilder().buildMessage(event), reciever);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 
@@ -53,5 +68,5 @@ public abstract class AbstractEventListener<T> implements MessageListener {
                 .orElseThrow( () -> new EntityNotFoundException("Notification service not found"));
     }
 
-
+    public abstract UserDto getUser(T event);
 }
--- src/main/java/faang/school/notificationservice/listener/FollowerEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/FollowerEventListener.java
@@ -1,45 +1,22 @@
 package faang.school.notificationservice.listener;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import faang.school.notificationservice.builder.FollowerMessageBuilder;
 import faang.school.notificationservice.client.UserServiceClient;
 import faang.school.notificationservice.dto.FollowerEventDto;
 import faang.school.notificationservice.dto.UserDto;
-import faang.school.notificationservice.service.NotificationService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.connection.Message;
-import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 
 @Component
-@RequiredArgsConstructor
-public class FollowerEventListener implements MessageListener {
-    private final ObjectMapper objectMapper;
+public class FollowerEventListener extends AbstractEventListener<FollowerEventDto> {
     private final UserServiceClient userServiceClient;
-    private final FollowerMessageBuilder followerMessageBuilder;
-    private final NotificationService notificationService;
+
+    public FollowerEventListener(UserServiceClient userServiceClient) {
+        super(FollowerEventDto.class);
+        this.userServiceClient = userServiceClient;
+    }
 
     @Override
-    public void onMessage(Message message, byte[] pattern) {
-        try {
-            objectMapper.registerModule(new JavaTimeModule());
-            FollowerEventDto followerEventDto = objectMapper.readValue(message.getBody(), FollowerEventDto.class);
-
-            // Собрать сообщение
-            // получить юзеров
-            UserDto follower = userServiceClient.getUser(followerEventDto.getFollowerId());
-
-            // отправить сообщение
-            UserDto followee = userServiceClient.getUser(followerEventDto.getFolloweeId());
-            // получить способ доставки
-            //...
-//            notificationService.send(followee, text);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
+    public UserDto getUser(FollowerEventDto event) {
+        return userServiceClient.getUser(event.getFolloweeId());
     }
 }
