8548f9ac7a7cb76cbb25097f4428fd050e922ab5
Nikita <soul2009.89@gmail.com>
2024-03-03 20:01:51 +0500

    4

--- src/main/java/faang/school/notificationservice/message/FollowerEventListener.java
+++ src/main/java/faang/school/notificationservice/message/FollowerEventListener.java
@@ -5,16 +5,21 @@ import faang.school.notificationservice.client.UserServiceClient;
 import faang.school.notificationservice.dto.FollowerEvent;
 import faang.school.notificationservice.dto.UserDto;
 import faang.school.notificationservice.service.TelegramService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class FollowerEventListener implements MessageListener {
 
     private final ObjectMapper objectMapper;
@@ -27,8 +32,11 @@ public class FollowerEventListener implements MessageListener {
     public void onMessage(Message message, byte[] pattern) {
         FollowerEvent event;
         try {
+            log.info("из json хотим сделать event, {}", message);
             event = objectMapper.readValue(message.getBody(), FollowerEvent.class);
+            log.info("из json делаем event, {}", message);
         } catch (IOException e) {
+            log.error("не получилось из json делаем event, {}", message);
             throw new RuntimeException(e);
         }
         UserDto user = userServiceClient.getUser(event.getFolloweeId());
