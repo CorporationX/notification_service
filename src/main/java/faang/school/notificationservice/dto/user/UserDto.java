af86630cd65da9164fbf43105657116c2ccdb95b
Rikaf <fakir207@mail.ru>
2024-03-06 01:51:38 +0500

    pre sleep

--- src/main/java/faang/school/notificationservice/dto/user/UserDto.java
+++ src/main/java/faang/school/notificationservice/dto/user/UserDto.java
@@ -1,11 +1,17 @@
 package faang.school.notificationservice.dto.user;
 
 import faang.school.notificationservice.dto.skill.SkillDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
     private long id;
     private String username;
