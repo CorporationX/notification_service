ef11c7f446a707a1586b57e3c9c5229da744cb46
Rikaf <fakir207@mail.ru>
2024-03-05 19:37:21 +0500

    working

---
+++ src/main/java/faang/school/notificationservice/messages/listener/AbstractEventListener.java
@@ -0,0 +1,53 @@
+package faang.school.notificationservice.messages.listener;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.messages.builder.MessageBuilder;
+import faang.school.notificationservice.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Consumer;
+
+@RequiredArgsConstructor
+@Slf4j
+public abstract class AbstractEventListener<T> {
+
+    protected final UserServiceClient userServiceClient;
+    protected final List<NotificationService> notificationServices;
+    protected final List<MessageBuilder<T>> messageBuilders;
+    protected final ObjectMapper objectMapper;
+
+    protected String getMessage(T eventType, Locale locale) {
+        return messageBuilders.stream()
+                .filter(messageBuilder -> messageBuilder.supportsEventType().equals(eventType.getClass()))
+                .findFirst()
+                .map(messageBuilder -> messageBuilder.builderMessage(eventType, locale))
+                .orElseThrow(() -> new IllegalArgumentException("Unsupported event type: " + eventType.getClass()));
+    }
+
+    protected void sendNotification(long id, String message) {
+        UserDto user = userServiceClient.getUser(id);
+        notificationServices.stream()
+                .filter(notificationService -> notificationService.getPreferredContact().equals(user.getPreference()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Ne naiden"))
+                .send(user, message);
+        log.info("оправлено");
+    }
+
+    protected void handleEvent(Message message, Class<T> type, Consumer<T> consumer) {
+        try {
+            T event = objectMapper.readValue(message.getBody(), type);
+            consumer.accept(event);
+        } catch (IOException e) {
+            log.error("Ошибка десериализации JSON в объект  " + type.getName(), e);
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/faang/school/notificationservice/messages/listener/LikeEventListener.java
+++ src/main/java/faang/school/notificationservice/messages/listener/LikeEventListener.java
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.notificationservice.client.UserServiceClient;
 import faang.school.notificationservice.dto.LikeEventDto;
 import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.messages.builder.MessageBuilder;
 import faang.school.notificationservice.service.NotificationService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
@@ -15,13 +15,15 @@ import java.io.IOException;
 import java.util.List;
 
 @Component
-@RequiredArgsConstructor
 @Slf4j
-public class LikeEventListener implements MessageListener {
+public class LikeEventListener extends AbstractEventListener<LikeEventDto> implements MessageListener {
 
-    private final ObjectMapper objectMapper;
-    private final UserServiceClient userServiceClient;
-    private final List<NotificationService> notificationServices;
+    public LikeEventListener(UserServiceClient userServiceClient,
+                             List<NotificationService> notificationServices,
+                             List<MessageBuilder<LikeEventDto>> messageBuilders,
+                             ObjectMapper objectMapper) {
+        super(userServiceClient, notificationServices, messageBuilders, objectMapper);
+    }
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
