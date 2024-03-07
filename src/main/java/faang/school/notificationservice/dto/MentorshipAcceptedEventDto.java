29fc00961fa8d134cdf4bf477663d8fd40b2794e
Marina Razina <razinamv3@gmail.com>
2024-03-07 08:54:30 +0300

    fix

--- src/main/java/faang/school/notificationservice/dto/MentorshipAcceptedEventDto.java
+++ src/main/java/faang/school/notificationservice/dto/MentorshipAcceptedEventDto.java
@@ -4,13 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class MentorshipAcceptedEventDto {
-    private long id;
     private long requesterId;
     private long receiverId;
+    private LocalDateTime createdAt;
     private String requesterName;
     private String receiverName;
 }
