f23927633639ef157ad22be9659f0122f600e7cd
Fedor Shtanko <atraxlxxxl@gmail.com>
2024-04-14 21:55:44 +0300

    enable retry

--- src/main/java/faang/school/notificationservice/NotificationServiceApp.java
+++ src/main/java/faang/school/notificationservice/NotificationServiceApp.java
@@ -8,8 +8,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 @EnableFeignClients("faang.school.notificationservice.client")
 @EnableJpaRepositories(basePackages = "faang.school.notificationservice.repository")
 public class NotificationServiceApp {
