29396dd6ac4c1e0625fec67f689651f8f04d5f77
Dmitriy Goncharov <incoom@yandex.ru>
2024-04-16 00:38:14 +0500

    initial commit

---
+++ src/main/java/faang/school/notificationservice/listener/AbstractEventListener.java
@@ -0,0 +1,35 @@
+package faang.school.notificationservice.listener;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.service.MessageBuilder;
+import faang.school.notificationservice.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.Locale;
+
+@RequiredArgsConstructor
+public abstract class AbstractEventListener<T> {
+    protected final ObjectMapper objectMapper;
+    protected final UserServiceClient userServiceClient;
+    protected final List<NotificationService> notificationServices;
+    protected final List<MessageBuilder<T>> messageBuilders;
+
+    protected void sendNotification(long userId, String message) {
+        UserDto user = userServiceClient.getUser(userId);
+        notificationServices.stream()
+                .filter(service -> service.getPreferredContact().equals(user.getPreference()))
+                .findFirst()
+                .ifPresent(service -> service.send(user, message));
+    }
+
+    protected String getMessage(T event, Locale locale) {
+        return messageBuilders.stream()
+                .filter(builder -> builder.getEventType() == event.getClass())
+                .findFirst()
+                .map(builder -> builder.buildMessage(event, locale))
+                .orElseThrow(() -> new IllegalStateException("No message builder found for event type: " + event.getClass()));
+    }
+}
