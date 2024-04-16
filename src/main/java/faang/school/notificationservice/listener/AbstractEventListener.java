da27f4823ffc35148c5da62624402e71a1c1909c
Dmitriy Goncharov <incoom@yandex.ru>
2024-04-17 03:10:25 +0500

    changes after cross-review

--- src/main/java/faang/school/notificationservice/listener/AbstractEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/AbstractEventListener.java
@@ -6,16 +6,25 @@ import faang.school.notificationservice.dto.UserDto;
 import faang.school.notificationservice.messagebuilder.MessageBuilder;
 import faang.school.notificationservice.service.NotificationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.serializer.SerializationException;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
+@Slf4j
 @RequiredArgsConstructor
-public abstract class AbstractEventListener<T> {
+public abstract class AbstractEventListener<T> implements MessageListener {
     protected final ObjectMapper objectMapper;
     protected final UserServiceClient userServiceClient;
     protected final List<NotificationService> notificationServices;
     protected final List<MessageBuilder<T>> messageBuilders;
+    protected final Class<T> eventType;
+
+    protected abstract void publishMessage(T event);
 
     protected void sendNotification(long userId, String message) {
         UserDto user = userServiceClient.getUser(userId);
@@ -33,5 +42,16 @@ public abstract class AbstractEventListener<T> {
                 .findFirst()
                 .map(builder -> builder.buildMessage(event, locale))
                 .orElseThrow(() -> new IllegalStateException("No message builder found for event type: " + event.getClass()));
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        try {
+            T event = objectMapper.readValue(message.getBody(), eventType);
+            publishMessage(event);
+        } catch (IOException e) {
+            log.error("IOException was thrown", e);
+            throw new SerializationException("Failed to deserialized a message", e);
+        }
     }
 }
--- src/main/java/faang/school/notificationservice/listener/FollowerEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/FollowerEventListener.java
@@ -6,34 +6,24 @@ import faang.school.notificationservice.event.follower.FollowerEvent;
 import faang.school.notificationservice.messagebuilder.MessageBuilder;
 import faang.school.notificationservice.service.NotificationService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.connection.Message;
-import org.springframework.data.redis.connection.MessageListener;
-import org.springframework.data.redis.serializer.SerializationException;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
 @Slf4j
 @Component
-public class FollowerEventListener extends AbstractEventListener<FollowerEvent> implements MessageListener {
+public class FollowerEventListener extends AbstractEventListener<FollowerEvent> {
 
     public FollowerEventListener(ObjectMapper objectMapper,
                                  UserServiceClient userServiceClient,
                                  List<NotificationService> notificationServices,
                                  List<MessageBuilder<FollowerEvent>> messageBuilders) {
-        super(objectMapper, userServiceClient, notificationServices, messageBuilders);
+        super(objectMapper, userServiceClient, notificationServices, messageBuilders, FollowerEvent.class);
     }
 
     @Override
-    public void onMessage(Message message, byte[] pattern) {
-        try {
-            FollowerEvent event = objectMapper.readValue(message.getBody(), FollowerEvent.class);
-            sendNotification(event.getFollowerId(), getMessage(event, Locale.ENGLISH));
-        } catch (IOException e) {
-            log.error("IOException while deserializing FollowerEvent", e);
-            throw new SerializationException("Could not deserialize FollowerEvent", e);
-        }
+    protected void publishMessage(FollowerEvent event) {
+        sendNotification(event.getFolloweeId(), getMessage(event, Locale.ENGLISH));
     }
 }
