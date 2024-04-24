2f02cfa48b63c07d084b536773cd540556163e5d
DadyaVolodya <pl4stmass@yandex.ru>
2024-04-24 16:25:20 +0300

    developing

--- src/main/java/faang/school/notificationservice/client/MailConfig.java
+++ src/main/java/faang/school/notificationservice/client/MailConfig.java
@@ -1,5 +1,6 @@
 package faang.school.notificationservice.client;
 
+import org.json.Property;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,16 +13,16 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
-    @Value("${spring.mail.port}")  // Injects port from application.properties
+    @Value("${spring.mail.port}")
     private int port;
 
-    @Value("${spring.mail.host}")  // Injects host from application.properties
+    @Value("${spring.mail.host}")
     private String host;
 
-    @Value("${spring.mail.username}") // Injects username from application.properties
+    @Value("${spring.mail.username}")
     private String username;
 
-    @Value("${spring.mail.password}") // Injects password from application.properties (replace with your password)
+    @Value("${spring.mail.password}")
     private String password;
 
     @Bean
