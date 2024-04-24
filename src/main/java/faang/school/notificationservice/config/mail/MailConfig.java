39531e5713290b27072dad090a62fdba3212c4db
DadyaVolodya <pl4stmass@yandex.ru>
2024-04-24 17:12:07 +0300

    developing

--- src/main/java/faang/school/notificationservice/config/mail/MailConfig.java
+++ src/main/java/faang/school/notificationservice/config/mail/MailConfig.java
@@ -1,6 +1,5 @@
-package faang.school.notificationservice.client;
+package faang.school.notificationservice.config.mail;
 
-import org.json.Property;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,28 +23,21 @@ public class MailConfig {
 
     @Value("${spring.mail.password}")
     private String password;
-
-    @Value("${spring.mail.properties.mail.smtp.auth}")
-    private boolean smtpAuth;
-
-    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
-    private boolean startTlsEnabled;
-
+    
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost(host);
         mailSender.setPort(port);
-
         mailSender.setUsername(username);
         mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-
+        props.put("mail.smtp.starttls.connectiontimeout", "5000");
+        props.put("mail.smtp.starttls.timeout", "5000");
+        props.put("mail.smtp.starttls.writetimeout", "5000");
         return mailSender;
     }
 }
