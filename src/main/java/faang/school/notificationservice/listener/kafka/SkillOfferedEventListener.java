3654e3bcb5788f655a0ded8293dec568cc685da3
Bukevsky <bykaplay@gmail.com>
2024-04-26 13:06:49 +0300

    completed consumer notification

---
+++ src/main/java/faang/school/notificationservice/listener/kafka/SkillOfferedEventListener.java
@@ -0,0 +1,36 @@
+package faang.school.notificationservice.listener.kafka;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.SkillOfferedEvent;
+import faang.school.notificationservice.message.MessageBuilder;
+import faang.school.notificationservice.service.NotificationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import java.util.List;
+import java.util.Locale;
+
+@Configuration
+@Slf4j
+public class SkillOfferedEventListener extends AbstractEventListener<SkillOfferedEvent>{
+
+    public SkillOfferedEventListener(MessageBuilder<SkillOfferedEvent> messageBuilder, List<NotificationService> notificationServices, UserServiceClient userServiceClient, ObjectMapper objectMapper) {
+        super(messageBuilder, notificationServices, userServiceClient, objectMapper);
+    }
+
+    @KafkaListener(topics = "${spring.data.kafka.channels.skill-channel.name}", groupId = "${spring.data.kafka.group-id}")
+    public void listen(String message){
+        try {
+            SkillOfferedEvent event = objectMapper.readValue(message, SkillOfferedEvent.class);
+            sendNotification(event.getRecipientUserId(), getMessage(event, Locale.ENGLISH));
+            log.info(getMessage(event, Locale.ENGLISH));
+            log.info("Sending notification in listener for user: " + event.getRecipientUserId());
+        } catch (JsonProcessingException e) {
+            log.error("error in json mapping", e);
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/faang/school/notificationservice/message/SkillOfferedMessage.java
@@ -0,0 +1,29 @@
+package faang.school.notificationservice.message;
+
+import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.SkillOfferedEvent;
+import faang.school.notificationservice.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+@RequiredArgsConstructor
+public class SkillOfferedMessage implements MessageBuilder<SkillOfferedEvent> {
+    private final MessageSource messageSource;
+    private final UserServiceClient userServiceClient;
+
+    @Override
+    public String buildMessage(SkillOfferedEvent eventType, Locale locale) {
+        UserDto userDto = userServiceClient.getUser(eventType.getRecipientUserId());
+        String defaultMessage = messageSource.getMessage("skill.offered", new Object[]{userDto.getUsername(), eventType.getTitleSkill()}, locale);
+        return messageSource.getMessage("skill.offered", new Object[]{userDto.getUsername(), eventType.getTitleSkill()}, defaultMessage, locale);
+    }
+
+    @Override
+    public Class<?> supportsEventType() {
+        return SkillOfferedEvent.class;
+    }
+}
