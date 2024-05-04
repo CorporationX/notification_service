0f7ff8fb799e06a61a405c8fef6e55d91fdb987c
Sam Migachev <sammigachev@gmail.com>
2024-05-04 13:44:47 +0300

    correct

--- src/main/java/faang/school/notificationservice/config/RedisConfig.java
+++ src/main/java/faang/school/notificationservice/config/RedisConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -24,12 +25,11 @@ public class RedisConfig {
     private String host;
     @Value("${spring.data.redis.channel.recommendationRequest}")
     private String recommendationRequestChannel;
+
     private final RecommendationRequestEventListener recommendationRequestEventListener;
-    private final ObjectMapper objectMapper;
 
     @Bean
     public JedisConnectionFactory connectionFactory() {
-        System.out.println(port);
         RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host, port);
         return new JedisConnectionFactory(configuration);
     }
@@ -39,7 +39,7 @@ public class RedisConfig {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<Object>(objectMapper, Object.class));
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
     }
 
