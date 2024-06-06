ec9d374d481b912238ce7cb4379ec33f44841df0
ethiqque <danielpyld.work@gmail.com>
2024-06-06 09:38:10 +0300

    feature-BJS2-7410

---
+++ src/main/java/faang/school/notificationservice/config/redis/RedisConfig.java
@@ -0,0 +1,57 @@
+package faang.school.notificationservice.config.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.Topic;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.util.Pair;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        return new JedisConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(
+            RedisConnectionFactory connectionFactory,
+            List<Pair<Topic, MessageListenerAdapter>> channelAdapterPairList) {
+
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+
+        channelAdapterPairList.forEach(pair -> container.addMessageListener(pair.getSecond(), pair.getFirst()));
+
+        return container;
+    }
+}
---
+++ src/main/java/faang/school/notificationservice/service/email/EmailService.java
@@ -0,0 +1,42 @@
+package faang.school.notificationservice.service.email;
+
+import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EmailService implements NotificationService {
+
+    private final JavaMailSender javaMailSender;
+    @Value("${spring.mail.username}")
+    private String sendMail;
+
+    @Override
+    public UserDto.PreferredContact getPreferredContact() {
+        return UserDto.PreferredContact.EMAIL;
+    }
+
+    @Override
+    public void send(UserDto user, String message) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(sendMail);
+        mailMessage.setTo(user.getEmail());
+        mailMessage.setSubject("Notification");
+        mailMessage.setText(message);
+
+        try {
+            javaMailSender.send(mailMessage);
+            log.info("Message sent successfully to {}", user.getEmail());
+        } catch (Exception e) {
+            log.error("Failed to send message to {}: {}", user.getEmail(), e.getMessage());
+            throw new RuntimeException("Error during email sending");
+        }
+    }
+}
