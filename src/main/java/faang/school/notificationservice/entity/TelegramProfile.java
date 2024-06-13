90658ec8ec4080ce8ecf2fb3f8d9c7577a37faaf
Tem <tipokruto014@gmail.com>
2024-06-13 21:15:10 +0300

    small fix

--- src/main/java/faang/school/notificationservice/entity/TelegramProfile.java
+++ src/main/java/faang/school/notificationservice/entity/TelegramProfile.java
@@ -21,7 +21,7 @@ public class TelegramProfile {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
     @Column(name = "user_id", nullable = false)
     private long userId;
