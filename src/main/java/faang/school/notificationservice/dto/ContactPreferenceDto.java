dec4f3a6bfd2a9d19dfcab7d5e04ffa375d70bf8
Tem <tipokruto014@gmail.com>
2024-06-14 00:19:20 +0300

    small fix

---
+++ src/main/java/faang/school/notificationservice/dto/ContactPreferenceDto.java
@@ -0,0 +1,24 @@
+package faang.school.notificationservice.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ContactPreferenceDto {
+
+    private long id;
+
+    @NotNull(message = "UserId should not be null")
+    @Positive(message = "UserId should be positive")
+    private Long userId;
+
+    @NotNull(message = "Preference should not be null")
+    private UserDto.PreferredContact preference;
+}
