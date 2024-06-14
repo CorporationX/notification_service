11ce41e82a1c3d31035b4c6fa3477d5d444731ac
nadir <den4ik.strelok@gmail.com>
2024-06-14 22:07:35 +0300

    Изменил структуру классов MessageBuilder и AbstractEventListener

--- src/main/java/faang/school/notificationservice/listener/AbstractEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/AbstractEventListener.java
@@ -3,36 +3,25 @@ package faang.school.notificationservice.listener;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.notificationservice.client.UserServiceClient;
 import faang.school.notificationservice.dto.UserDto;
-import faang.school.notificationservice.exception.ListenerException;
 import faang.school.notificationservice.messaging.MessageBuilder;
 import faang.school.notificationservice.service.NotificationService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Locale;
-import java.util.Optional;
-
-import static faang.school.notificationservice.exception.message.ListenerExceptionMessage.MESSAGE_BUILDER_NOT_FOUND;
 
 @Component
-@Setter
+@Getter
 @RequiredArgsConstructor
 public abstract class AbstractEventListener<T> {
     private final ObjectMapper objectMapper;
     private final UserServiceClient userServiceClient;
     private final List<NotificationService> notificationServices;
-    private final List<MessageBuilder<T>> messageBuilders;
+    private final MessageBuilder<T> messageBuilder;
 
-    public String getMessage(Class<?> eventType, Locale userLocale, T event) {
-        Optional<MessageBuilder<T>> messageBuilder = messageBuilders.stream()
-                .filter(builder -> builder.getInstance().equals(eventType))
-                .findFirst();
-
-        return messageBuilder
-                .orElseThrow(() -> new ListenerException(MESSAGE_BUILDER_NOT_FOUND.getMessage()))
-                .buildMessage(event, userLocale);
+    public T mapMessageBodyToEvent(Object messageBody, Class<T> eventType) {
+        return objectMapper.convertValue(messageBody, eventType);
     }
 
     public void sendNotification(long userId, String message) {
--- src/test/java/faang/school/notificationservice/listener/AbstractEventListenerTest.java
+++ src/test/java/faang/school/notificationservice/listener/AbstractEventListenerTest.java
@@ -1,8 +1,8 @@
 package faang.school.notificationservice.listener;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.notificationservice.client.UserServiceClient;
 import faang.school.notificationservice.dto.UserDto;
-import faang.school.notificationservice.exception.ListenerException;
 import faang.school.notificationservice.messaging.MessageBuilder;
 import faang.school.notificationservice.service.NotificationService;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,18 +14,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 import java.util.Locale;
-import java.util.Random;
 
 import static faang.school.notificationservice.dto.UserDto.PreferredContact.SMS;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -35,55 +30,43 @@ class AbstractEventListenerTest {
     @Mock
     private UserServiceClient userServiceClient;
     private List<NotificationService> notificationServices;
-    private List<MessageBuilder<Object>> messageBuilders;
+    private ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp() {
         notificationServices = List.of(mock(NotificationService.class));
-        MessageBuilder<Object> messageBuilder = mock(TestMessageBuilder.class);
-        messageBuilders = List.of(messageBuilder);
+        objectMapper = mock(ObjectMapper.class);
 
-        abstractEventListener = spy(new NotSoAbstractEventListener(userServiceClient,
+        abstractEventListener = spy(new NotSoAbstractEventListener(objectMapper,
+                userServiceClient,
                 notificationServices,
-                messageBuilders));
+                null));
     }
 
     static class NotSoAbstractEventListener extends AbstractEventListener<Object> {
-        public NotSoAbstractEventListener(UserServiceClient userServiceClient,
+        public NotSoAbstractEventListener(ObjectMapper objectMapper,
+                                          UserServiceClient userServiceClient,
                                           List<NotificationService> notificationServices,
-                                          List<MessageBuilder<Object>> messageBuilders) {
-            super(null, userServiceClient, notificationServices, messageBuilders);
+                                          MessageBuilder<Object> messageBuilder) {
+            super(objectMapper, userServiceClient, notificationServices, messageBuilder);
         }
     }
 
     static class TestMessageBuilder implements MessageBuilder<Object> {
-
-        @Override
-        public Class<?> getInstance() {
-            return null;
-        }
-
         @Override
         public String buildMessage(Object event, Locale locale) {
-            return null;
+            return "";
         }
     }
 
     @Nested
     class PositiveTests {
         @Test
-        void getMessage() {
-            Class<?> eventType = Object.class;
-            String message = "Message";
+        void mapMessageBodyToEventTest() {
+            Object messageBody = new Object();
+            abstractEventListener.mapMessageBodyToEvent(messageBody, Object.class);
 
-            messageBuilders.forEach(builder -> {
-                doReturn(eventType).when(builder).getInstance();
-                when(builder.buildMessage(any(), any(Locale.class))).thenReturn(message);
-            });
-
-            String actualResult = abstractEventListener.getMessage(eventType, Locale.CHINA, new Object());
-
-            assertEquals(message, actualResult);
+            verify(objectMapper).convertValue(messageBody, Object.class);
         }
 
         @Test
@@ -101,20 +84,6 @@ class AbstractEventListenerTest {
             abstractEventListener.sendNotification(1L, message);
 
             notificationServices.forEach(notificationService -> verify(notificationService).send(any(UserDto.class), eq(message)));
-        }
-    }
-
-    @Nested
-    class NegativeTests {
-        @Test
-        void getMessage() {
-            messageBuilders.forEach(builder -> doReturn(Object.class).when(builder).getInstance());
-
-            assertThrows(ListenerException.class, () -> abstractEventListener.getMessage(Random.class, Locale.CHINA, List.of()));
-
-            messageBuilders.forEach(builder -> {
-                verify(builder, times(0)).buildMessage(any(Locale.class), any());
-            });
         }
     }
 }
