5d9c85068981ff56b070ecea6dbe51ba7dd533e5
Ilya <ilya-ermakov.2003@yandex.ru>
2024-06-20 21:41:06 +0300

    изменение конфига

--- src/main/java/faang/school/notificationservice/config/redis/RedisConfig.java
+++ src/main/java/faang/school/notificationservice/config/redis/RedisConfig.java
@@ -1,47 +1,53 @@
 package faang.school.notificationservice.config.redis;
 
 import faang.school.notificationservice.listener.RecommendationRequestedEventListener;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
+
+@Setter
 @Configuration
+@RequiredArgsConstructor
+@ConfigurationProperties(prefix = "spring.data.redis")
 public class RedisConfig {
-
-    @Value("${spring.data.redis.port}")
-    private int redisPort;
-    @Value("${spring.data.redis.host}")
-    private String redisHost;
-    @Value("${spring.data.redis.channel.recommendation}")
-    private String recommendationChannel;
+    private int port;
+    private String host;
+    private Channels channel;
+    private final RecommendationRequestedEventListener recommendationRequestedEventListener;
 
     @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
-        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(redisHost, redisPort);
-        return new JedisConnectionFactory(redisConfig);
+    RedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        return new JedisConnectionFactory(config);
     }
 
     @Bean
-    MessageListenerAdapter recommendationListener(RecommendationRequestedEventListener requestedEventListener) {
-        return new MessageListenerAdapter(requestedEventListener);
-    }
+    RedisMessageListenerContainer redisContainer() {
+        var messageListenerAdapter = new MessageListenerAdapter(recommendationRequestedEventListener);
+        var container = new RedisMessageListenerContainer();
 
-    @Bean
-    ChannelTopic recommendationTopic() {
-        return new ChannelTopic(recommendationChannel);
-    }
-
-    @Bean
-    RedisMessageListenerContainer redisContainer(MessageListenerAdapter recommendationListener) {
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(jedisConnectionFactory());
-        container.addMessageListener(recommendationListener, recommendationTopic());
+        container.addMessageListener(messageListenerAdapter, createTopic(channel.getRecommendation()));
 
         return container;
+    }
+
+    private ChannelTopic createTopic(String topicName) {
+        return new ChannelTopic(topicName);
+    }
+
+    @Data
+    private static class Channels {
+        private String recommendation;
     }
 }
