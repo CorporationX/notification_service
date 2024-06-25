df1f7f360163ac72a9d7098608ec6b38a43e9184
ethiqque <danielpyld.work@gmail.com>
2024-06-25 19:45:25 +0300

    Фундамент

---
+++ src/main/java/faang/school/notificationservice/listener/profile/ProfileViewListener.java
@@ -0,0 +1,36 @@
+package faang.school.notificationservice.listener.profile;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.event.profile.ProfileViewEvent;
+import faang.school.notificationservice.listener.AbstractEventListener;
+import faang.school.notificationservice.messaging.MessageBuilder;
+import faang.school.notificationservice.service.NotificationService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.errors.SerializationException;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Locale;
+
+@Slf4j
+@Component
+public class ProfileViewListener extends AbstractEventListener<ProfileViewEvent> {
+
+    public ProfileViewListener(MessageBuilder<ProfileViewEvent> messageBuilder, List<NotificationService> notificationServices, UserServiceClient userServiceClient, ObjectMapper objectMapper) {
+        super(messageBuilder, notificationServices, userServiceClient, objectMapper);
+    }
+
+    @KafkaListener(topics = "${spring.data.kafka.channel.profile.view.name}", groupId = "${spring.data.kafka.group-id}")
+    public void listen(String event) {
+        try {
+            ProfileViewEvent profileViewEvent = objectMapper.readValue(event, ProfileViewEvent.class);
+            sendNotification(profileViewEvent.getUserId(), getMessage(profileViewEvent, Locale.ENGLISH));
+        } catch (JsonProcessingException e) {
+            throw new SerializationException(e);
+        }
+    }
+}
+
---
+++ src/main/java/faang/school/notificationservice/messaging/ProfileViewEventMessageBuilder.java
@@ -0,0 +1,30 @@
+package faang.school.notificationservice.messaging;
+
+import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.event.profile.ProfileViewEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+@RequiredArgsConstructor
+public class ProfileViewEventMessageBuilder implements MessageBuilder<ProfileViewEvent> {
+
+    private final UserServiceClient userServiceClient;
+    private final MessageSource messageSource;
+
+    @Override
+    public String buildMessage(ProfileViewEvent event, Locale locale) {
+        UserDto viewer = userServiceClient.getUser(event.getUserId());
+        String defaultMessage = messageSource.getMessage("profile.view", new Object[]{viewer.getUsername()}, Locale.ENGLISH);
+        return messageSource.getMessage("profile.view", new Object[]{viewer.getUsername()}, defaultMessage, locale);
+    }
+
+    @Override
+    public Class<?> getInstance() {
+        return ProfileViewEvent.class;
+    }
+}
