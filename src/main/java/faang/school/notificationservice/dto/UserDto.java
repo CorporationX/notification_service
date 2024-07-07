56180dc6e2dc208024a6c42276787995fbead60e
Maxim Suhanov <maksimka_13.05.2004@mail.ru>
2024-07-07 22:43:43 +0400

    Resolved a conflict.

--- src/main/java/faang/school/notificationservice/dto/UserDto.java
+++ src/main/java/faang/school/notificationservice/dto/UserDto.java
@@ -2,13 +2,17 @@ package faang.school.notificationservice.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import faang.school.notificationservice.entity.PreferredContact;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.jackson.Jacksonized;
 
 @Data
 @Builder
 @Jacksonized
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDto {
     private long id;
