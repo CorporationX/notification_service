2fa3cfafce8be0a92e38b32ee06703e1c9e36595
Stanislav Ovsyankin <cancion2able@gmail.com>
2024-08-06 18:47:49 +0500

    BJS2-15903 # Удаление неиспользуемых импортов

--- src/test/java/faang/school/notificationservice/listener/AbstractEventListenerTest.java
+++ src/test/java/faang/school/notificationservice/listener/AbstractEventListenerTest.java
@@ -9,8 +9,6 @@ import faang.school.notificationservice.service.NotificationService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.redis.connection.Message;
