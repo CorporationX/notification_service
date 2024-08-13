791d61ea35d471217f27722436362a00537f6cbd
Stanislav Ovsyankin <cancion2able@gmail.com>
2024-08-13 18:51:07 +0500

    BJS2-15889 # Добавить конфигурацию топика и слушателя

--- src/main/java/faang/school/notificationservice/config/RedisConfig.java
+++ src/main/java/faang/school/notificationservice/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package faang.school.notificationservice.config;
 
+import faang.school.notificationservice.listener.mentorship.MentorshipAcceptedEventListener;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,7 +8,9 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -15,9 +18,10 @@ public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
     private String host;
-
     @Value("${spring.data.redis.port}")
     private int port;
+    @Value("${spring.data.redis.channel.mentorship}")
+    private String mentorshipChannelName;
 
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
@@ -34,10 +38,22 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisMessageListenerContainer redisContainer(RedisConnectionFactory redisConnectionFactory) {
+    public RedisMessageListenerContainer redisContainer(RedisConnectionFactory redisConnectionFactory,
+                                                        MentorshipAcceptedEventListener mentorshipAcceptedEventListener) {
         var container = new RedisMessageListenerContainer();
         container.setConnectionFactory(redisConnectionFactory);
+        container.addMessageListener(mentorshipAcceptedEventListener, mentorshipTopic());
         return container;
+    }
+
+    @Bean
+    public MessageListenerAdapter mentorshipAcceptedListenerAdapter(MentorshipAcceptedEventListener mentorshipAcceptedEventListener) {
+        return new MessageListenerAdapter(mentorshipAcceptedEventListener);
+    }
+
+    @Bean
+    public ChannelTopic mentorshipTopic() {
+        return new ChannelTopic(mentorshipChannelName);
     }
 
 }
