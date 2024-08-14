e8a1bbed212277390e352a3e90695f3435452114
Vladimir Haiduk <nightshade2903@gmail.com>
2024-08-14 17:28:38 +0300

    feature-BJS2-25762
    AbstractEventListener complete

--- src/main/java/faang/school/notificationservice/listener/AbstractEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/AbstractEventListener.java
@@ -2,24 +2,50 @@ package faang.school.notificationservice.listener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.UserDto;
 import faang.school.notificationservice.messaging.MessageBuilder;
 import faang.school.notificationservice.service.NotificationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
-import org.springframework.data.redis.connection.MessageListener;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
+@Slf4j
 @RequiredArgsConstructor
-public abstract class AbstractEventListener implements MessageListener {
+public abstract class AbstractEventListener<T> {
     private final ObjectMapper objectMapper;
     private final UserServiceClient userServiceClient;
     private final List<NotificationService> notificationServices;
-    private final List<MessageBuilder<?>> messageBuilders;
+    private final List<MessageBuilder<T>> messageBuilders;
 
+    protected void handleEvent(Message message, Class<T> type) {
+        try {
+            T event = objectMapper.readValue(message.getBody(), type);
+        } catch (IOException e) {
+            String msg = "Exception occurred while parsing event message";
+            log.error(msg, e);
+            throw new RuntimeException(e);
+        }
+    }
 
-    @Override
-    public void onMessage(Message message, byte[] pattern) {
+    protected String getMessage(T event, Locale userLocale) {
+        return messageBuilders.stream()
+                .filter(messageBuilder -> messageBuilder.getInstance() == event.getClass())
+                .findFirst()
+                .map(messageBuilder -> messageBuilder.buildMessage(event, userLocale))
+                .orElseThrow(() -> new IllegalStateException(String.format("No such event type:%s in message builder.",
+                        event.getClass().getName())));
+    }
 
+    protected void sendNotification(long userId, String message) {
+        UserDto user = userServiceClient.getUser(userId);
+        notificationServices.stream()
+                .filter(service -> service.getPreferredContact() == user.getPreference())
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("No such preference contact"))
+                .send(user, message);
     }
 }
