3c0c6fb6ca6e7ed9362721aeead17092a63601bc
Vladimir Haiduk <nightshade2903@gmail.com>
2024-08-15 20:30:52 +0300

    feature-BJS2-25807
    Create JavaMailSenderConfiguration
    Create EmailService

---
+++ src/main/java/faang/school/notificationservice/service/email/EmailService.java
@@ -0,0 +1,27 @@
+package faang.school.notificationservice.service.email;
+
+import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService implements NotificationService {
+    private final JavaMailSender javaMailSender;
+
+    @Override
+    public void send(UserDto user, String message) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setText(message);
+        simpleMailMessage.setTo(user.getEmail());
+        javaMailSender.send(simpleMailMessage);
+    }
+
+    @Override
+    public UserDto.PreferredContact getPreferredContact() {
+        return UserDto.PreferredContact.EMAIL;
+    }
+}
