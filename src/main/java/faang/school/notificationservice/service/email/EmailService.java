230a5fe6892881f1cccde1c449139141dc1a4877
shade2903 <nightshade2903@gmail.com>
2024-08-16 10:40:43 +0300

    feature-BJS2-25807
    fix bag in EmailService

--- src/main/java/faang/school/notificationservice/service/email/EmailService.java
+++ src/main/java/faang/school/notificationservice/service/email/EmailService.java
@@ -3,6 +3,7 @@ package faang.school.notificationservice.service.email;
 import faang.school.notificationservice.dto.UserDto;
 import faang.school.notificationservice.service.NotificationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,10 +12,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EmailService implements NotificationService {
     private final JavaMailSender javaMailSender;
+    @Value("${spring.mail.username}")
+    private String emailFrom;
+
+    private final String SUBJECT_MESSAGE = "Notification service";
+
 
     @Override
     public void send(UserDto user, String message) {
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setSubject(SUBJECT_MESSAGE);
+        simpleMailMessage.setFrom(emailFrom);
         simpleMailMessage.setText(message);
         simpleMailMessage.setTo(user.getEmail());
         javaMailSender.send(simpleMailMessage);
