eb3c44a30d60acb4e6661998937e7a5231cceb3a
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-08-18 23:32:49 +0300

    feat: add implementations `MessageBuilder` which create text of notifications

---
+++ src/main/java/faang/school/notificationservice/messaging/achievement/AchievementReceiveMessageBuilder.java
@@ -0,0 +1,40 @@
+package faang.school.notificationservice.messaging.achievement;
+
+import faang.school.notificationservice.client.AchievementServiceClient;
+import faang.school.notificationservice.dto.achievement.AchievementDto;
+import faang.school.notificationservice.event.achievement.AchievementEvent;
+import faang.school.notificationservice.messaging.MessageBuilder;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+@RequiredArgsConstructor
+public class AchievementReceiveMessageBuilder implements MessageBuilder<AchievementEvent> {
+
+    private final AchievementServiceClient achievementServiceClient;
+    private final MessageSource source;
+
+    @Override
+    public Class<?> getInstance() {
+        return null;
+    }
+
+    @Override
+    public String buildMessage(AchievementEvent event, Locale locale) {
+
+        Long achievementId = event.getAchievementId();
+        AchievementDto achievement = achievementServiceClient.getAchievement(achievementId);
+
+        String message = source.getMessage(
+                "achievement.receive",
+                new Object[]{achievement.getTitle()},
+                locale
+        );
+
+        return message;
+    }
+}
