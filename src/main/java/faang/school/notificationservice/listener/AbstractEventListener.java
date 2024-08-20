00675ffdf00dc1de817c3c1f0ef17c5d302f404a
Grisha Tkach <grisatkac@mail.ru>
2024-08-20 23:42:01 +0300

    add abstract event listener

---
+++ src/main/java/faang/school/notificationservice/listener/AbstractEventListener.java
@@ -0,0 +1,47 @@
+package faang.school.notificationservice.listener;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.service.messaging.MessageBuilder;
+import faang.school.notificationservice.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.connection.Message;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+@RequiredArgsConstructor
+public abstract class AbstractEventListener<T> {
+
+    protected final ObjectMapper objectMapper;
+    protected final UserServiceClient userServiceClient;
+    private final List<MessageBuilder<T>> messageBuilders;
+    private final List<NotificationService> notificationServices;
+
+    protected T handleEvent(Class<T> type, Message message) {
+        try {
+            return objectMapper.readValue(message.getBody(), type);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected String getMessage(T event, Locale locale) {
+        return messageBuilders.stream()
+                .filter(messageBuilder -> messageBuilder.supportEventType(event))
+                .findFirst()
+                .map(messageBuilder -> messageBuilder.getMessage(event, locale))
+                .orElseThrow(() -> new IllegalArgumentException("No message found for the given event type " + event.getClass().getName()));
+    }
+
+    protected void sendNotification(Long id, String message) {
+        UserDto user = userServiceClient.getUser(id);
+        notificationServices.stream()
+                .filter(service -> service.getPreferredContact().equals(user.getPreference()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(String.format("User %s doesn't have available notification service", id)))
+                .send(user, message);
+    }
+}
