3395d477a6c04208169af48e4e3451f3887323c6
shade2903 <nightshade2903@gmail.com>
2024-08-21 12:48:53 +0300

    feature-BJS2-25807
    add Validation Annotation in UserDto;

--- src/main/java/faang/school/notificationservice/service/email/EmailService.java
+++ src/main/java/faang/school/notificationservice/service/email/EmailService.java
@@ -2,7 +2,6 @@ package faang.school.notificationservice.service.email;
 
 import faang.school.notificationservice.dto.UserDto;
 import faang.school.notificationservice.service.NotificationService;
-import faang.school.notificationservice.validator.EmailServiceValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
@@ -13,14 +12,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EmailService implements NotificationService {
     private final JavaMailSender javaMailSender;
-    private final EmailServiceValidator emailServiceValidator;
     @Value("${spring.mail.username}")
     private String emailFrom;
     private final String SUBJECT_MESSAGE = "Notification service";
 
     @Override
     public void send(UserDto user, String message) {
-        emailServiceValidator.validationParam(user, message);
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setSubject(SUBJECT_MESSAGE);
         simpleMailMessage.setFrom(emailFrom);
