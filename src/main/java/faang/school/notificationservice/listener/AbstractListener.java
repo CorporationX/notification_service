332d7d440723509f7ace64adc23f359a6e8075f6
Andrei Kuzmin <Andreytko@mail.ru>
2024-08-29 18:20:39 +0300

    working on listener

--- src/main/java/faang/school/notificationservice/listener/AbstractListener.java
+++ src/main/java/faang/school/notificationservice/listener/AbstractListener.java
@@ -1,29 +1,43 @@
 package faang.school.notificationservice.listener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.messaging.MessageBuilder;
+import faang.school.notificationservice.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 
 import java.io.IOException;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
 public abstract class AbstractListener<T> implements MessageListener {
     protected final ObjectMapper objectMapper;
-
-
-    protected abstract T handleEvent(Message message) throws IOException;
+    private final UserServiceClient userServiceClient;
+    private final List<NotificationService> notificationServices;
+    private final MessageBuilder<T> messageBuilder;
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
         T event;
         try {
-            event = handleEvent(message);
+            event = objectMapper.readValue(message.getBody(), eventType);
         } catch (IOException e) {
             throw new RuntimeException(e + "couldn't deserialize message");
         }
+    }
 
+    public void sendNotification(long userId, String message) {
+        UserDto user = userServiceClient.getUser(userId);
+        if (user.getPreference() == null) {
+            log.warn("User {} has no preference", userId);
+            return;
+        }
+        notificationServices.stream().filter(service -> service.getPreferredContact().equals(user.getPreference()))
+                .forEach(service -> service.send(user, message));
     }
 }
--- src/main/java/faang/school/notificationservice/listener/CommentEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/CommentEventListener.java
@@ -1,22 +1,23 @@
 package faang.school.notificationservice.listener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.notificationservice.client.UserServiceClient;
 import faang.school.notificationservice.dto.CommentEvent;
-import org.springframework.data.redis.connection.Message;
+import faang.school.notificationservice.messaging.CommentEventMessageBuilder;
+import faang.school.notificationservice.service.NotificationService;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
+import java.util.List;
 
 @Component
 public class CommentEventListener extends AbstractListener<CommentEvent> {
 
-    public CommentEventListener(ObjectMapper objectMapper) {
-        super(objectMapper);
+    public CommentEventListener(ObjectMapper objectMapper,
+                                UserServiceClient userServiceClient,
+                                List<NotificationService> notificationServices,
+                                CommentEventMessageBuilder commentEventMessageBuilder) {
+        super(objectMapper, userServiceClient, notificationServices, commentEventMessageBuilder);
     }
 
-    @Override
-    public CommentEvent handleEvent(Message message) throws IOException {
-        return objectMapper.readValue(message.getBody(), CommentEvent.class);
-    }
 
 }
