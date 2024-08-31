e87ac6ce867133b14ef9a6c79594b5a40be7f31a
DonMaxim <donmaxim1990@gmail.com>
2024-08-31 12:45:44 -0300

    Try to fix CI error

--- src/test/java/faang/school/notificationservice/messaging/LikeMessageBuilderTest.java
+++ src/test/java/faang/school/notificationservice/messaging/LikeMessageBuilderTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 
 import java.text.MessageFormat;
 import java.util.Locale;
@@ -16,6 +19,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
 public class LikeMessageBuilderTest {
+    @MockBean
+    private LettuceConnectionFactory mockConnectionFactory;
+    @MockBean
+    private RedisMessageListenerContainer mockListenerContainer;
     @Autowired
     private LikeMessageBuilder messageBuilder;
     private String receiverName = "Viet";
