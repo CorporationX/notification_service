b37bc9529fc55092f629c8ba2e5234ba4fbaabe4
Rauan Kaztaev <rtishtyk@gmail.com>
2024-09-01 20:16:59 +0500

    minor fix

--- src/main/java/faang/school/notificationservice/messaging/comment/CommentMessageBuilder.java
+++ src/main/java/faang/school/notificationservice/messaging/comment/CommentMessageBuilder.java
@@ -1,16 +1,12 @@
 package faang.school.notificationservice.messaging.comment;
 
-import faang.school.notificationservice.client.UserServiceClient;
 import faang.school.notificationservice.dto.UserDto;
 import faang.school.notificationservice.event.comment.CommentEvent;
-import faang.school.notificationservice.exception.ExceptionMessages;
-import faang.school.notificationservice.exception.feign.UserServiceException;
 import faang.school.notificationservice.messaging.MessageBuilder;
-import feign.FeignException;
+import faang.school.notificationservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClientException;
 
 import java.util.Locale;
 
@@ -18,8 +14,7 @@ import java.util.Locale;
 @RequiredArgsConstructor
 public class CommentMessageBuilder implements MessageBuilder<CommentEvent> {
     private final MessageSource messageSource;
-    private final UserServiceClient userServiceClient;
-
+    private final UserService userService;
 
     @Override
     public Class<CommentEvent> getInstance() {
@@ -28,17 +23,7 @@ public class CommentMessageBuilder implements MessageBuilder<CommentEvent> {
 
     @Override
     public String buildMessage(CommentEvent event, Locale locale) {
-        UserDto userDto = fetchUser(event.getAuthorId());
+        UserDto userDto = userService.fetchUser(event.getAuthorId());
         return messageSource.getMessage("comment.new", new Object[]{userDto.getUsername()}, locale);
-    }
-
-    private UserDto fetchUser(Long id) {
-        UserDto user;
-        try {
-            user = userServiceClient.getUser(id);
-        } catch (FeignException | RestClientException exception) {
-            throw new UserServiceException(String.format(ExceptionMessages.USER_DATA_FETCH_FAILURE, id), exception);
-        }
-        return user;
     }
 }
