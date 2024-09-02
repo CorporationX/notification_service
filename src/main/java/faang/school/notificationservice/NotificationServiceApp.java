e048abed4769f1163129930403cfb3be8bea9249
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-09-03 00:16:42 +0500

    Исправил код

--- src/main/java/faang/school/notificationservice/NotificationServiceApp.java
+++ src/main/java/faang/school/notificationservice/NotificationServiceApp.java
@@ -3,12 +3,10 @@ package faang.school.notificationservice;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
-//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
 @EnableFeignClients("faang.school.notificationservice.client")
 public class NotificationServiceApp {
