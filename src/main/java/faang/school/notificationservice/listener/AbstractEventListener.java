1512983f4b952cf4a7d1874dd6252f3198a74c20
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-09-03 00:36:49 +0300

    remove: delete old version of `AbstractEventListener`

--- src/main/java/faang/school/notificationservice/listener/AbstractEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/AbstractEventListener.java
@@ -10,7 +10,6 @@ import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
@@ -55,39 +54,3 @@ public abstract class AbstractEventListener<E> implements MessageListener {
         return preferred == notification.getPreferredContact();
     }
 }
-
-// import faang.school.notificationservice.client.UserServiceClient;
-// import faang.school.notificationservice.dto.UserDto;
-// import faang.school.notificationservice.messaging.MessageBuilder;
-// import faang.school.notificationservice.service.NotificationService;
-// import lombok.RequiredArgsConstructor;
-
-// import java.util.List;
-// import java.util.Locale;
-
-
-// @RequiredArgsConstructor
-// public abstract class AbstractEventListener<T> {
-//     protected final ObjectMapper objectMapper;
-//     protected final UserServiceClient userServiceClient;
-//     private final List<NotificationService> notificationServiceList;
-//     private final List<MessageBuilder<T>> messageBuilderList;
-
-//     protected String getMessage(T event, Locale userLocale, Object[] args) {
-//         return messageBuilderList.stream()
-//                 .filter(messageBuilder -> messageBuilder.getInstance().equals(event.getClass()))
-//                 .findFirst()
-//                 .orElseThrow(() -> new IllegalArgumentException("MessageBuilder with " + event.getClass() + " type not found"))
-//                 .buildMessage(event, userLocale, args);
-//     }
-
-//     protected void sendNotification(long userId, String message) {
-//         UserDto user = userServiceClient.getUser(userId);
-//         notificationServiceList.stream()
-//                 .filter(notificationService -> notificationService.getPreferredContact() == user.getPreference())
-//                 .findFirst()
-//                 .orElseThrow(() -> new IllegalArgumentException("NotificationService with preferred user contact " + user.getPreference() + " not found"))
-//                 .send(user, message);
-
-//     }
-// }
