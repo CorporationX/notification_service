7753823d9fd4889d18f692fe70573781efb4f409
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-09-06 23:19:07 +0500

    Удалил лишний класс

--- src/main/java/faang/school/notificationservice/exception/notification/TelegramBotInitializationException.java
+++
@@ -1,7 +0,0 @@
-package faang.school.notificationservice.exception.notification;
-
-public class TelegramBotInitializationException extends RuntimeException{
-    public TelegramBotInitializationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
