49bd7b0d972e9b822532103bc08e187825853277
grisatkac <grisatkac@mail.ru>
2024-09-08 23:38:59 +0300

    update dto

--- src/main/java/faang/school/notificationservice/dto/UserDto.java
+++ src/main/java/faang/school/notificationservice/dto/UserDto.java
@@ -2,6 +2,8 @@ package faang.school.notificationservice.dto;
 
 import lombok.Data;
 
+import java.util.Locale;
+
 @Data
 public class UserDto {
     private long id;
@@ -9,6 +11,7 @@ public class UserDto {
     private String email;
     private String phone;
     private PreferredContact preference;
+    private Locale locale;
 
     public enum PreferredContact {
         EMAIL, SMS, TELEGRAM
