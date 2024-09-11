779c68e582a90f17bda76644cca057cd9a2c7c89
Artem <xotabggg@gmail.com>
2024-09-11 23:52:18 +0700

    feature-BJS2-15714

---
+++ src/main/java/faang/school/notificationservice/listener/ProfileViewEventListener.java
@@ -0,0 +1,34 @@
+package faang.school.notificationservice.listener;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.event.ProfileViewEvent;
+import faang.school.notificationservice.messaging.MessageBuilder;
+import faang.school.notificationservice.service.NotificationService;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Locale;
+
+@Component
+public class ProfileViewEventListener extends AbstractEventListener<ProfileViewEvent> implements MessageListener {
+
+    public ProfileViewEventListener(ObjectMapper objectMapper,
+                                    UserServiceClient userServiceClient,
+                                    List<NotificationService> notificationServices,
+                                    List<MessageBuilder<ProfileViewEvent>> messageBuilders) {
+        super(objectMapper, userServiceClient, messageBuilders, notificationServices);
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        handleEvent(message, ProfileViewEvent.class, event -> {
+            String notificationMessage = getMessage(event, Locale.getDefault());
+            UserDto userDto = userServiceClient.getUser(event.getUserOwnerId());
+            sendNotification(userDto, notificationMessage);
+        });
+    }
+}
