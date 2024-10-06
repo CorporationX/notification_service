ee5a8da3d21c11c70730ead33ff4be9ecf89f77b
amilosh <alexander.milosh@softeq.com>
2024-10-06 23:43:49 +0200

    Исправил стиль кода в соответсвии с договорённостями в команде

--- src/main/java/faang/school/notificationservice/client/UserServiceClient.java
+++ src/main/java/faang/school/notificationservice/client/UserServiceClient.java
@@ -5,9 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", url = "${user-service.host}:${user-service.port}")
+@FeignClient(name = "user-service", url = "${user-service.url}")
 public interface UserServiceClient {
-
     @GetMapping("/users/{id}")
     UserDto getUser(@PathVariable long id);
 }
--- src/test/java/faang/school/notificationservice/NotificationServiceAppTests.java
+++ src/test/java/faang/school/notificationservice/NotificationServiceAppTests.java
@@ -3,7 +3,6 @@ package faang.school.notificationservice;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
 class NotificationServiceAppTests {
     @Test
     void contextLoads() {
