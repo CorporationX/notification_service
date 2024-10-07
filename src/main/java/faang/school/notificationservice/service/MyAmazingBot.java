6fe990f5855e4ebbc7b1993abc4f7ec6e4ef21c4
Ivan <spvanym@gmail.com>
2024-10-07 21:02:48 +0300

    старт

---
+++ src/main/java/faang/school/notificationservice/service/MyAmazingBot.java
@@ -0,0 +1,11 @@
+package faang.school.notificationservice.service;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+public class MyAmazingBot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotToken() {
+        return "";
+    }
+}
