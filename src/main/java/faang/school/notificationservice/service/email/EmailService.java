4295ac7a649b90b90b5a7af67c02fa375f5a14b6
Maksim Kononov <07maks7@mail.ru>
2024-10-09 01:25:04 +0300

    delete controller, modify code

--- src/main/java/faang/school/notificationservice/service/email/EmailService.java
+++ src/main/java/faang/school/notificationservice/service/email/EmailService.java
@@ -3,14 +3,21 @@ package faang.school.notificationservice.service.email;
 import faang.school.notificationservice.dto.UserDto;
 import faang.school.notificationservice.service.NotificationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.Locale;
+
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailService implements NotificationService {
     private final JavaMailSender mailSender;
+    private final MessageSource messageSource;
 
     @Override
     public UserDto.PreferredContact getPreferredContact() {
@@ -19,14 +26,19 @@ public class EmailService implements NotificationService {
 
     @Override
     public void send(UserDto user, String text) {
-        send(user, text, "subject");
+        send(user, text, "email.subject", Locale.ENGLISH);
     }
 
-    public void send(UserDto user, String text, String subject) {
+    public void send(UserDto user, String text, String subjectCode, Locale locale) {
+        String subject = messageSource.getMessage(subjectCode, null, locale);
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(user.getEmail());
         message.setSubject(subject);
         message.setText(text);
-        mailSender.send(message);
+        try {
+            mailSender.send(message);
+        }catch (MailException e){
+            log.error("Failed to send email to user: {}", user.getEmail(), e);
+        }
     }
 }
