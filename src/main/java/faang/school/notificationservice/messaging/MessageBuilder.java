a175c922eb0eebd5af204bb6240a72e56c765d91
Denis Panasenko <panasenkodenis1995@gmail.com>
2024-10-09 07:45:17 +0300

    BJS2-26735 done no tests

--- src/main/java/faang/school/notificationservice/messaging/MessageBuilder.java
+++ src/main/java/faang/school/notificationservice/messaging/MessageBuilder.java
@@ -3,8 +3,5 @@ package faang.school.notificationservice.messaging;
 import java.util.Locale;
 
 public interface MessageBuilder<T> {
-
-    Class<?> getInstance();
-
     String buildMessage(T event, Locale locale);
 }
