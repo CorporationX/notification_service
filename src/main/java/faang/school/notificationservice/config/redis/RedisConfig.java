7acec6fc9668b5f665b180ddb66824ea19e65b8a
Evgen <fragmatik.Oo@yandex.ru>
2024-10-11 11:43:29 +0300

    Отправка SMS сообщений - BJS2-26738 - решил конфликты

--- src/main/java/faang/school/notificationservice/config/redis/RedisConfig.java
+++ src/main/java/faang/school/notificationservice/config/redis/RedisConfig.java
@@ -1,15 +1,12 @@
 package faang.school.notificationservice.config.redis;
 
 import faang.school.notificationservice.listener.EventStartEventListener;
+import faang.school.notificationservice.listener.LikeEventListener;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import faang.school.notificationservice.listener.LikeEventListener;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -63,7 +60,7 @@ public class RedisConfig {
     }
 
     @Bean
-    MessageListenerAdapter eventStartListenerAdapter(){
+    MessageListenerAdapter eventStartListenerAdapter() {
         return new MessageListenerAdapter(eventStartEventListener);
     }
 
@@ -73,7 +70,7 @@ public class RedisConfig {
     }
 
     @Bean
-    public ChannelTopic eventStarter(){
+    public ChannelTopic eventStarter() {
         return new ChannelTopic(eventStarter);
     }
 
