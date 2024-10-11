5cc889b89d54b66dfaadc92078fc71bebe6232f0
Ivan Tsegelnyuk <tsegelnyuk.ivan@gmail.com>
2024-10-11 22:39:54 +0300

    BJS2-27145 Отправка Email сообщений

---
+++ src/main/java/faang/school/notificationservice/service/EmailService.java
@@ -0,0 +1,27 @@
+package faang.school.notificationservice.service;
+
+import faang.school.notificationservice.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class EmailService implements NotificationService {
+    private final JavaMailSender javaMailSender;
+
+    @Override
+    public void send(UserDto user, String message) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(user.getEmail());
+        simpleMailMessage.setSubject("Notification from BootCamp");
+        simpleMailMessage.setText(message);
+        javaMailSender.send(simpleMailMessage);
+    }
+
+    @Override
+    public UserDto.PreferredContact getPreferredContact() {
+        return UserDto.PreferredContact.EMAIL;
+    }
+}
