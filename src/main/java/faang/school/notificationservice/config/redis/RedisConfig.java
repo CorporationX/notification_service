7d207f8e5507caf0c22d3a503f5372ab9f194721
KarabasBarabbas <lkn_popov@mail.ru>
2024-10-14 18:02:28 +0300

    BJS2-27527 промежуточный коммит

--- src/main/java/faang/school/notificationservice/config/redis/RedisConfig.java
+++ src/main/java/faang/school/notificationservice/config/redis/RedisConfig.java
@@ -1,15 +1,51 @@
 package faang.school.notificationservice.config.redis;
 
+import faang.school.notificationservice.listener.FollowerEventListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.channel.follower}")
+    private String followerEventListenerTopicName;
+
+    @Value("${spring.data.redis.host")
+    private String host;
+
+    @Value("${${spring.data.redis.port}")
+    private int port;
+
+    @Autowired
+    private FollowerEventListener followerEventListener;
+
     @Bean(name = "followerTopic")
-    ChannelTopic followerTopic(@Value("${spring.data.redis..channel.follower}") String topicName) {
-        return new ChannelTopic(topicName);
+    ChannelTopic followerTopic() {
+        return new ChannelTopic(followerEventListenerTopicName);
+    }
+
+    @Bean
+    MessageListenerAdapter followerEventListener() {
+        return new MessageListenerAdapter(followerEventListener);
+    }
+
+    @Bean
+    RedisMessageListenerContainer redisContainer() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        RedisConnectionFactory factory = new JedisConnectionFactory(config);
+
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(factory);
+        container.addMessageListener(followerEventListener(), followerTopic());
+
+        return container;
     }
 }
--- src/main/java/faang/school/notificationservice/listener/FollowerEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/FollowerEventListener.java
@@ -1,18 +1,44 @@
 package faang.school.notificationservice.listener;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.redis.listener.ChannelTopic;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.FollowerEvent;
+import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Component;
+import java.util.List;
+
+import java.io.IOException;
+import java.util.Objects;
 
 @Component
-public class FollowerEventListener {
+@RequiredArgsConstructor
+@Slf4j
+public class FollowerEventListener implements MessageListener {
+    private final ObjectMapper objectMapper;
+    private final UserServiceClient userServiceClient;
+    private final List<NotificationService> notificationServices;
 
-    @Qualifier("followerTopic")
-    private ChannelTopic topic;
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        try {
+            FollowerEvent event = objectMapper.readValue(message.getBody(), FollowerEvent.class);
+            UserDto userDto = userServiceClient.getUser(event.followerId());
+            List<NotificationService> services = notificationServices.stream()
+                    .filter(notificationService -> Objects.equals(notificationService.getPreferredContact(), userDto.getPreference()))
+                    .toList();
+            NotificationService notificationService = services.get(0);
+            if (notificationService == null) {
+                throw new RuntimeException("Preferred notification method was not found");
+            }
 
-    public FollowerEventListener(@Qualifier("followerTopic") ChannelTopic topic) {
-        this.topic = topic;
+        } catch (IOException e) {
+            log.error("Error parsing from json: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
-
-
 }
