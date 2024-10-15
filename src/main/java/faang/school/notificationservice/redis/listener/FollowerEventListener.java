66123b0db4d1c14b76e34ab00acbe2a7b9471747
cemeh666 <semen-5555@mail.ru>
2024-10-15 18:23:00 +0300

    BJS2-27138 Уведомление о подписке правки по ревью

--- src/main/java/faang/school/notificationservice/redis/listener/FollowerEventListener.java
+++ src/main/java/faang/school/notificationservice/redis/listener/FollowerEventListener.java
@@ -2,10 +2,15 @@ package faang.school.notificationservice.redis.listener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.notificationservice.client.UserServiceClientMock;
+import faang.school.notificationservice.config.redis.listener.RedisContainerMessageListener;
 import faang.school.notificationservice.redis.event.FollowerEvent;
 import faang.school.notificationservice.messaging.MessageBuilder;
 import faang.school.notificationservice.service.NotificationService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.Topic;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -13,7 +18,10 @@ import java.util.Locale;
 
 @Slf4j
 @Component
-public class FollowerEventListener extends AbstractEventListener<FollowerEvent> {
+public class FollowerEventListener extends AbstractEventListener<FollowerEvent> implements RedisContainerMessageListener {
+    @Value("${spring.data.redis.channel-topics.follower-event.name}")
+    private String topicName;
+
     public FollowerEventListener(UserServiceClientMock userServiceClient,
                                  List<MessageBuilder<?>> messageBuilders,
                                  List<NotificationService> notificationServices,
@@ -26,5 +34,15 @@ public class FollowerEventListener extends AbstractEventListener<FollowerEvent> 
         MessageBuilder<FollowerEvent> messageBuilder = (MessageBuilder<FollowerEvent>) defineBuilder(FollowerEvent.class);
         String userMessage = messageBuilder.buildMessage(event, Locale.getDefault());
         sendNotification(event.followedId(), userMessage);
+    }
+
+    @Override
+    public MessageListenerAdapter getAdapter() {
+        return new MessageListenerAdapter(this);
+    }
+
+    @Override
+    public Topic getTopic() {
+        return new ChannelTopic(topicName);
     }
 }
