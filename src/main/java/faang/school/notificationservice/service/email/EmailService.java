1c4034989a12f0cc0867ba35df54610450d7e8ad
Ivan Tsegelnyuk <tsegelnyuk.ivan@gmail.com>
2024-10-16 21:48:53 +0300

    BJS2-27145 Отправка Email сообщений

--- src/main/java/faang/school/notificationservice/service/email/EmailService.java
+++ src/main/java/faang/school/notificationservice/service/email/EmailService.java
@@ -5,18 +5,19 @@ import faang.school.notificationservice.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
-@Component
+@Service
 public class EmailService implements NotificationService {
+    private static final String MESSAGE_SUBJECT = "Notification from BootCamp";
     private final JavaMailSender javaMailSender;
 
     @Override
     public void send(UserDto user, String message) {
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setTo(user.getEmail());
-        simpleMailMessage.setSubject("Notification from BootCamp");
+        simpleMailMessage.setSubject(MESSAGE_SUBJECT);
         simpleMailMessage.setText(message);
         javaMailSender.send(simpleMailMessage);
     }
