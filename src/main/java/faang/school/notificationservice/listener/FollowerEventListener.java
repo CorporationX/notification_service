6b05dbb0626162cecc554d0b22d8f62203261ea7
KarabasBarabbas <lkn_popov@mail.ru>
2024-10-16 17:31:47 +0300

    BJS2-27527 промежуточный коммит

--- src/main/java/faang/school/notificationservice/listener/FollowerEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/FollowerEventListener.java
@@ -1,19 +1,12 @@
 package faang.school.notificationservice.listener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import faang.school.notificationservice.bot.TelegramBot;
 import faang.school.notificationservice.client.UserServiceClient;
 import faang.school.notificationservice.dto.FollowerEvent;
 import faang.school.notificationservice.dto.UserDto;
-import faang.school.notificationservice.repository.TelegramContactRepository;
 import faang.school.notificationservice.service.NotificationService;
-import faang.school.notificationservice.service.telegram.TelegramContactService;
-import faang.school.notificationservice.service.telegram.TelegramService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.connection.Message;
-import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Component;
 import java.util.List;
 
@@ -21,13 +14,12 @@ import java.io.IOException;
 import java.util.Objects;
 
 @Component
-@RequiredArgsConstructor
 @Slf4j
-public class FollowerEventListener implements MessageListener {
-    private final ObjectMapper objectMapper;
-    private final UserServiceClient userServiceClient;
-    private final List<NotificationService> notificationServices;
-    private final TelegramService notificationService1;
+public class FollowerEventListener extends AbstractEventListener {
+
+    public FollowerEventListener(ObjectMapper objectMapper, UserServiceClient userServiceClient, List<NotificationService> notificationServices) {
+        super(objectMapper, userServiceClient, notificationServices);
+    }
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
