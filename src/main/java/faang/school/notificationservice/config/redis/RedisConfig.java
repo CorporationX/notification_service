1745a15f19268a37cf479a55106381972bca7ea3
Maksim Kononov <07maks7@mail.ru>
2024-10-16 12:01:01 +0300

    fix code style 1

--- src/main/java/faang/school/notificationservice/config/redis/RedisConfig.java
+++ src/main/java/faang/school/notificationservice/config/redis/RedisConfig.java
@@ -1,29 +1,18 @@
 package faang.school.notificationservice.config.redis;
 
 import faang.school.notificationservice.listener.GoalCompletedEventListener;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-
-    @Value("${spring.data.redis.port}")
-    private int port;
-
-    @Value("${spring.data.redis.host}")
-    private String host;
-
-    @Value("${spring.data.redis.channel.goal}")
-    private String goalCompletedTopic;
 
     @Bean
     MessageListenerAdapter goalListenerAdapter(GoalCompletedEventListener goalCompletedEventListener) {
@@ -32,31 +21,21 @@ public class RedisConfig {
 
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        RedisStandaloneConfiguration configuration =
-                new RedisStandaloneConfiguration(host, port);
-        return new JedisConnectionFactory(configuration);
+        return new JedisConnectionFactory();
     }
 
     @Bean
     RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory,
-                                                 MessageListenerAdapter goalListenerAdapter) {
+                                                 MessageListenerAdapter goalListenerAdapter,
+                                                 @Qualifier("goalCompletedTopic") ChannelTopic goalCompletedTopic) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
-        container.addMessageListener(goalListenerAdapter, goalCompletedTopic());
+        container.addMessageListener(goalListenerAdapter, goalCompletedTopic);
         return container;
     }
 
-    @Bean
-    RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, String> template = new RedisTemplate<String, String>();
-        template.setConnectionFactory(connectionFactory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
-        return template;
-    }
-
-    @Bean
-    ChannelTopic goalCompletedTopic() {
-        return new ChannelTopic(goalCompletedTopic);
+    @Bean(value = "goalCompletedTopic")
+    ChannelTopic goalCompletedTopic(@Value("${spring.data.redis.channels.goal-channel.name}") String topic) {
+        return new ChannelTopic(topic);
     }
 }
