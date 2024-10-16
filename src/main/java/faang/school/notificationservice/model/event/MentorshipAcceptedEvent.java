527d41e39c6e5c38bedaa4cf8fed4ea52c72500d
Дмитрий Бахтин <dmitridorje@gmail.com>
2024-10-16 10:10:56 +0300

    BJS2-27317 - для события добавлена аннотация JsonIgnoreProperties

--- src/main/java/faang/school/notificationservice/model/event/MentorshipAcceptedEvent.java
+++ src/main/java/faang/school/notificationservice/model/event/MentorshipAcceptedEvent.java
@@ -1,10 +1,12 @@
 package faang.school.notificationservice.model.event;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MentorshipAcceptedEvent {
     Long requestId;
     Long requesterId;
