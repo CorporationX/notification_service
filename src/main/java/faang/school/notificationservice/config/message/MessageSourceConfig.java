beeaf51bce7424771e3eaec4a8d127ae408d935d
Ivan <spvanym@gmail.com>
2024-10-17 13:53:20 +0300

    Правки

--- src/main/java/faang/school/notificationservice/config/message/MessageSourceConfig.java
+++ src/main/java/faang/school/notificationservice/config/message/MessageSourceConfig.java
@@ -5,14 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
+import java.nio.charset.StandardCharsets;
+
 @Configuration
 public class MessageSourceConfig {
 
     @Bean
     public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages"); // Имя файла без расширения
-        messageSource.setDefaultEncoding("UTF-8"); // Кодировка
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
         return messageSource;
     }
 }
