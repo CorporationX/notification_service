0560a41c6f6a34beb9e1f5fdbb0625529537c201
Okabe <mathew.mackonoha@gmail.com>
2024-10-17 10:26:14 +0300

    Vonage зависимость в билде, смс сервис сделан, но не тестирован (пока, тесты напишу)

--- src/main/java/faang/school/notificationservice/service/impl/SmsServiceImpl.java
+++ src/main/java/faang/school/notificationservice/service/impl/SmsServiceImpl.java
@@ -5,29 +5,33 @@ import com.vonage.client.sms.MessageStatus;
 import com.vonage.client.sms.SmsSubmissionResponse;
 import com.vonage.client.sms.messages.TextMessage;
 import faang.school.notificationservice.service.SmsService;
+import faang.school.notificationservice.service.config.SmsConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class SmsServiceImpl implements SmsService {
-
-    private final VonageClient client;
-
-    public SmsServiceImpl() {
-        this.client = VonageClient.builder()
-                .apiKey("ea3cbdef")
-                .apiSecret("t6qqwTma9etW5JwR")
-                .build();
-    }
+    private final SmsConfig smsConfig;
 
     @Override
     public void sendSms(String to, String messageText) {
         TextMessage message = new TextMessage("Vonage APIs", to, messageText);
+        VonageClient client = smsConfig.vonageClient();
         SmsSubmissionResponse response = client.getSmsClient().submitMessage(message);
 
-        if (response.getMessages().get(0).getStatus() == MessageStatus.OK) {
-            System.out.println("Message sent successfully.");
+        if (response.getMessages() != null && !response.getMessages().isEmpty()) {
+            if (response.getMessages().get(0).getStatus() == MessageStatus.OK) {
+                log.info("Message sent successfully.");
+            } else {
+                log.error("Message failed with error: {}", response.getMessages().get(0).getErrorText());
+                throw new RuntimeException("Message failed with error: " + response.getMessages().get(0).getErrorText());
+            }
         } else {
-            System.out.println("Message failed with error: " + response.getMessages().get(0).getErrorText());
+            log.error("No messages were returned in the response.");
+            throw new RuntimeException("No messages were returned in the response.");
         }
     }
 }
