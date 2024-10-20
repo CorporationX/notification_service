746be752fc6012a152824e0af6709707eb34e36d
Denis Alginin <AlgininDK@gmail.com>
2024-10-20 18:30:50 +0500

    Внесенные изменения: 1. Регистрация бота в TelegramBotsApi 2. Пустой метод onUpdateReceived теперь выполняет функцию "эхо-бота"

--- src/main/java/faang/school/notificationservice/telegram/TelegramBot.java
+++ src/main/java/faang/school/notificationservice/telegram/TelegramBot.java
@@ -4,7 +4,9 @@ import faang.school.notificationservice.config.telegram.BotConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 @RequiredArgsConstructor
@@ -14,7 +16,28 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
 
+            String responseText;
+            if (messageText.equalsIgnoreCase("/start")) {
+                responseText = "Привет! Я твой Telegram бот.";
+            } else {
+                responseText = "Вы написали: " + messageText;
+            }
+
+            SendMessage sendMessage = SendMessage.builder()
+                    .chatId(String.valueOf(chatId))
+                    .text(responseText)
+                    .build();
+
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
