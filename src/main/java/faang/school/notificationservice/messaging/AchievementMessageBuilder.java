b03c644e16ced6f9e27d7772065ae2ad3a0e7be1
ifef228 <ifef228@gmail.com>
2024-10-20 11:30:17 +0300

    AchievementMessageBuilder create

---
+++ src/main/java/faang/school/notificationservice/messaging/AchievementMessageBuilder.java
@@ -0,0 +1,23 @@
+package faang.school.notificationservice.messaging;
+
+import faang.school.notificationservice.dto.AchievementEvent;
+import faang.school.notificationservice.dto.UserDto;
+import org.springframework.context.MessageSource;
+
+public class AchievementMessageBuilder extends MessageBuilder<AchievementEvent> {
+
+    public AchievementMessageBuilder(MessageSource messageSource) {
+        super(messageSource);
+    }
+
+    @Override
+    public Class<AchievementEvent> getInstance() {
+        return AchievementEvent.class;
+    }
+
+    @Override
+    public String buildMessage(UserDto userDto, AchievementEvent event) {
+        Object[] args = {userDto.getUsername(), event.getAchievementTitle()};
+        return messageSource.getMessage("achievement.new", args, userDto.getLocale());
+    }
+}
