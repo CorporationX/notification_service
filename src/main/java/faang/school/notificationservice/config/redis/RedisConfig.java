606fa1c11d5389380854ab34d17b6e30c5981ce0
Maksim Kononov <07maks7@mail.ru>
2024-10-20 13:43:27 +0300

    try resolve conflict

--- src/main/java/faang/school/notificationservice/config/redis/RedisConfig.java
+++ src/main/java/faang/school/notificationservice/config/redis/RedisConfig.java
@@ -1,12 +1,18 @@
 package faang.school.notificationservice.config.redis;
 
-import lombok.RequiredArgsConstructor;
-import faang.school.notificationservice.listener.LikeEventListener;
 import faang.school.notificationservice.listener.EventStartEventListener;
+import faang.school.notificationservice.listener.LikeEventListener;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -15,15 +21,17 @@ public class RedisConfig {
     @Value("${spring.data.redis.channels.comment-channel.name}")
     private String commentChannel;
 
-
-
+    @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
     MessageListenerAdapter eventListenerAdapter(EventStartEventListener eventStartEventListener) {
         return new MessageListenerAdapter(eventStartEventListener);
     }
 
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
     public ChannelTopic commentTopic() {
         return new ChannelTopic(commentChannel);
     }
@@ -47,17 +55,16 @@ public class RedisConfig {
     @Bean
     public RedisMessageListenerContainer container(MessageListenerAdapter commentMessageListenerAdapter,
                                                    MessageListenerAdapter likeListener,
-                                                   @Qualifier("likeEventTopic") ChannelTopic likeEventTopic) {
-    RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory,
-                                                 MessageListenerAdapter eventListenerAdapter,
-                                                 @Qualifier("eventTopic") ChannelTopic eventTopic) {
+                                                   MessageListenerAdapter eventListenerAdapter,
+                                                   @Qualifier("eventTopic") ChannelTopic eventTopic,
+                                                   @Qualifier("likeChannel") ChannelTopic likeEventTopic) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(jedisConnectionFactory());
         container.addMessageListener(commentMessageListenerAdapter, commentTopic());
         container.addMessageListener(likeListener, likeEventTopic);
-        container.setConnectionFactory(connectionFactory);
         container.addMessageListener(eventListenerAdapter, eventTopic);
-}
-}
+        return container;
+    }
 
     @Bean(value = "eventTopic")
     public ChannelTopic eventTopic(@Value("${spring.data.redis.channels.event-channel.name}") String topic) {
