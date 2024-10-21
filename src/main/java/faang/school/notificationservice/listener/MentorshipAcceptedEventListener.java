56592f82d4583b38b1af613065750c03b652c85e
SharkB38 <val.wolf.ne@gmail.com>
2024-10-22 03:28:35 +0900

    implement listener for mentorship request acceptation

---
+++ src/main/java/faang/school/notificationservice/listener/MentorshipAcceptedEventListener.java
@@ -0,0 +1,31 @@
+package faang.school.notificationservice.listener;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.MentorshipAcceptedEvent;
+import faang.school.notificationservice.messaging.MessageBuilder;
+import faang.school.notificationservice.service.NotificationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Locale;
+
+@Slf4j
+@Component("mentorshipAcceptedEventListener")
+public class MentorshipAcceptedEventListener extends AbstractEventListener<MentorshipAcceptedEvent> {
+    public MentorshipAcceptedEventListener(List<NotificationService> notificationServices,
+                                           List<MessageBuilder<MentorshipAcceptedEvent>> messageBuilders,
+                                           UserServiceClient userServiceClient,
+                                           ObjectMapper objectMapper) {
+        super(notificationServices, messageBuilders, userServiceClient, objectMapper);
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        MentorshipAcceptedEvent event = handleEvent(message, MentorshipAcceptedEvent.class);
+        String answerMessage = getMessage(event, Locale.UK);
+        sendNotification(event.menteeId(), answerMessage);
+    }
+}
