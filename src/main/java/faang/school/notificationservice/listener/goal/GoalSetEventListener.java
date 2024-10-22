fbd0e10129ed5716ed1ae70b791461c66973aa82
Sergei Kochkin <lowerKamaCase@gmail.com>
2024-10-22 08:05:33 +0300

    TESTgit add .! DO NOT MERGE IT

---
+++ src/main/java/faang/school/notificationservice/listener/goal/GoalSetEventListener.java
@@ -0,0 +1,39 @@
+package faang.school.notificationservice.listener.goal;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.goal.GoalSetEvent;
+import faang.school.notificationservice.dto.user.UserDto;
+import faang.school.notificationservice.listener.AbstractEventListener;
+import faang.school.notificationservice.messaging.MessageBuilder;
+import faang.school.notificationservice.service.NotificationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+import java.util.Map;
+
+@Slf4j
+@Component
+public class GoalSetEventListener extends AbstractEventListener<GoalSetEvent> {
+
+    public GoalSetEventListener(
+            ObjectMapper objectMapper,
+            UserServiceClient userServiceClient,
+            Map<Class<?>, MessageBuilder<?>> messageBuilders,
+            Map<UserDto.PreferredContact, NotificationService> notificationServices) {
+        super(objectMapper, userServiceClient, messageBuilders, notificationServices);
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        log.info("Got message, trying to handle it");
+        handleEvent(message, GoalSetEvent.class, event -> {
+            UserDto user = userServiceClient.getUser(event.getUserId());
+            Locale userPreferedLocale = user.getLocale() != null ? user.getLocale() : Locale.ENGLISH;
+            String text = getMessage(event, userPreferedLocale);
+            sendNotification(user, text);
+        });
+    }
+}
