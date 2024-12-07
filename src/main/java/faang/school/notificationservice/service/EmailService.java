58fe8e71d823aee9b4a92cf8eadf0caba55c81ff
Dmitry Shakhaev <dmtry.shahaev@gmail.com>
2024-12-07 12:49:53 +0300

    BJS2-41859 - Отправка Email сообщений

--- src/main/java/faang/school/notificationservice/service/EmailService.java
+++ src/main/java/faang/school/notificationservice/service/EmailService.java
@@ -21,7 +21,7 @@ public class EmailService implements NotificationService {
     public void send(UserDto user, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(user.getEmail());
-        message.setSubject("subject");
+        message.setSubject("For: " + user.getUsername());
         message.setText(text);
 
         emailSender.send(message);
