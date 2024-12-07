6afae598a3b0bbc7360363c51c74a154cf8e6abc
Dmytro Yevsieienko <youdima70@gmail.com>
2024-12-07 08:03:49 +0200

    Рефакторинг

--- src/main/java/faang/school/notificationservice/config/notification/EmailConfig.java
+++ src/main/java/faang/school/notificationservice/config/notification/EmailConfig.java
@@ -1,5 +1,6 @@
 package faang.school.notificationservice.config.notification;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +11,7 @@ import java.util.concurrent.Executors;
 public class EmailConfig {
 
     @Bean
-    public Executor mailExecutor() {
-        return Executors.newFixedThreadPool(10);
+    public Executor mailExecutor(@Value("${email-service.thread-pool-size}") int threadPoolSize) {
+        return Executors.newFixedThreadPool(threadPoolSize);
     }
 }
