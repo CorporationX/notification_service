a4753433d4f86317ed9735b1e2c84230ae0bcf93
Dmitry Shakhaev <dmtry.shahaev@gmail.com>
2024-12-08 13:34:39 +0300

    BJS2-41859 - Отправка Email сообщений (subject fix)

--- src/main/java/faang/school/notificationservice/service/EmailService.java
+++ src/main/java/faang/school/notificationservice/service/EmailService.java
@@ -2,6 +2,7 @@ package faang.school.notificationservice.service;
 
 import faang.school.notificationservice.dto.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,9 @@ public class EmailService implements NotificationService {
 
     private final JavaMailSender emailSender;
 
+    @Value("${company.name}")
+    private String companyName;
+
     @Override
     public UserDto.PreferredContact getPreferredContact() {
         return UserDto.PreferredContact.EMAIL;
@@ -21,7 +25,7 @@ public class EmailService implements NotificationService {
     public void send(UserDto user, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(user.getEmail());
-        message.setSubject("For: " + user.getUsername());
+        message.setSubject("For: " + user.getUsername() + " | From: " + companyName);
         message.setText(text);
 
         emailSender.send(message);
