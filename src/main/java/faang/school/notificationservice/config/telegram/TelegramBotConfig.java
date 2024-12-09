3092552bbb61e2860030b00be3995281daba8b3d
Egor Semenovskii <gaoqiao5217@gmail.com>
2024-12-09 20:25:40 +0300

    BJS2-41853 Отправка сообщений в Telegram

---
+++ src/main/java/faang/school/notificationservice/config/telegram/TelegramBotConfig.java
@@ -0,0 +1,33 @@
+package faang.school.notificationservice.config.telegram;
+
+import faang.school.notificationservice.service.telegram.TelegramBot;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Slf4j
+@Configuration
+@Getter
+@RequiredArgsConstructor
+public class TelegramBotConfig {
+
+    private final TelegramBot telegramBot;
+
+    @PostConstruct
+    public void createBot() {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(telegramBot);
+            log.info("Bot initial configuration set");
+        } catch (TelegramApiException e) {
+            log.error("Error while configuring Telegram Bot", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ src/main/java/faang/school/notificationservice/service/telegram/TelegramBot.java
@@ -0,0 +1,55 @@
+package faang.school.notificationservice.service.telegram;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TelegramBot extends TelegramLongPollingBot{
+
+    @Value("${spring.telegram-bot.name}")
+    private String name;
+
+    @Value("${spring.telegram-bot.token}")
+    private String token;
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+
+            SendMessage message = SendMessage.builder()
+                    .chatId(chat_id)
+                    .text(messageText)
+                    .build();
+
+            try {
+                execute(message);
+                log.info("Message was sent to user {} device!", message.getChatId());
+            } catch (TelegramApiException e) {
+                log.error("Error while sending telegram notification to user: {}", message.getChatId(), e);
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return name;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+}
