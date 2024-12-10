d499bcc87b7a5f68a6ac55137473c8c2a6966d79
Юрий <angelbes777@gmail.com>
2024-12-10 14:33:40 +0300

    Update LikeEventListener.java

--- src/main/java/faang/school/notificationservice/listener/LikeEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/LikeEventListener.java
@@ -6,9 +6,7 @@ import faang.school.notificationservice.data.PreferredContact;
 import faang.school.notificationservice.dto.LikeEvent;
 import faang.school.notificationservice.dto.UserDto;
 import faang.school.notificationservice.messaging.LikeMessageBuilder;
-import faang.school.notificationservice.service.EmailService;
-import faang.school.notificationservice.service.SmsService;
-import faang.school.notificationservice.service.TelegramService;
+import faang.school.notificationservice.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
@@ -16,17 +14,16 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Locale;
 
 @Component
 @RequiredArgsConstructor
 public class LikeEventListener implements MessageListener {
-    private final EmailService emailService;
-    private final TelegramService telegramService;
-    private final SmsService smsService;
     private final LikeMessageBuilder likeMessageBuilder;
     private final ObjectMapper objectMapper;
     private final UserServiceClient userServiceClient;
+    private final List<NotificationService> notificationServices;
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
@@ -41,11 +38,10 @@ public class LikeEventListener implements MessageListener {
             String preference = userServiceClient.getProfileSettings(event.getPostAuthorId()).getPreference();
             user.setPreference(PreferredContact.valueOf(preference));
 
-            switch (preference) {
-                case "TELEGRAM" -> telegramService.send(user, likeMessageBuilder.buildMessage(event, Locale.US));
-                case "SMS" -> smsService.send(user, likeMessageBuilder.buildMessage(event, Locale.US));
-                default -> emailService.send(user, likeMessageBuilder.buildMessage(event, Locale.US));
-            }
+            notificationServices.stream()
+                    .filter(service -> user.getPreference().equals(service.getPreferredContact()))
+                    .findFirst()
+                    .ifPresent(service -> service.send(user, likeMessageBuilder.buildMessage(event, Locale.US)));
         } catch (IOException e) {
             e.printStackTrace();
         }
