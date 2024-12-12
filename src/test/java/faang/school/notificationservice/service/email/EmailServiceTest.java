82a2f931415644046999c5bade7304dd8986ecb6
Alexey Zimin <zimin.alexey@gmail.com>
2024-12-13 01:41:17 +0300

    [BJS2-41899] - email sending (add package)

--- src/test/java/faang/school/notificationservice/service/email/EmailServiceTest.java
+++ src/test/java/faang/school/notificationservice/service/email/EmailServiceTest.java
@@ -1,7 +1,6 @@
-package faang.school.notificationservice;
+package faang.school.notificationservice.service.email;
 
 import faang.school.notificationservice.dto.UserDto;
-import faang.school.notificationservice.service.EmailService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
