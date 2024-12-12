757e025c8cc162300e761b8cf22a8698c42cc3ad
Alexey Zimin <zimin.alexey@gmail.com>
2024-12-13 01:41:17 +0300

    [BJS2-41899] - email sending (add package)

--- src/main/java/faang/school/notificationservice/service/TestController.java
+++ src/main/java/faang/school/notificationservice/service/TestController.java
@@ -1,6 +1,7 @@
 package faang.school.notificationservice.service;
 
 import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.service.email.EmailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
