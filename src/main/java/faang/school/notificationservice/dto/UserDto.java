d317449521451fecf28c893bcf2255f2d5b5b082
Egor Semenovskii <gaoqiao5217@gmail.com>
2024-12-12 16:23:24 +0300

    Conflict

--- src/main/java/faang/school/notificationservice/dto/UserDto.java
+++ src/main/java/faang/school/notificationservice/dto/UserDto.java
@@ -3,12 +3,10 @@ package faang.school.notificationservice.dto;
 import jakarta.validation.constraints.Email;
 import lombok.Builder;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Builder;
 import lombok.Data;
 
 @Builder
 @Data
-@Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDto {
     private long id;
