41da5e25da573cfb443e3fe4e1436e828c54cdce
bogda <bogdanskrip404@gmail.com>
2024-12-13 22:22:21 +0500

    доработал конфиг редиса

--- src/main/java/faang/school/notificationservice/config/RedisConfig.java
+++ src/main/java/faang/school/notificationservice/config/RedisConfig.java
@@ -1,11 +1,16 @@
 package faang.school.notificationservice.config;
+import faang.school.notificationservice.listener.LikeEventListener;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -36,6 +41,21 @@ public class RedisConfig {
         return template;
     }
 
+    @Bean
+    public MessageListenerAdapter likeListener(LikeEventListener likeEventListener) {
+        return new MessageListenerAdapter(likeEventListener);
+    }
 
+    @Bean
+    public ChannelTopic likeTopic() {
+        return new ChannelTopic(likeChannel);
+    }
 
+    @Bean
+    public RedisMessageListenerContainer redisContainer(MessageListenerAdapter likeListener) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(jedisConnectionFactory());
+        container.addMessageListener(likeListener, likeTopic());
+        return container;
+    }
 }
