566c94091026fe3e7320cf65dd899e74cdf42521
DavidT <davidx5@bk.ru>
2024-12-14 21:37:36 +0300

    BJS2-41767

---
+++ src/main/java/faang/school/notificationservice/config/context/BotInitializer.java
@@ -0,0 +1,28 @@
+package faang.school.notificationservice.config.context;
+
+import faang.school.notificationservice.service.telegram.TelegramBotService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class BotInitializer {
+    private final TelegramBotService bot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(bot);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+        }
+    }
+}
