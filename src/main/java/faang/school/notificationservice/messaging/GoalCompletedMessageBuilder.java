04b5aa4c3153ba4a4894c8a2abc704e0ffbb8869
infani <abrormukimov@gmail.com>
2024-12-15 22:41:33 +0500

    feature: add a message builder

--- src/main/java/faang/school/notificationservice/messaging/GoalCompletedMessageBuilder.java
+++ src/main/java/faang/school/notificationservice/messaging/GoalCompletedMessageBuilder.java
@@ -1,15 +1,21 @@
 package faang.school.notificationservice.messaging;
 
+import faang.school.notificationservice.dto.GoalCompletedEventDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class GoalCompletedMessageBuilder implements MessageBuilder {
+    private final MessageSource messageSource;
+
     @Override
     public Class<?> getInstance() {
         return GoalCompletedMessageBuilder.class;
@@ -17,6 +23,14 @@ public class GoalCompletedMessageBuilder implements MessageBuilder {
 
     @Override
     public String buildMessage(Object event, Locale locale) {
-        return "";
+        GoalCompletedEventDto goalEvent = (GoalCompletedEventDto) event;
+        log.info("Received a request to build a message, {}", "goalcompleted.message");
+        String eventDate = goalEvent.getCompletedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", locale));
+        String result = messageSource.getMessage(
+                "goalcompleted.message",
+                new Object[]{eventDate},
+                locale
+        );
+        return result;
     }
 }
