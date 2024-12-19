dddcb4695457e2fc407f0f3808e9bf001ca57458
Ruslan G. <marlinco263@gmail.com>
2024-12-19 11:00:05 +0100

    BJS2-41896 not completed

--- src/main/java/faang/school/notificationservice/NotificationServiceApp.java
+++ src/main/java/faang/school/notificationservice/NotificationServiceApp.java
@@ -1,18 +1,15 @@
 package faang.school.notificationservice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import faang.school.notificationservice.config.email.EmailConfig;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableFeignClients("faang.school.notificationservice.client")
-@EnableConfigurationProperties(EmailConfig.class)
 public class NotificationServiceApp {
     public static void main(String[] args) {
         new SpringApplicationBuilder(NotificationServiceApp.class)
