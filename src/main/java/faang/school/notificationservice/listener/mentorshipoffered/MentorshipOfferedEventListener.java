8b875415bcc824575e2cb8d29f476d2dd47c2217
Ruslan G. <marlinco263@gmail.com>
2024-12-21 16:53:37 +0100

    BJS2-41896 fixed comments

--- src/main/java/faang/school/notificationservice/listener/mentorshipoffered/MentorshipOfferedEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/mentorshipoffered/MentorshipOfferedEventListener.java
@@ -3,24 +3,31 @@ package faang.school.notificationservice.listener.mentorshipoffered;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.notificationservice.event.MentorshipOfferedEvent;
 import faang.school.notificationservice.listener.AbstractEventListener;
-import faang.school.notificationservice.service.EventService;
+import faang.school.notificationservice.messaging.MentorshipOfferedMessageBuilder;
+import faang.school.notificationservice.service.NotificationSender;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.stereotype.Service;
 
+import java.util.Locale;
 
 @Service
 public class MentorshipOfferedEventListener extends AbstractEventListener<MentorshipOfferedEvent> {
-    private final EventService eventService;
+    private final NotificationSender notificationSender;
+    private final MentorshipOfferedMessageBuilder builder;
 
-    public MentorshipOfferedEventListener(ObjectMapper objectMapper, EventService eventService) {
+    public MentorshipOfferedEventListener(ObjectMapper objectMapper, NotificationSender notificationSender, MentorshipOfferedMessageBuilder mentorshipOfferedMessageBuilder) {
         super(objectMapper);
-        this.eventService = eventService;
+        this.notificationSender = notificationSender;
+        this.builder = mentorshipOfferedMessageBuilder;
     }
 
     @Override
     public void onMessage(@NotNull Message message, byte[] pattern) {
         handleEvent(message, MentorshipOfferedEvent.class, event
-                -> eventService.sendMentorshipOfferedMessage(event.idRequest(), event.idAuthor(), event.idReceiver()));
+                -> {
+            String str = builder.buildMessage(new MentorshipOfferedEvent(event.idRequest(), event.idAuthor(), event.idReceiver()), Locale.ENGLISH);
+            notificationSender.sendNotification(event.idReceiver(), str);
+        });
     }
 }
