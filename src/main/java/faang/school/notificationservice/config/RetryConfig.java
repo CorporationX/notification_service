a85b7448a494a4f1a2e01e8770a8511b9aecf879
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-12-27 20:37:08 +0500

    Feature BJS2-41793 with Listener and with all tests
    (Version 10.0)

--- src/main/java/faang/school/notificationservice/config/RetryConfig.java
+++ src/main/java/faang/school/notificationservice/config/RetryConfig.java
@@ -1,9 +1,13 @@
 package faang.school.notificationservice.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.retry.interceptor.RetryInterceptorBuilder;
 import org.springframework.retry.interceptor.RetryOperationsInterceptor;
 
+@Configuration
+@EnableRetry
 public class RetryConfig {
     @Bean
     public RetryOperationsInterceptor retryInterceptor(RetryProperties retryProperties) {
