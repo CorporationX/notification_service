fd41dc6b9c1a610c74be0c315e26a6e9a010cb77
VovaBuzivskyi <vov4ikvseti@gmail.com>
2024-12-30 01:09:16 +0700

    refactor: CreateAccountEvent

--- src/main/java/faang/school/notificationservice/event/account/CreateAccountEvent.java
+++ src/main/java/faang/school/notificationservice/event/account/CreateAccountEvent.java
@@ -1,5 +1,6 @@
 package faang.school.notificationservice.event.account;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CreateAccountEvent {
     private Long ownerId;
     private String ownerType;
