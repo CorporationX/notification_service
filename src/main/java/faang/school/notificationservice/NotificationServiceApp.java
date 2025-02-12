54aa49db4e3574eba4ae90e5cab4b3fdb408af39
Alexey Gorokhov <almigor@mail.ru>
2025-02-12 11:59:42 +0300

    Email-notification

--- src/main/java/faang/school/notificationservice/NotificationServiceApp.java
+++ src/main/java/faang/school/notificationservice/NotificationServiceApp.java
@@ -2,10 +2,12 @@ package faang.school.notificationservice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.notificationservice.telegram.TelegramBot;
+import faang.school.notificationservice.config.email.MailProperties;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
@@ -14,6 +16,7 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableFeignClients("faang.school.notificationservice.client")
+@EnableConfigurationProperties(MailProperties.class)
 public class NotificationServiceApp {
     public static void main(String[] args) {
         new SpringApplicationBuilder(NotificationServiceApp.class)
