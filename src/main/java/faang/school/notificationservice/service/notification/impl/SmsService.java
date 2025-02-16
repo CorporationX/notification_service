3e31f20b358f959dd091a509caa80cf7ca84b21d
Stanislav <stasikbygaev@gmail.com>
2025-02-16 19:56:47 +0500

    Обработка всех сообщений в ответе

--- src/main/java/faang/school/notificationservice/service/notification/impl/SmsService.java
+++ src/main/java/faang/school/notificationservice/service/notification/impl/SmsService.java
@@ -6,10 +6,13 @@ import com.vonage.client.sms.SmsSubmissionResponse;
 import com.vonage.client.sms.SmsSubmissionResponseMessage;
 import com.vonage.client.sms.messages.TextMessage;
 import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.exception.ExternalServiceException;
 import faang.school.notificationservice.service.notification.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -27,10 +30,13 @@ public class SmsService implements NotificationService {
         SmsSubmissionResponse response = vonageClient.getSmsClient()
                 .submitMessage(smsMessage);
 
-        SmsSubmissionResponseMessage responseMessage = response.getMessages().get(0);
+        List<SmsSubmissionResponseMessage> responseMessages = response.getMessages();
 
-        if (responseMessage.getStatus() != MessageStatus.OK) {
-            log.error("Message failed with error: {}", responseMessage.getErrorText());
+        for (var responseMessage : responseMessages) {
+            if (responseMessage.getStatus() != MessageStatus.OK) {
+                log.error("Message failed with error: {}", responseMessage.getErrorText());
+                throw new ExternalServiceException("Failed to send SMS");
+            }
         }
     }
 
