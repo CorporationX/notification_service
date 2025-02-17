c27aded7ca9f35cb4f0e3cf66c69eaca4e5e7470
triplek <weribel@mail.ru>
2025-02-17 21:15:56 +0300

    подключил телеграм нотификации

---
+++ src/main/java/faang/school/notificationservice/controller/TelegramBotController.java
@@ -0,0 +1,21 @@
+package faang.school.notificationservice.controller;
+
+import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.service.TelegramBotService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/telegram-bot")
+public class TelegramBotController {
+    private final TelegramBotService telegramBotService;
+
+    @PostMapping("/notify")
+    public void send (@RequestBody UserDto user) {
+        telegramBotService.send(user, "Привет! Это тестовое уведомление 🚀");
+    }
+}
--- src/main/java/faang/school/notificationservice/dto/UserDto.java
+++ src/main/java/faang/school/notificationservice/dto/UserDto.java
@@ -9,6 +9,7 @@ public class UserDto {
     private String email;
     private String phone;
     private PreferredContact preference;
+    private String telegramChatId;
 
     public enum PreferredContact {
         EMAIL, SMS, TELEGRAM
