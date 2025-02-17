850cdef32154790be8aede6acee6c8453cfdcb18
AesaQ <ilyaovchinnikov2095@gmail.com>
2025-02-17 16:47:19 +0500

    rebuild: pubsub -> kafka

---
+++ src/main/java/faang/school/notificationservice/config/KafkaConsumingConfig.java
@@ -0,0 +1,50 @@
+package faang.school.notificationservice.config;
+
+import faang.school.notificationservice.LikeEvent;
+import faang.school.notificationservice.builder.LikeEventBuilder;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaConsumingConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+    private LikeEventBuilder likeEventBuilder;
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group-id");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String>
+                factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @KafkaListener(topics = "${spring.kafka.like_post_event_topic_name}", groupId = "group-id")
+    public void onLikePostEvent(String message) {
+        System.out.println("LIKE EVENT DETECTED: " + message);
+        LikeEvent event = likeEventBuilder.build(message);
+        /// ...
+        /// ...
+        /// ...
+    }
+}
