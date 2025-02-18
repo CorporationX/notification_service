279282c4772b7e4a49bad447dd3befbcf4f28e85
therealadik <vladerm2000@yandex.ru>
2025-02-18 23:33:36 +0500

    part 1

--- src/main/java/faang/school/notificationservice/listener/CommentEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/CommentEventListener.java
@@ -1,16 +1,38 @@
 package faang.school.notificationservice.listener;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.notificationservice.client.UserServiceClient;
 import faang.school.notificationservice.dto.CommentEvent;
+import faang.school.notificationservice.messaging.MessageBuilder;
+import faang.school.notificationservice.service.NotificationService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Locale;
 
 @Slf4j
 @Component
-public class CommentEventListener {
+public class CommentEventListener extends AbstractEventListener<CommentEvent> implements  MessageListener {
 
-    @KafkaListener(topics = "comment-events")
-    public void commentEventListen(CommentEvent comment) {
-        log.info("Received comment event: {}", comment);
+    public CommentEventListener(ObjectMapper objectMapper,
+                                UserServiceClient userServiceClient,
+                                List<MessageBuilder<CommentEvent>> messageBuilders,
+                                List<NotificationService> notificationServices) {
+        super(objectMapper, userServiceClient, messageBuilders, notificationServices);
+    }
+
+    @KafkaListener(topics = "comment_create")
+    @Override
+    public void onMessage(ConsumerRecord<String, String> record, Acknowledgment acknowledgment) {
+        handleEvent(record, CommentEvent.class, commentEvent -> {
+            String message = getMessage(commentEvent, Locale.UK);
+            sendNotification(commentEvent.getAuthorId(), message);
+        });
+
+        acknowledgment.acknowledge();
     }
 }
