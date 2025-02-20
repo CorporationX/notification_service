0ceb05bad5d4520a8b7cadfc2b278db0460dec42
Ilya Vysokolyan <rd2d@list.ru>
2025-02-20 20:43:07 +0300

    BJS2-54647: поправил тесты

--- src/test/java/faang/school/notificationservice/service/EmailServiceTest.java
+++ src/test/java/faang/school/notificationservice/service/EmailServiceTest.java
@@ -1,10 +1,11 @@
 package faang.school.notificationservice.service;
 
 import faang.school.notificationservice.dto.UserDto;
-import faang.school.notificationservice.service.EmailService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -15,11 +16,11 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
-public class emailServiceTest {
+public class EmailServiceTest {
+
     @Mock
     private JavaMailSender emailSender;
-    @Mock
-    private SimpleMailMessage templateMessage;
+
     @InjectMocks
     private EmailService emailService;
 
@@ -35,11 +36,16 @@ public class emailServiceTest {
         user.setPreference(UserDto.PreferredContact.EMAIL);
     }
 
-
     @Test
     public void testSendEmailSuccess() {
         emailService.send(user, MESSAGE);
-        verify(emailSender, times(1)).send(templateMessage);
-    }
+        ArgumentCaptor<SimpleMailMessage> mailMessageCaptor = ArgumentCaptor.forClass(SimpleMailMessage.class);
+        verify(emailSender, times(1)).send(mailMessageCaptor.capture());
 
+        SimpleMailMessage capturedMessage = mailMessageCaptor.getValue();
+
+        Assertions.assertEquals("test name", capturedMessage.getFrom());
+        Assertions.assertEquals("example@example.com", capturedMessage.getTo()[0]);
+        Assertions.assertEquals("test message", capturedMessage.getText());
+    }
 }
