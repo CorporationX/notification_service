5976a6068ba8c8b1a71ed75889668164bba713d9
Maria Bortsova <bortsovamariia@gmail.com>
2025-02-24 22:44:58 +0300

    BJS2-52590, BJS2-53493: refactor

--- src/main/java/faang/school/notificationservice/config/MessageSourceConfig.java
+++ src/main/java/faang/school/notificationservice/config/MessageSourceConfig.java
@@ -4,14 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
+import java.nio.charset.StandardCharsets;
+
+
 @Configuration
 public class MessageSourceConfig {
 
-   @Bean
-   public ResourceBundleMessageSource messageSource() {
-      ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-      messageSource.setBasename("messages");
-      messageSource.setDefaultEncoding("UTF-8");
-      return messageSource;
-   }
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.toString());
+        return messageSource;
+    }
 }
--- src/main/java/faang/school/notificationservice/kafka/listener/RecommendationRequestEventListener.java
+++ src/main/java/faang/school/notificationservice/kafka/listener/RecommendationRequestEventListener.java
@@ -2,35 +2,26 @@ package faang.school.notificationservice.kafka.listener;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import faang.school.notificationservice.client.UserServiceClient;
 import faang.school.notificationservice.dto.recommendation.RecommendationRequestEvent;
-import faang.school.notificationservice.dto.user.UserDto;
-import faang.school.notificationservice.messaging.MessageBuilder;
-import faang.school.notificationservice.service.recommendation.RecommendationRequestNotificationService;
+import faang.school.notificationservice.service.recommendation.RecommendationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-
-import java.util.Locale;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class RecommendationRequestEventListener {
 
-    private final RecommendationRequestNotificationService notificationService;
-    private final UserServiceClient userServiceClient;
-    private final MessageBuilder<RecommendationRequestEvent> messageBuilder;
+    private final RecommendationService recommendationService;
     private final ObjectMapper objectMapper;
 
     @KafkaListener(topics = "${kafka.recommendation.request.topic}", groupId = "notifications-group")
     public void listen(String message) {
         RecommendationRequestEvent recommendationRequestEvent = mapInputToRecommendationRequestEvent(message);
         log.info("Received recommendation request: {}", recommendationRequestEvent);
-        UserDto receiverUser = userServiceClient.getUser(recommendationRequestEvent.getReceiverId());
-        String messageToSend = messageBuilder.buildMessage(recommendationRequestEvent, Locale.getDefault());
-        notificationService.send(receiverUser, messageToSend);
+        recommendationService.sendNotification(recommendationRequestEvent);
     }
 
     private RecommendationRequestEvent mapInputToRecommendationRequestEvent(String input) {
