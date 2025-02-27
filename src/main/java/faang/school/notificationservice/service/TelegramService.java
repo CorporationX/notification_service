4e93c3eca215d3e8b22f72ca7dbfe3f070ece770
Natalya Vasileva <boryantheone228@yandex.ru>
2025-02-27 17:47:10 +0300

    BJS2-54655 Добавление @NotNull

--- src/main/java/faang/school/notificationservice/service/TelegramService.java
+++ src/main/java/faang/school/notificationservice/service/TelegramService.java
@@ -3,6 +3,7 @@ package faang.school.notificationservice.service;
 import faang.school.notificationservice.dto.UserDto;
 import faang.school.notificationservice.exception.TelegramChatIdNotFound;
 import faang.school.notificationservice.service.bot.TelegramBot;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -15,7 +16,7 @@ public class TelegramService implements NotificationService {
     private final TelegramBot telegramBot;
 
     @Override
-    public void send(UserDto user, String message) {
+    public void send(@NotNull UserDto user, String message) {
         if (user.getTelegramChatId() == null) {
             throw new TelegramChatIdNotFound("Нельзя отправить уведомление в телеграм, так как нет id чата");
         }
