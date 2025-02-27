cdde45107ed2f4a5afff0b22b74284625486e7e8
Alexey Gorokhov <almigor@mail.ru>
2025-02-27 23:13:44 +0300

    Правки после ребейза

--- src/main/java/faang/school/notificationservice/config/message_source/MessageSourceConfig.java
+++ src/main/java/faang/school/notificationservice/config/message_source/MessageSourceConfig.java
@@ -1,5 +1,6 @@
 package faang.school.notificationservice.config.message_source;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -12,17 +13,20 @@ import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 
 @Configuration
+@RequiredArgsConstructor
 public class MessageSourceConfig {
 
     @Value("${spring.messages.basename}")
-    private String basename;
+    private final String baseName;
+    @Value("${spring.messages.cacheseconds}")
+    private final Integer cacheSeconds;
 
     @Bean
     public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasenames(basename);
+        messageSource.setBasenames(baseName);
         messageSource.setDefaultEncoding(StandardCharsets.UTF_8.displayName());
-        messageSource.setCacheSeconds(3600);
+        messageSource.setCacheSeconds(cacheSeconds);
         return messageSource;
     }
 
