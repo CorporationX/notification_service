edc865bba1c34bd6d1fdfc99529564a73a33102a
Ivan Baev <ibai86@yandex.ru>
2025-03-01 17:01:24 +0300

    add unit test
    add message builder
    
    initial commit

---
+++ src/main/java/faang/school/notificationservice/dto/kafka/FollowUserDto.java
@@ -0,0 +1,9 @@
+package faang.school.notificationservice.dto.kafka;
+
+import java.time.LocalDateTime;
+
+public record FollowUserDto (
+        Long followerId,
+        Long followeeId,
+        LocalDateTime followedAt
+){}
