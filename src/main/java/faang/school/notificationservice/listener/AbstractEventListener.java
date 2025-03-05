d0ce769022f2333476b3ed26c456b4a75f706009
rostex <rostex2@yandex.ru>
2025-03-05 20:54:23 +0300

    Внесены изменения после кросс-ревью

--- src/main/java/faang/school/notificationservice/listener/AbstractEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/AbstractEventListener.java
@@ -3,34 +3,61 @@ package faang.school.notificationservice.listener;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.notificationservice.client.UserServiceClient;
 import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.exception.BusinessException;
+import faang.school.notificationservice.exception.DataValidationException;
 import faang.school.notificationservice.messaging.MessageBuilder;
 import faang.school.notificationservice.service.NotificationStrategyService;
+import faang.school.notificationservice.service.UserService;
+import faang.school.notificationservice.service.notification.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.MessageListener;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
-@Slf4j
 @RequiredArgsConstructor
 public abstract class AbstractEventListener<T> implements MessageListener {
+    private static final String MESSAGE_BUILDER_NOT_FOUND =
+            "Для типа события %s не найден MessageBuilder";
+    private static final String NOT_FOUND_NOTIFICATION_SERVICE =
+            "Не найден необходимый сервис отправки уведомления";
 
-    private final ObjectMapper objectMapper;
-    protected final NotificationStrategyService notificationStrategyService;
-    protected final UserServiceClient userServiceClient;
-    protected final MessageBuilder<T> messageBuilder;
+    protected final ObjectMapper objectMapper;
+    protected final UserService userService;
+    protected final List<NotificationService> notificationServices;
+    protected final List<MessageBuilder<T>> messageBuilders;
 
-    public T handleEvent(byte[] body, Class<T> eventType) {
+    protected void sendNotification(Long userId, String message) {
+        UserDto userDto = userService.getUserById(userId);
+        if (userDto.getPreference() == null) {
+            throw new DataValidationException("У пользователя c ID " + userDto.getId()
+                    + " отсутствует способ отправки уведомления");
+        }
+        notificationServices.stream()
+                .filter(service -> service.getPreferredContact() == userDto.getPreference())
+                .findFirst()
+                .orElseThrow(() -> new BusinessException(NOT_FOUND_NOTIFICATION_SERVICE))
+                .send(userDto, message);
+    }
+
+    protected T getEventFromBytes(byte[] body, Class<T> typeClass) {
         try {
-            return objectMapper.readValue(body, eventType);
+            return objectMapper.readValue(body, typeClass);
         } catch (IOException e) {
-            log.error("Ошибка при чтении События из Redis: {}", e.getMessage());
             throw new RuntimeException(e);
         }
     }
 
-    public void sendNotification(UserDto userDto, String message) {
-        notificationStrategyService.getNotificationService(userDto)
-                .send(userDto, message);
+    protected String getMessage(T event, Locale locale) {
+        return messageBuilders.stream()
+                .filter(messageBuilder ->
+                        messageBuilder.getInstance().equals(event.getClass()))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException(
+                        String.format(MESSAGE_BUILDER_NOT_FOUND, event.getClass())
+                ))
+                .buildMessage(event, locale);
     }
 }
