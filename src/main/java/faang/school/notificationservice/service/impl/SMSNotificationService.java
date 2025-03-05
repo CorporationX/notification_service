f8a60da50793c0d2229ae06c2ad43a44d14e043f
Pavel <45390423+rsticks@users.noreply.github.com>
2025-03-05 17:55:23 +0300

    BJS2-53540 правки

--- src/main/java/faang/school/notificationservice/service/impl/SMSNotificationService.java
+++ src/main/java/faang/school/notificationservice/service/impl/SMSNotificationService.java
@@ -17,7 +17,8 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 public class SMSNotificationService implements NotificationService {
-    private static final String FROM = "Corp X";
+    private static final String MESSAGE_FROM = "Corp X";
+
     private final VonageClient vonageClient;
     @Value("${notification-service.sms.enabled}")
     private boolean isSMSEnabled;
@@ -25,23 +26,23 @@ public class SMSNotificationService implements NotificationService {
     @Override
     @Retryable(retryFor = {RuntimeException.class}, backoff = @Backoff(delay = 20000, multiplier = 2))
     public void send(UserDto user, String message) {
-        if (IsSMSAvailable(user)) {
+        if (isSMSNotAvailable(user)) {
             return;
         }
 
         log.info("Send SMS notification to user: {}, message: {}", user, message);
 
-        TextMessage textMessage = new TextMessage(FROM, user.getPhone(), message);
+        TextMessage textMessage = new TextMessage(MESSAGE_FROM, user.getPhone(), message);
         try {
             SmsSubmissionResponse response = vonageClient.getSmsClient().submitMessage(textMessage);
-            checkResponse(response);
+            checkResponse(response, message);
         } catch (RuntimeException e) {
             log.error("Error vonage", e);
             throw e;
         }
     }
 
-    private boolean IsSMSAvailable(UserDto user) {
+    private boolean isSMSNotAvailable(UserDto user) {
         if (!isSMSEnabled) {
             log.info("SMS notification is disabled");
             return true;
@@ -57,11 +58,11 @@ public class SMSNotificationService implements NotificationService {
         return false;
     }
 
-    private void checkResponse(SmsSubmissionResponse response) {
+    private void checkResponse(SmsSubmissionResponse response, String message) {
         if (response.getMessages().get(0).getStatus() == MessageStatus.OK) {
-            log.info("Message sent successfully.");
+            log.info("Message: {} sent successfully.", message);
         } else {
-            log.info("Message failed with error: {}", response.getMessages().get(0).getErrorText());
+            log.info("Message: {} failed with error: {}", message, response.getMessages().get(0).getErrorText());
         }
     }
 }
