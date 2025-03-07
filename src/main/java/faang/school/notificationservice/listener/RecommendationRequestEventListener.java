1d49a35186db8c621766e6b1380245901c825be1
Dmitriy0010 <dda20040609@gmail.com>
2025-03-07 20:51:27 +0300

    1 commit

---
+++ src/main/java/faang/school/notificationservice/listener/RecommendationRequestEventListener.java
@@ -0,0 +1,33 @@
+package faang.school.notificationservice.listener;
+
+import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.RecommendationRequestedEvent;
+import faang.school.notificationservice.messaging.MessageBuilder;
+import faang.school.notificationservice.service.NotificationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+public class RecommendationRequestEventListener extends AbstractEventListener<RecommendationRequestedEvent> {
+
+    public RecommendationRequestEventListener(
+            List<MessageBuilder<RecommendationRequestedEvent>> messageBuilders,
+            List<NotificationService> notificationServices,
+            UserServiceClient userServiceClient) {
+        super(userServiceClient, messageBuilders, notificationServices);
+    }
+
+    @KafkaListener(
+            topics = "${spring.kafka.topics.recommendation_requests}",
+            properties = "spring.json.value.default.type=faang.school.notificationservice.dto.RecommendationRequestedEvent"
+    )
+    @Override
+    public void onMessage(RecommendationRequestedEvent event) {
+        handleMessage(event, event.getTargetUserId());
+        log.info("Recommendation request processed: {}", event);
+    }
+}
