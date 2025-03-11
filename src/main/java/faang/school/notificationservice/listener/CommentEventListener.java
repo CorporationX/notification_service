b8ffcfc4bc83db1c9410d12def7f0015591d3d41
Pavel Schelkin <rhanan1233@gmail.com>
2025-03-11 23:07:15 +0300

    Create comment notification

--- src/main/java/faang/school/notificationservice/listener/CommentEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/CommentEventListener.java
@@ -1,7 +1,7 @@
 package faang.school.notificationservice.listener;
 
 import faang.school.notificationservice.client.UserServiceClient;
-import faang.school.notificationservice.dto.UserEventDto;
+import faang.school.notificationservice.dto.UserNotificationDto;
 import faang.school.notificationservice.listener.event.CommentEvent;
 import faang.school.notificationservice.messaging.MessageBuilder;
 import faang.school.notificationservice.service.NotificationService;
@@ -9,23 +9,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.util.Map;
 
 @Component
 public class CommentEventListener extends AbstractEventListener<CommentEvent> {
 
     @Autowired
-    public CommentEventListener(List<MessageBuilder<CommentEvent>> messageBuilders,
-                                List<NotificationService> notificationServiceList,
-                                UserServiceClient userServiceClient) {
-        super(messageBuilders, notificationServiceList, userServiceClient);
+    public CommentEventListener(UserServiceClient userServiceClient,
+                                Map<UserNotificationDto.PreferredContact, NotificationService> notificationServicesMap,
+                                Map<Class<?>, MessageBuilder<?>> messageBuildersMap) {
+        super(userServiceClient, notificationServicesMap, messageBuildersMap);
     }
 
-    @KafkaListener(topics = "${user.comment.kafka_topic}",
+    @KafkaListener(topics = "${spring.kafka.consumer.comment-create.topic}",
             groupId = "${spring.kafka.consumer.group-id}",
             containerFactory = "kafkaListenerContainerFactory")
     public void commentEventListner(CommentEvent commentEvent) {
-        UserEventDto userAuthorPost = userServiceClient.getUserForEvent(commentEvent.getAuthorPostId());
+        UserNotificationDto userAuthorPost = userServiceClient.getUserNotificationDto(commentEvent.getAuthorPostId());
         String message = getMessage(commentEvent, userAuthorPost.getLocale());
         sendNotification(userAuthorPost.getId(), message);
     }
--- src/main/java/faang/school/notificationservice/messaging/CommentEventMessage.java
+++ src/main/java/faang/school/notificationservice/messaging/CommentEventMessage.java
@@ -1,14 +1,9 @@
 package faang.school.notificationservice.messaging;
 
-import faang.school.notificationservice.client.ProjectServiceClient;
 import faang.school.notificationservice.client.UserServiceClient;
-import faang.school.notificationservice.dto.UserDto;
-import faang.school.notificationservice.dto.UserEventDto;
-import faang.school.notificationservice.listener.CommentEventListener;
+import faang.school.notificationservice.dto.UserNotificationDto;
 import faang.school.notificationservice.listener.event.CommentEvent;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 
@@ -29,7 +24,7 @@ public class CommentEventMessage implements MessageBuilder<CommentEvent> {
 
     @Override
     public String buildMessage(CommentEvent event, Locale locale) {
-        UserEventDto authorComment = userServiceClient.getUserForEvent(event.getAuthorCommentId());
+        UserNotificationDto authorComment = userServiceClient.getUserNotificationDto(event.getAuthorCommentId());
         return messageSource.getMessage(MESSAGE_KEY, new Object[]{authorComment.getUsername(), event.getPostId()
         }, locale);
     }
