4537b4d92c5dfa3571cf49abd6eb98dab53763fb
Anton <basanton@mail.ru>
2025-04-08 16:59:40 +0300

    Выполнение задачи BJS2-65631 Отправка SMS сообщений

--- src/main/java/faang/school/notificationservice/dto/UserDto.java
+++ src/main/java/faang/school/notificationservice/dto/UserDto.java
@@ -1,5 +1,6 @@
 package faang.school.notificationservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +14,7 @@ public class UserDto {
     private long id;
     private String username;
     private String email;
+    @NotBlank(message = "To send SMS, a phone number is a mandatory requirement")
     private String phone;
     private PreferredContact preference;
 
