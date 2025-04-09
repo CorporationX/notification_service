5532aaa52d55a9ed461c736f45ba3f3159c3e6c9
vtroshin <vtroshin@rvision.ru>
2025-04-09 19:33:28 +0300

    BJS2-66540 Java Doc

--- src/main/java/faang/school/notificationservice/event/AbstractEventListener.java
+++ src/main/java/faang/school/notificationservice/event/AbstractEventListener.java
@@ -18,6 +18,30 @@ import java.util.List;
 import java.util.Locale;
 import java.util.function.Consumer;
 
+/**
+ * Абстрактный слушатель обработки всех входящих событий
+ * <p>
+ * Слушатель передает на обработку события сервисам
+ * </p>
+ *
+ * <p>
+ * Основные функции:
+ * <ul>
+ *      <li>{@link #handleEvent(Message, Class, Consumer)} Считывает тело события и отправляет в обработку.</li>
+ *      <li>{@link #getMessage(Object, Locale)} Преобразует тест в зависимости от класса объекта и локали.</li>
+ *      <li>{@link #sendNotification(Long, String)}  Отправляет нотификацию указанному юзеру.</li>
+ * </ul>
+ * </p>
+ *
+ * @author takewqa
+ * @see Message
+ * @see Locale
+ * @see Consumer
+ * @see ObjectMapper
+ * @see UserServiceClient
+ * @see MessageBuilder
+ * @see NotificationService
+ */
 @RequiredArgsConstructor
 @Slf4j
 public class AbstractEventListener<T> {
@@ -27,6 +51,12 @@ public class AbstractEventListener<T> {
     private final List<MessageBuilder<T>> messageBuilders;
     private final List<NotificationService> notificationServices;
 
+    /**
+     * Считывает тело событие и отправляет в обработку
+     * @param message Сообщение из Redis
+     * @param clazz Class объекта
+     * @param consumer Обработчик события
+     */
     protected void handleEvent(@NotNull Message message,@NotNull Class<T> clazz, @NotNull Consumer<T> consumer) {
         try {
             T event = objectMapper.readValue(message.getBody(), clazz);
@@ -37,6 +67,12 @@ public class AbstractEventListener<T> {
         }
     }
 
+    /**
+     * Получение отформатированного текста
+     * @param event Объект для обработки текста
+     * @param locale Локаль пользователя
+     * @return Отформатированное сообщение
+     */
     protected String getMessage(@NotNull T event, Locale locale) {
         return messageBuilders.stream()
                 .filter(builder -> builder.getInstance().equals(event.getClass()))
@@ -46,6 +82,11 @@ public class AbstractEventListener<T> {
                 .orElseThrow(() -> new EventListenerException("No suitable builder found for " + event.getClass()));
     }
 
+    /**
+     * Отправляет сообщение
+     * @param userId Id получателя
+     * @param message Текст сообщения
+     */
     protected void sendNotification(@NotNull Long userId, @NotBlank String message) {
         UserDto userDto = userServiceClient.getUser(userId);
         if (userDto == null) {
