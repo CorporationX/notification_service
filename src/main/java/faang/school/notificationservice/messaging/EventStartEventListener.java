d7c12124795c54c111404416f80736297a9c2f11
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-04-12 15:15:13 +0500

    https://faang-school.atlassian.net/browse/BJS2-65650
    config redis + add EventStartEventListener

--- src/main/java/faang/school/notificationservice/messaging/EventStartEventListener.java
+++ src/main/java/faang/school/notificationservice/messaging/EventStartEventListener.java
@@ -13,6 +13,6 @@ public class EventStartEventListener implements MessageListener {
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
-
+        message.getBody(); //тут надо описать что будет делать по факту
     }
 }
