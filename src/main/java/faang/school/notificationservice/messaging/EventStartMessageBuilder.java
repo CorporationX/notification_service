bc2e579d7faf7acdbc52cef7486568f2dd5bc6d9
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-04-12 16:27:02 +0500

    https://faang-school.atlassian.net/browse/BJS2-65650
    update + MessageBuilder

--- src/main/java/faang/school/notificationservice/messaging/EventStartMessageBuilder.java
+++ src/main/java/faang/school/notificationservice/messaging/EventStartMessageBuilder.java
@@ -1,10 +1,8 @@
 package faang.school.notificationservice.messaging;
 
-import faang.school.notificationservice.dto.UserDto;
 import faang.school.notificationservice.messaging.event.EventStartEvent;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.Locale;
 
 @Component
@@ -17,19 +15,7 @@ public class EventStartMessageBuilder implements MessageBuilder<EventStartEvent>
 
     @Override
     public String buildMessage(EventStartEvent event, Locale locale) {
-        // Персонализируем сообщение для каждого участника
-        String eventId = event.getEventId();
-        List<String> participantIds = event.getParticipantIds();
-
-        // Генерация текста уведомления
-        StringBuilder message = new StringBuilder("Событие ");
-        message.append(eventId).append(" начинается прямо сейчас!");
-
-        // Здесь можно добавить локализацию для разных языков, если нужно
-        if (locale != null && locale.getLanguage().equals("ru")) {
-            message.append(" Присоединяйтесь к нам!");
-        }
-
-        return message.toString();
+        // Строим сообщение для события
+        return "Событие " + event.getEventId() + " начинается прямо сейчас!";
     }
 }
