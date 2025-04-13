be5341e163d0caab52aaddf1fc8da210060f9283
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-04-13 11:48:50 +0300

    Реализовал механику ListenerEvent

---
+++ src/main/java/faang/school/notificationservice/service/listener/PostLikeEventListener.java
@@ -0,0 +1,37 @@
+package faang.school.notificationservice.service.listener;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.event.PostLikeEvent;
+import faang.school.notificationservice.messaging.MessageBuilder;
+import faang.school.notificationservice.service.NotificationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Locale;
+
+@Slf4j
+@Service
+public class PostLikeEventListener extends AbstractEventListener {
+
+    public PostLikeEventListener(ObjectMapper objectMapper,
+                                 UserServiceClient userServiceClient,
+                                 List<NotificationService> notificationServices,
+                                 List<MessageBuilder<PostLikeEvent>> messageBuilders) {
+        super(objectMapper, userServiceClient, notificationServices, messageBuilders);
+    }
+
+    @KafkaListener(topics = "${spring.kafka.topics.like.notification.post-like-topic}",
+            groupId = "${spring.kafka.topics.like.notification.post-like-group-id}")
+    public void listen(String message) {
+        handleEvent(message, PostLikeEvent.class, event -> {
+            PostLikeEvent likeEvent = (PostLikeEvent) event;
+            UserDto author = userServiceClient.getUser(likeEvent.getPostId());
+            String text = getMessage(likeEvent, Locale.UK);
+            sendNotification(author.getId(), text);
+        });
+    }
+}
