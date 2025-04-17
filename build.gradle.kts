da3bb171f6155f4f8b48f96388387aada1d217f5
Artpyak <art.pyak@yandex.ru>
2025-04-17 16:30:12 +0300

    BJS2-66474 добавил более новую версию spring-retry 2.0.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.0.2")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     implementation ("org.telegram:telegrambots-springboot-longpolling-starter:8.3.0")
-    implementation("org.springframework.retry:spring-retry:2.0.5")
+    implementation("org.springframework.retry:spring-retry:2.0.11")
     implementation("org.springframework:spring-aspects")
 
     /**
