154de1525efa05005cc9ab3cdbbc32f2af7e6967
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-04-18 20:13:13 +0300

    test: Added AbstractEventListener.sendNotification tests

--- src/test/java/faang/school/notificationservice/event_listener/AbstractEventListenerTest.java
+++ src/test/java/faang/school/notificationservice/event_listener/AbstractEventListenerTest.java
@@ -3,16 +3,22 @@ package faang.school.notificationservice.event_listener;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
 import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.UserDto;
 import faang.school.notificationservice.event.AnotherTestEvent;
 import faang.school.notificationservice.event.TestEvent;
 import faang.school.notificationservice.exception.BuildMessageFailedException;
+import faang.school.notificationservice.exception.DataValidationException;
+import faang.school.notificationservice.exception.UserNotFoundException;
 import faang.school.notificationservice.messaging.HelloWorldEventMessageBuilder;
 import faang.school.notificationservice.messaging.JsonTestEventMessageBuilder;
 import faang.school.notificationservice.messaging.MessageBuilder;
 import faang.school.notificationservice.messaging.TestEventMessageBuilder;
 import faang.school.notificationservice.service.NotificationService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
@@ -25,6 +31,8 @@ import java.util.Locale;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -46,40 +54,51 @@ public class AbstractEventListenerTest {
     private List<NotificationService> notificationServices;
     private List<MessageBuilder<?>> messageBuilders;
 
-    private AbstractEventListener abstractEventListener;
+    private final NotificationService smsNotificationService = Mockito.mock(NotificationService.class);
+    private final NotificationService emailNotificationService = Mockito.mock(NotificationService.class);
+    private final NotificationService anotherEmailNotificationService = Mockito.mock(NotificationService.class);
+
+    @BeforeEach
+    public void setUp() {
+        when(smsNotificationService.getPreferredContact()).thenReturn(UserDto.PreferredContact.PHONE);
+        when(emailNotificationService.getPreferredContact()).thenReturn(UserDto.PreferredContact.EMAIL);
+        when(anotherEmailNotificationService.getPreferredContact()).thenReturn(UserDto.PreferredContact.EMAIL);
+
+        notificationServices = new ArrayList<>();
+        notificationServices.add(smsNotificationService);
+        notificationServices.add(emailNotificationService);
+        notificationServices.add(anotherEmailNotificationService);
+    }
 
     @Test
     public void shouldPostConstruct_throws_whenSeveralMessageBuildersForSingleEventType() {
-        notificationServices = List.of();
         messageBuilders = new ArrayList<>();
         messageBuilders.add(new TestEventMessageBuilder());
         messageBuilders.add(new HelloWorldEventMessageBuilder());
-        abstractEventListener = new AbstractEventListener(objectMapper, userServiceClient, notificationServices,
+        var abstractEventListener = new AbstractEventListener(objectMapper, userServiceClient, notificationServices,
                 messageBuilders);
 
-        assertThrows(IllegalStateException.class, () -> abstractEventListener.postConstruct());
+        assertThrows(IllegalStateException.class, abstractEventListener::postConstruct);
     }
 
     @Test
     public void shouldPostConstruct_success_whenNoMessageBuildersAreRegistered() {
-        notificationServices = List.of();
         messageBuilders = List.of();
-        abstractEventListener = new AbstractEventListener(objectMapper, userServiceClient, notificationServices,
+        var abstractEventListener = new AbstractEventListener(objectMapper, userServiceClient, notificationServices,
                 messageBuilders);
 
-        assertDoesNotThrow(() -> abstractEventListener.postConstruct());
+        assertDoesNotThrow(abstractEventListener::postConstruct);
     }
 
     @Test
     public void shouldPostConstruct_success_whenAllMessageBuildersProcessDifferentEventTypes() {
-        notificationServices = List.of();
         messageBuilders = new ArrayList<>();
         messageBuilders.add(new TestEventMessageBuilder());
         messageBuilders.add(new JsonTestEventMessageBuilder(objectMapper));
-        abstractEventListener = new AbstractEventListener(objectMapper, userServiceClient, notificationServices,
+        var abstractEventListener = new AbstractEventListener(objectMapper, userServiceClient, notificationServices,
                 messageBuilders);
 
-        assertDoesNotThrow(() -> abstractEventListener.postConstruct());
+        assertDoesNotThrow(abstractEventListener::postConstruct);
     }
 
     @Test
@@ -88,8 +107,7 @@ public class AbstractEventListenerTest {
         var messageBuilder = Mockito.mock(TestEventMessageBuilder.class);
         messageBuilders = new ArrayList<>();
         messageBuilders.add(messageBuilder);
-        notificationServices = List.of();
-        abstractEventListener = new AbstractEventListener(objectMapper, userServiceClient, notificationServices,
+        var abstractEventListener = new AbstractEventListener(objectMapper, userServiceClient, notificationServices,
                 messageBuilders);
 
         when(messageBuilder.getInstance()).thenAnswer(invocationOnMock -> TestEvent.class);
@@ -112,8 +130,7 @@ public class AbstractEventListenerTest {
         var messageBuilder = Mockito.mock(JsonTestEventMessageBuilder.class);
         messageBuilders = new ArrayList<>();
         messageBuilders.add(messageBuilder);
-        notificationServices = List.of();
-        abstractEventListener = new AbstractEventListener(objectMapper, userServiceClient, notificationServices,
+        var abstractEventListener = new AbstractEventListener(objectMapper, userServiceClient, notificationServices,
                 messageBuilders);
 
         when(messageBuilder.getInstance()).thenAnswer(invocationOnMock -> AnotherTestEvent.class);
@@ -138,8 +155,7 @@ public class AbstractEventListenerTest {
         var messageBuilder = Mockito.mock(TestEventMessageBuilder.class);
         messageBuilders = new ArrayList<>();
         messageBuilders.add(messageBuilder);
-        notificationServices = List.of();
-        abstractEventListener = new AbstractEventListener(objectMapper, userServiceClient, notificationServices,
+        var abstractEventListener = new AbstractEventListener(objectMapper, userServiceClient, notificationServices,
                 messageBuilders);
 
         when(messageBuilder.getInstance()).thenAnswer(invocationOnMock -> TestEvent.class);
@@ -159,13 +175,12 @@ public class AbstractEventListenerTest {
     }
 
     @Test
-    void shouldGetMessage_throws_whenBuilderIsNotFound() {
+    public void shouldGetMessage_throws_whenBuilderIsNotFound() {
         // Подготовка данных
         Class<?> otherClass = String.class;
-        notificationServices = List.of();
         messageBuilders = new ArrayList<>();
         messageBuilders.add(new TestEventMessageBuilder());
-        abstractEventListener = new AbstractEventListener(objectMapper, userServiceClient, notificationServices,
+        var abstractEventListener = new AbstractEventListener(objectMapper, userServiceClient, notificationServices,
                 messageBuilders);
 
         assertThrows(BuildMessageFailedException.class,
@@ -173,15 +188,106 @@ public class AbstractEventListenerTest {
     }
 
     @Test
-    void shouldGetMessage_throws_whenInvalidJsonIsPassed() throws Exception {
+    public void shouldGetMessage_throws_whenInvalidJsonIsPassed() throws Exception {
         var invalidJson = "{invalid json}";
-        notificationServices = List.of();
         messageBuilders = new ArrayList<>();
         messageBuilders.add(new TestEventMessageBuilder());
-        abstractEventListener = new AbstractEventListener(objectMapper, userServiceClient, notificationServices,
+        var abstractEventListener = new AbstractEventListener(objectMapper, userServiceClient, notificationServices,
                 messageBuilders);
 
         assertThrows(BuildMessageFailedException.class,
                 () -> abstractEventListener.getMessage(TestEvent.class, TEST_LOCALE, invalidJson));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "  "})
+    public void shouldSendNotification_throws_whenMessageIsInvalid(String message) {
+        messageBuilders = List.of();
+        var abstractEventListener = new AbstractEventListener(objectMapper, userServiceClient, notificationServices,
+                messageBuilders);
+
+        assertThrows(DataValidationException.class,
+                () -> abstractEventListener.sendNotification(faker.number().randomNumber(), message));
+        verifyNoInteractions(userServiceClient);
+        verifyNoInteractions(smsNotificationService);
+        verifyNoInteractions(emailNotificationService);
+        verifyNoInteractions(anotherEmailNotificationService);
+    }
+
+    @Test
+    public void shouldSendNotification_throws_whenUserIsNotFound() {
+        // Arrange
+        messageBuilders = List.of();
+        var abstractEventListener = new AbstractEventListener(objectMapper, userServiceClient, notificationServices,
+                messageBuilders);
+
+        var userId = faker.number().randomNumber();
+        when(userServiceClient.getUser(userId)).thenThrow(new RuntimeException("Service error"));
+
+        // Act + Assert
+        assertThrows(UserNotFoundException.class,
+                () -> abstractEventListener.sendNotification(userId, "Test message"));
+        verify(userServiceClient, times(1)).getUser(userId);
+        verifyNoInteractions(smsNotificationService);
+        verifyNoInteractions(emailNotificationService);
+        verifyNoInteractions(anotherEmailNotificationService);
+    }
+
+    @Test
+    public void shouldSendNotification_success_whenNotificationServiceIsFound() {
+        // Arrange
+        var userId = faker.number().randomNumber();
+        messageBuilders = List.of();
+        var abstractEventListener = new AbstractEventListener(objectMapper, userServiceClient, notificationServices,
+                messageBuilders);
+
+        var testUser = UserDto.builder()
+                .id(userId)
+                .phone(faker.phoneNumber().phoneNumber())
+                .email(faker.internet().emailAddress())
+                .username(faker.name().username())
+                .preference(UserDto.PreferredContact.EMAIL)
+                .build();
+        when(userServiceClient.getUser(userId)).thenReturn(testUser);
+
+        var message = "Test message";
+
+        // Act
+        var result = abstractEventListener.sendNotification(userId, message);
+
+        // Assert
+        assertDoesNotThrow(result::join);
+        verify(userServiceClient, times(1)).getUser(userId);
+        verify(emailNotificationService, times(1)).send(testUser, message);
+        verify(smsNotificationService, times(0)).send(any(), anyString());
+        verify(anotherEmailNotificationService).send(testUser, message);
+    }
+
+    @Test
+    public void shouldSendNotification_success_whenNoMatchingService() {
+        // Arrange
+        var userId = faker.number().randomNumber();
+        messageBuilders = List.of();
+        var abstractEventListener = new AbstractEventListener(objectMapper, userServiceClient, notificationServices,
+                messageBuilders);
+
+        var testUser = UserDto.builder()
+                .id(userId)
+                .phone(faker.phoneNumber().phoneNumber())
+                .email(faker.internet().emailAddress())
+                .username(faker.name().username())
+                .preference(UserDto.PreferredContact.TELEGRAM)
+                .build();
+        when(userServiceClient.getUser(userId)).thenReturn(testUser);
+
+        // Act
+        var result = abstractEventListener.sendNotification(userId, "Test message");
+
+        // Assert
+        assertDoesNotThrow(result::join);
+        verify(userServiceClient, times(1)).getUser(userId);
+        verify(emailNotificationService, times(0)).send(any(), anyString());
+        verify(smsNotificationService, times(0)).send(any(), anyString());
+        verify(anotherEmailNotificationService, times(0)).send(any(), anyString());
     }
 }
