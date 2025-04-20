2f2d4d15532ed595504a42d22e70dd8b00dc8df5
Max Goloviznin <roxet59rus@mail.ru>
2025-04-21 03:14:15 +0500

    Revert "(BJS2-65255) email test disable"
    This reverts commit 097c9a248b6858404c09d18b4ba4adac321a4240.

--- src/test/java/faang/school/notificationservice/service/EmailServiceTest.java
+++ src/test/java/faang/school/notificationservice/service/EmailServiceTest.java
@@ -15,9 +15,6 @@ class EmailServiceTest {
     @Test
     @Disabled("Для ручного запуска с реальным Gmail")
     void positiveSendEmail() {
-        if (System.getenv("CI") != null) {
-            return;
-        }
         UserDto userDto = UserDto.builder()
                 .id(1L)
                 .email("example@gmail.com")
