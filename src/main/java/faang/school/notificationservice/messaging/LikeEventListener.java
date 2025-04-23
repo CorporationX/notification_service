acb61d33bd09387fdd42951a702c98973f2ac486
Andrey Ievlev <avievlev@gmail.com>
2025-04-24 00:19:48 +0300

    BJS2-65682 Отправка Email сообщений: для тестов реализовал задачу "BJS2-65652 Уведомление о лайке" но без тестов.

--- src/main/java/faang/school/notificationservice/messaging/LikeEventListener.java
+++ src/main/java/faang/school/notificationservice/messaging/LikeEventListener.java
@@ -4,30 +4,28 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.notificationservice.client.UserServiceClient;
 import faang.school.notificationservice.dto.LikeEvent;
 import faang.school.notificationservice.service.NotificationService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 @Component
-@RequiredArgsConstructor
 @Slf4j
-public class LikeEventListener implements MessageListener {
-    private final ObjectMapper objectMapper;
-    private final UserServiceClient userServiceClient;
-    private final List<NotificationService> notificationServices;
+public class LikeEventListener extends AbstractEventListener<LikeEvent> implements MessageListener {
+
+    public LikeEventListener(ObjectMapper objectMapper, UserServiceClient userServiceClient,
+                             List<NotificationService> notificationServices,
+                             List<MessageBuilder<LikeEvent>> messageBuilders) {
+        super(objectMapper, userServiceClient, notificationServices, messageBuilders);
+    }
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
-        try {
-            LikeEvent likeEvent = objectMapper.readValue(message.getBody(), LikeEvent.class);
-            log.info("Like event received: {}", likeEvent);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        handleEvent(message, LikeEvent.class,
+                event -> sendNotification(event.getAuthorId(), getMessage(event, Locale.getDefault()))
+        );
     }
 }
