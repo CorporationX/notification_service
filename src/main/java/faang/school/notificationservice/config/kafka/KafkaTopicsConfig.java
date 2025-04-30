42bb15e26851f76061f3db236d296505e647f10c
Max Goloviznin <roxet59rus@mail.ru>
2025-05-01 03:14:25 +0500

    (BJS2-65255) fix bugs

--- src/main/java/faang/school/notificationservice/config/kafka/KafkaTopicsConfig.java
+++ src/main/java/faang/school/notificationservice/config/kafka/KafkaTopicsConfig.java
@@ -24,7 +24,6 @@ public class KafkaTopicsConfig {
         return TopicBuilder.name(name)
                 .partitions(partitions)
                 .replicas(replicas)
-                .compact()
                 .build();
     }
 }
