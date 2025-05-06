447f33dd9091d505376fb9a270ba364647c0b31a
Max Goloviznin <roxet59rus@mail.ru>
2025-05-06 18:33:26 +0500

    (BJS2-65255) fix by comments

--- src/main/java/faang/school/notificationservice/config/properties/HashtagNotificationTopicProperties.java
+++ src/main/java/faang/school/notificationservice/config/properties/HashtagNotificationTopicProperties.java
@@ -2,7 +2,7 @@ package faang.school.notificationservice.config.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "spring.data.kafka.topics.hashtag-notification")
+@ConfigurationProperties(prefix = "spring.data.kafka.topic.hashtag-notification")
 public record HashtagNotificationTopicProperties(
         String name,
         int partitions,
