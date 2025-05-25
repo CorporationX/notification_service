471306b70f1f004321943db2667016faea5b17a0
naught-wo <catrinesaxon@yandex.ru>
2025-05-25 10:16:41 +0300

    поправила отступы

--- src/main/java/faang/school/notificationservice/messaging/MessageBuilder.java
+++ src/main/java/faang/school/notificationservice/messaging/MessageBuilder.java
@@ -5,15 +5,15 @@ import org.springframework.context.MessageSource;
 
 import java.util.Locale;
 
-    @RequiredArgsConstructor
-    public abstract class MessageBuilder<T> {
-        private final MessageSource messageSource;
+@RequiredArgsConstructor
+public abstract class MessageBuilder<T> {
+    private final MessageSource messageSource;
 
-        public abstract Class<?> getInstance();
+    public abstract Class<?> getInstance();
 
-        public abstract String buildMessage(T event, Locale locale);
+    public abstract String buildMessage(T event, Locale locale);
 
-        protected String buildMessage(String code, Locale locale, String... args) {
-            return messageSource.getMessage(code, args, locale);
-        }
+    protected String buildMessage(String code, Locale locale, String... args) {
+        return messageSource.getMessage(code, args, locale);
+    }
 }
