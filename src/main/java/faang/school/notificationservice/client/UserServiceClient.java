daf3df87d2927118af28e189c7c1ffa022d08507
Kirill <kirill551@yandex.ru>
2025-06-04 12:41:04 +0200

    "Добавить health check для Service Discovery"

--- src/main/java/faang/school/notificationservice/client/UserServiceClient.java
+++ src/main/java/faang/school/notificationservice/client/UserServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", url = "${user-service.host}:${user-service.port}")
+@FeignClient(name = "${services.user-service.name}")
 public interface UserServiceClient {
 
     @GetMapping("/users/{id}")
