f6db9ebd7d02416c71f2fdfed8ed6dad082043b3
Stanislaff Fedyanin <re1971reg@gmail.com>
2025-06-15 20:17:00 +0300

    stream10 Notification Service (jacoco, checkstyle, CI/CD)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     java
     id("org.springframework.boot") version "3.0.6"
     id("io.spring.dependency-management") version "1.1.0"
+    id("jacoco")
+    id("checkstyle")
 }
 
 group = "faang.school"
@@ -60,10 +62,73 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport, tasks.jacocoTestCoverageVerification)
 }
 
 val test by tasks.getting(Test::class) { testLogging.showStandardStreams = true }
 
 tasks.bootJar {
     archiveFileName.set("service.jar")
+}
+
+checkstyle {
+    toolVersion = "10.17.0"
+    configFile = file("${project.rootDir}/config/checkstyle/checkstyle.xml")
+    checkstyle.enableExternalDtdLoad.set(true)
+}
+
+tasks.checkstyleMain {
+    source = fileTree("${project.rootDir}/src/main/java")
+    include("**/*.java")
+    exclude("**/resources/**")
+    classpath = files()
+}
+
+tasks.checkstyleTest {
+    source = fileTree("${project.rootDir}/src/test")
+    include("**/*.java")
+    classpath = files()
+}
+
+val jacocoExclude = listOf(
+    "faang/school/notificationservice/NotificationServiceApp*",
+    "faang/school/notificationservice/client/Feign*",
+    "**/dto/**",
+    "**/mapper/**"
+)
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(false)
+        csv.required.set(false)
+        html.required.set(true)
+    }
+    classDirectories.setFrom(classDirectories.files.map {
+        fileTree(it).matching {
+            exclude(jacocoExclude)
+        }
+    })
+}
+
+val jacocoClassExclude = listOf(
+    "faang.school.notificationservice.config.*",
+    "faang.school.notificationservice.dto.*",
+    "faang.school.notificationservice.mapper.*",
+    "faang.school.notificationservice.NotificationServiceApp",
+    "faang.school.notificationservice.client.Feign*"
+)
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            element = "CLASS"
+            isEnabled = true
+            excludes = jacocoClassExclude
+            limit {
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                minimum = "0.8".toBigDecimal()
+            }
+        }
+    }
 }
