1db5e873da2e57635cc7c140b43689350dc0f7b4
Jegor <gaanjara3@gmail.com>
2025-06-18 23:07:44 +0300

    [UPDATE] BJS2-77725 Сделал абстрактный класс EventListener-а.

--- src/main/java/faang/school/notificationservice/config/redis/RedisProperties.java
+++ src/main/java/faang/school/notificationservice/config/redis/RedisProperties.java
@@ -2,7 +2,6 @@ package faang.school.notificationservice.config.redis;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
