9ac8e6f1bffd75160d0ca31f9959136efc231017
Jegor <gaanjara3@gmail.com>
2025-06-20 11:31:31 +0300

    [UPDATE] BJS2-77725 Исправил RedisConfiguration как было оговорено(с Александром) чтобы листенеры добавляли топики через абстрактное решение.

--- src/main/java/faang/school/notificationservice/listener/AbstractEventListener.java
+++ src/main/java/faang/school/notificationservice/listener/AbstractEventListener.java
@@ -2,15 +2,20 @@ package faang.school.notificationservice.listener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.config.redis.RedisProperties;
 import faang.school.notificationservice.dto.UserDto;
 import faang.school.notificationservice.messaging.MessageBuilder;
 import faang.school.notificationservice.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.listener.ChannelTopic;
 
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -19,6 +24,7 @@ public abstract class AbstractEventListener<T> implements MessageListener {
     protected final List<MessageBuilder<T>> messageBuilders;
     protected final UserServiceClient userServiceClient;
     protected final ObjectMapper objectMapper;
+    protected final RedisProperties redisProperties;
 
     public String getMessage(Class<?> classType, Locale locale, T event) {
         MessageBuilder<T> messageBuilder = messageBuilders.stream()
@@ -34,5 +40,15 @@ public abstract class AbstractEventListener<T> implements MessageListener {
                         ns.getPreferredContact().equals(userDto.getPreference()))
                 .findFirst().orElseThrow(() -> new IllegalArgumentException("No required prefference found."));
         notificationService.send(userDto, message);
+    }
+
+    protected abstract List<String> getTopicNameKeys();
+
+    public Set<ChannelTopic> getChannelTopics() {
+        return redisProperties.getChannels().entrySet().stream()
+                .filter(entry -> getTopicNameKeys().contains(entry.getKey()))
+                .map(Map.Entry::getValue)
+                .map(ChannelTopic::new)
+                .collect(Collectors.toSet());
     }
 }
