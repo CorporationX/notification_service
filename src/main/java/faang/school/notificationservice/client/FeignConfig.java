cdfe1988066fa4abc2f35a8f25edc1b79ba63d9b
Vladimir Ekgardt <dj.estet@yandex.ru>
2025-06-25 14:43:41 +0300

    1) Удалил контроллер 2) Логгер удалил 3) Убрал переменные в проперти

--- src/main/java/faang/school/notificationservice/client/FeignConfig.java
+++ src/main/java/faang/school/notificationservice/client/FeignConfig.java
@@ -1,13 +1,19 @@
 package faang.school.notificationservice.client;
 
 import faang.school.notificationservice.config.context.UserContext;
-import feign.Logger;
 import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
+
+    @Value("${sms.username}")
+    private String username;
+
+    @Value("${sms.password}")
+    private String password;
 
     @Bean
     public FeignUserInterceptor feignUserInterceptor(UserContext userContext) {
@@ -15,12 +21,7 @@ public class FeignConfig {
     }
 
     @Bean
-    public Logger.Level feignLoggerLevel() {
-        return Logger.Level.FULL;  // FULL, BASIC, HEADERS, NONE
-    }
-
-    @Bean
     public BasicAuthRequestInterceptor basicAuthenticationInterceptor() {
-        return new BasicAuthRequestInterceptor("t89994676160", "504421");
+        return new BasicAuthRequestInterceptor(username, password);
     }
 }
