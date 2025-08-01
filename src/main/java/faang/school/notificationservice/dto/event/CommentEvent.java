2635622cd7b573ce04aa41febad3bbaf765f932d
Ilyas Kystaubayev <kystaubaev.iliyas@mail.ru>
2025-08-01 15:45:52 +0500

    Получение комментария

--- src/main/java/faang/school/notificationservice/dto/event/CommentEvent.java
+++ src/main/java/faang/school/notificationservice/dto/event/CommentEvent.java
@@ -1,16 +1,12 @@
 package faang.school.notificationservice.dto.event;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.io.Serializable;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class CommentEvent {
-    private Long commentId;
-    private Long postId;
-    private Long postAuthorId;
-    private Long commentAuthorId;
-    private String commentText;
+public record CommentEvent(
+        Long postId,
+        Long commentId,
+        Long commentAuthorId,
+        Long postAuthorId,
+        String commentText
+) implements Serializable {
 }
