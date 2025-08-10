2cba1b54bc137852b62157cc4962cc832648234e
mrnght <misterday116@gmail.com>
2025-08-10 17:22:37 +0800

    сделал конфиг редиса

---
+++ src/main/java/faang/school/notificationservice/dto/EventDto.java
@@ -0,0 +1,5 @@
+package faang.school.notificationservice.dto;
+
+public class EventDto {
+
+}
---
+++ src/main/java/faang/school/notificationservice/listener/EventStartListener.java
@@ -0,0 +1,35 @@
+package faang.school.notificationservice.listener;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.EventDto;
+import faang.school.notificationservice.messaging.MessageBuilder;
+import faang.school.notificationservice.service.NotificationService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class EventStartListener extends AbstractEventListener<EventDto> implements MessageListener {
+
+    public EventStartListener(@Qualifier("objectMapper") ObjectMapper objectMapper,
+                              UserServiceClient userServiceClient,
+                              List<NotificationService> notificationServices,
+                              List<MessageBuilder<EventDto>> messageBuilders, ObjectMapper objectMapper) {
+        super(objectMapper, userServiceClient, notificationServices, messageBuilders);
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        try {
+            EventDto event = objectMapper.readValue(message.getBody(), EventDto.class);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
