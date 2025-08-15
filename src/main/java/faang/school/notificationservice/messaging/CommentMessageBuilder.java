b0972258a8d9c1aa054eb39e70a45f9bc8bef2b7
JekaCAP <agentgeka@yandex.ru>
2025-08-15 15:18:37 +0500

    BJS2-84156 Правки в CommentMessageBuilder

--- src/main/java/faang/school/notificationservice/messaging/CommentMessageBuilder.java
+++ src/main/java/faang/school/notificationservice/messaging/CommentMessageBuilder.java
@@ -1,6 +1,8 @@
 package faang.school.notificationservice.messaging;
 
 import faang.school.notificationservice.dto.CommentEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 
 import java.util.Locale;
@@ -15,7 +17,10 @@ import java.util.Locale;
  * @since 13.08.2025
  */
 @Component
+@RequiredArgsConstructor
 public class CommentMessageBuilder implements MessageBuilder<CommentEvent> {
+    private final MessageSource messageSource;
+
     @Override
     public Class<?> getInstance() {
         return CommentEvent.class;
@@ -23,8 +28,10 @@ public class CommentMessageBuilder implements MessageBuilder<CommentEvent> {
 
     @Override
     public String buildMessage(CommentEvent event, Locale locale) {
-        return "Пользователь " + event.commentAuthorId() +
-               " прокомментировал ваш пост " + event.postId() +
-               ": \"" + event.text() + "\"";
+        return messageSource.getMessage(
+                "comment.new",
+                new Object[]{event.commentAuthorId(), event.postId(), event.text()},
+                locale
+        );
     }
 }
