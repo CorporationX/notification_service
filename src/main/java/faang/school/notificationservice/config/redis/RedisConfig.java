796eb1a03d3a60a60c1a01927655ff7ba00bacd1
mrnght <misterday116@gmail.com>
2025-08-16 00:06:29 +0800

    BJS2-84147 исправил конфликт

--- src/main/java/faang/school/notificationservice/config/redis/RedisConfig.java
+++ src/main/java/faang/school/notificationservice/config/redis/RedisConfig.java
@@ -16,6 +16,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfig {
 
     private final CommentEventListener listener;
