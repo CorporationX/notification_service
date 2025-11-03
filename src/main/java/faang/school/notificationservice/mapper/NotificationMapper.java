6ad55bf0cec4b9385a0c7e402e74936797cb2162
Andre` <acmisatyuk@gmail.com>
2025-11-03 12:06:16 +0800

    BJS2-97035 Отправка SMS сообщений: initial version

---
+++ src/main/java/faang/school/notificationservice/mapper/NotificationMapper.java
@@ -0,0 +1,17 @@
+package faang.school.notificationservice.mapper;
+
+import faang.school.notificationservice.dto.SendSmsRequestDto;
+import faang.school.notificationservice.dto.UserDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface NotificationMapper {
+
+    @Mapping(source = "userId", target = "id")
+    @Mapping(source = "phone", target = "phone")
+    @Mapping(target = "username", ignore = true)
+    @Mapping(target = "email", ignore = true)
+    @Mapping(target = "preference", constant = "PHONE")
+    UserDto toUserDto(SendSmsRequestDto userDto);
+}
