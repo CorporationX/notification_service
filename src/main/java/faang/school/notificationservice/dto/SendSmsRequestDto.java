8d937372a38ef1405ed735567c19585484bfd81d
Andre` <acmisatyuk@gmail.com>
2025-11-03 12:08:57 +0800

    BJS2-97035 Отправка SMS сообщений: initial version

--- src/main/java/faang/school/notificationservice/dto/SendSmsRequestDto.java
+++ src/main/java/faang/school/notificationservice/dto/SendSmsRequestDto.java
@@ -1,10 +1,14 @@
 package faang.school.notificationservice.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record SendSmsRequestDto(
+        @NotNull
         long userId,
+        @NotNull
         String phone,
+        @NotEmpty
         String message
 ) {
 
