96da7b65624ae8c01c1e5eb306e0a0e06e3e7f2a
Andre` <acmisatyuk@gmail.com>
2025-11-10 17:50:23 +0300

    BJS2-97035 Отправка SMS сообщений: build failed, code review

--- src/main/java/faang/school/notificationservice/controller/NotificationController.java
+++ src/main/java/faang/school/notificationservice/controller/NotificationController.java
@@ -4,6 +4,7 @@ import faang.school.notificationservice.dto.SendSmsRequestDto;
 import faang.school.notificationservice.dto.UserDto;
 import faang.school.notificationservice.mapper.NotificationMapper;
 import faang.school.notificationservice.service.NotificationService;
+import faang.school.notificationservice.service.SmsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/notify")
 public class NotificationController {
-    private final NotificationService smsService;
+    private final SmsServiceImpl smsService;
     private final NotificationMapper mapper;
 
     @PostMapping("/sms")
