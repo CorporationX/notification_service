5da98289ca47a1365d7d79386972e23af69dbb96
Elya Khamzina <ekkhamzi@mail.ru>
2025-11-11 00:57:21 +0300

    feature-BJS2-97031-send-email-messages 2

--- src/main/java/faang/school/notificationservice/service/EmailService.java
+++ src/main/java/faang/school/notificationservice/service/EmailService.java
@@ -4,6 +4,7 @@ import faang.school.notificationservice.dto.UserDto;
 import faang.school.notificationservice.error.EmailGatewayException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -15,12 +16,15 @@ public class EmailService implements NotificationService {
 
     private final JavaMailSender emailSender;
 
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     @Override
     public void send(UserDto user, String message) {
         try {
             SimpleMailMessage mailmessage = new SimpleMailMessage();
-            mailmessage.setFrom("live1qweq@gmail.com");
-            mailmessage.setTo(user.getEmail()); //@Data автоматически генерирует: getEmail() (У UserDto есть поле email)
+            mailmessage.setFrom(fromEmail);
+            mailmessage.setTo(user.getEmail());
             mailmessage.setSubject("Notification");
             mailmessage.setText(message);
             emailSender.send(mailmessage);
