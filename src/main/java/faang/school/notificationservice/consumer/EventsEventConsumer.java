e72710830e7fed00113ca09cbc26e7ec8d1a89f0
Scherbi <shherbakov99ilya@mail.ru>
2025-11-13 23:00:08 +0300

    BJS2-97312 decomposition event listener

--- src/main/java/faang/school/notificationservice/consumer/EventsEventConsumer.java
+++ src/main/java/faang/school/notificationservice/consumer/EventsEventConsumer.java
@@ -1,25 +1,15 @@
 package faang.school.notificationservice.consumer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import faang.school.notificationservice.client.UserServiceClient;
 import faang.school.notificationservice.dto.EventStartEventDto;
-import faang.school.notificationservice.dto.TimeLeft;
-import faang.school.notificationservice.dto.UserDto;
-import faang.school.notificationservice.dto.UserIdsClientDto;
-import faang.school.notificationservice.messaging.EventMessageConsumer;
 import faang.school.notificationservice.service.EventNotificationService;
-import faang.school.notificationservice.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.support.Acknowledgment;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 @Slf4j
@@ -28,7 +18,6 @@ import java.util.Map;
 public class EventsEventConsumer {
 
     private final EventNotificationService eventNotificationService;
-    private final EventMessageConsumer eventOwnerMessageConsumer;
     private final ObjectMapper objectMapper;
 
     @KafkaListener(topics = "${spring.kafka.topic.events}",
@@ -37,21 +26,8 @@ public class EventsEventConsumer {
 
         EventStartEventDto eventStartEventDto = objectMapper.convertValue(message, EventStartEventDto.class);
 
-        String text = eventOwnerMessageConsumer.buildMessage(eventStartEventDto, Locale.getDefault());
+        eventNotificationService.processEventStart(eventStartEventDto);
 
-        log.info("Information about the event has arrived! event id - {}, owner id-{} and name-{}, name event -{}",
-                eventStartEventDto.eventId(), eventStartEventDto.userId(),
-                eventStartEventDto.nameOwner(), eventStartEventDto.titleEvent());
-
-        List<UserDto> attendeesIds = eventStartEventDto.attendeesUser();
-
-        if (attendeesIds.isEmpty()) {
-            log.info("There are no subscribers to the event {}.", eventStartEventDto.eventId());
-        } else {
-            attendeesIds.forEach(user -> eventNotificationService.send(user
-                    , text));
-        }
-        log.info("Received EventStartEvent: {} ", eventStartEventDto.nameOwner());
         ack.acknowledge();
     }
 }
--- src/main/java/faang/school/notificationservice/service/EventNotificationService.java
+++ src/main/java/faang/school/notificationservice/service/EventNotificationService.java
@@ -1,16 +1,21 @@
 package faang.school.notificationservice.service;
 
-import faang.school.notificationservice.client.UserServiceClient;
+import faang.school.notificationservice.dto.EventStartEventDto;
 import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.messaging.EventMessageConsumer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Locale;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class EventNotificationService {
 
+    private final EventMessageConsumer eventOwnerMessageConsumer;
 
     public void send(UserDto userDto, String message) {
         log.info("{} - {}", userDto.getId(), message);
@@ -19,5 +24,23 @@ public class EventNotificationService {
 
     public UserDto.PreferredContact getPreferredContact() {
         return UserDto.PreferredContact.PHONE;
+    }
+
+    public void processEventStart(EventStartEventDto eventStartEventDto) {
+        String text = eventOwnerMessageConsumer.buildMessage(eventStartEventDto, Locale.getDefault());
+
+        log.info("Information about the event has arrived! event id - {}, owner id-{} and name-{}, name event -{}",
+                eventStartEventDto.eventId(), eventStartEventDto.userId(),
+                eventStartEventDto.nameOwner(), eventStartEventDto.titleEvent());
+
+        List<UserDto> attendeesIds = eventStartEventDto.attendeesUser();
+
+        if (attendeesIds.isEmpty()) {
+            log.info("There are no subscribers to the event {}.", eventStartEventDto.eventId());
+        } else {
+            attendeesIds.forEach(user -> send(user
+                    , text));
+        }
+        log.info("Received EventStartEvent: {} ", eventStartEventDto.nameOwner());
     }
 }
