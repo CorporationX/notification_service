6d96cf323d0fc399b8e02f67db3cb59282b88f09
Viks <viktor.naroznoi@gmail.com>
2025-11-24 11:27:30 +0200

    --BJS2-97084 - corrections based on feedback

--- src/main/java/faang/school/notificationservice/dto/event/GoalCompletedEvent.java
+++ src/main/java/faang/school/notificationservice/dto/event/GoalCompletedEvent.java
@@ -1,7 +1,11 @@
 package faang.school.notificationservice.dto.event;
 
+import jakarta.validation.constraints.NotNull;
+
 public record GoalCompletedEvent(
+        @NotNull
         Long userId,
+        @NotNull
         Long goalId
 ) {
 }
