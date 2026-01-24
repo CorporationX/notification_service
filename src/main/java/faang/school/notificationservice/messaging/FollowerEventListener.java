cf0cc27677dacb20fabbe37b0bf7255009594dcc
derzhko <a.derzhko@yandex.ru>
2026-01-24 22:55:45 +0300

    c1

--- src/main/java/faang/school/notificationservice/messaging/FollowerEventListener.java
+++ src/main/java/faang/school/notificationservice/messaging/FollowerEventListener.java
@@ -1,24 +1,40 @@
 package faang.school.notificationservice.messaging;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.notificationservice.client.UserServiceClient;
 import faang.school.notificationservice.dto.FollowerEvent;
+import faang.school.notificationservice.dto.UserDto;
+import faang.school.notificationservice.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class FollowerEventListener implements MessageListener {
 
     private final ObjectMapper objectMapper;
+    private final List<NotificationService> notificationServices;
+    private final UserServiceClient userServiceClient;
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
         try {
             FollowerEvent event = objectMapper.readValue(message.getBody(), FollowerEvent.class);
+            UserDto follower = userServiceClient.getUser(event.getFollowerId());
+            UserDto followee = userServiceClient.getUser(event.getFolloweeId());
+            String text = "YOU followee from " + follower.getUsername();
+            notificationServices.stream()
+                    .filter(service -> service.getPreferredContact() == followee.getPreference())
+                    .findFirst()
+                    .ifPresentOrElse(
+                            service -> service.send(followee, text),
+                            () -> {}
+                    );
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
