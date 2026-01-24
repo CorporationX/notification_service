267e4ce696911457d90d8b45c23963e80d02e6a0
derzhko <a.derzhko@yandex.ru>
2026-01-25 01:29:16 +0300

    fix warnings

--- src/main/java/faang/school/notificationservice/config/RedisConfiguration.java
+++ src/main/java/faang/school/notificationservice/config/RedisConfiguration.java
@@ -10,15 +10,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import javax.management.monitor.StringMonitor;
 
 @Configuration
 @RequiredArgsConstructor
